7b21b9d9cab4364bb22ceab4c8626ff12a7ee9cb
Alex Naidis <alex.naidis@linux.com>
2016-12-31 16:08:30 +0100

    kgsl: Increase priority of RT thread
    We need kgsl_worker_thread to preempt
    all userspace surfaceflinger threads to
    avoid a possible deadlock.
    
    This will prevent the SF
    threads from "stealing" cputime from
    kgsl_worker_thread.
    This is important, since kgsl_worker_thread
    executes work which blocks SF from proceeding.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5474,7 +5474,7 @@ static void kgsl_core_exit(void)
 static int __init kgsl_core_init(void)
 {
 	int result = 0;
-	struct sched_param param = { .sched_priority = 2 };
+	struct sched_param param = { .sched_priority = 6 };
 
 	/* alloc major and minor device numbers */
 	result = alloc_chrdev_region(&kgsl_driver.major, 0,
