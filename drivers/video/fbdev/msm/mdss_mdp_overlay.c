c69843bf6bc04d92a594bed7b2073212d235a21e
Sultan Alsawaf <sultan@kerneltoast.com>
2018-05-29 17:19:45 -0700

    msm: mdss: Mark IRQ and important kthreads as performance critical
    These items are in the critical path for rendering frames to the display
    (they exhibit increased CPU usage when heavy frame rendering takes place),
    so mark them as performance-critical.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -6291,7 +6291,7 @@ static int __vsync_retire_setup(struct msm_fb_data_type *mfd)
 		kthread_init_work(&mdp5_data->vsync_work,
 			__vsync_retire_work_handler);
 
-		mdp5_data->thread = kthread_run(kthread_worker_fn,
+		mdp5_data->thread = kthread_run_perf_critical(cpu_perf_mask kthread_worker_fn,
 					&mdp5_data->worker,
 					"vsync_retire_work");
 		if (IS_ERR(mdp5_data->thread)) {
