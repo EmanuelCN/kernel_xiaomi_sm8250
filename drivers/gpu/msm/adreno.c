dc7408ccebe32f228739ae3f49c589b71077d066
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-08-05 18:05:08 +0200

    gpu: adreno: Increase data processing limit
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -34,7 +34,7 @@ static unsigned int counter_delta(struct kgsl_device *device,
 
 static struct devfreq_msm_adreno_tz_data adreno_tz_data = {
 	.bus = {
-		.max = 350,
+		.max = 1200,
 		.floating = true,
 	},
 	.device_id = KGSL_DEVICE_3D0,
