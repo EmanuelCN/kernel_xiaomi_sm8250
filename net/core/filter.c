a63f11c5474be7f53dfed8ca9a4296574e44b242
Jesper Dangaard Brouer <brouer@redhat.com>
2018-09-03 09:54:57 +0200

    UPSTREAM: xdp: unlikely instrumentation for xdp map redirect
    Notice the compiler generated ASM code layout was suboptimal.  It
    assumed map enqueue errors as the likely case, which is shouldn't.
    It assumed that xdp_do_flush_map() was a likely case, due to maps
    changing between packets, which should be very unlikely.
    
    Change-Id: Id4d1f724dfceabb5288efa82f81ab54d85fb371b
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3174,7 +3174,7 @@ static int __bpf_tx_xdp_map(struct net_device *dev_rx, void *fwd,
 		struct bpf_dtab_netdev *dst = fwd;
 
 		err = dev_map_enqueue(dst, xdp, dev_rx);
-		if (err)
+		if (unlikely(err))
 			return err;
 		break;
 	}
@@ -3182,7 +3182,7 @@ static int __bpf_tx_xdp_map(struct net_device *dev_rx, void *fwd,
 		struct bpf_cpu_map_entry *rcpu = fwd;
 
 		err = cpu_map_enqueue(rcpu, xdp, dev_rx);
-		if (err)
+		if (unlikely(err))
 			return err;
 		break;
 	}
@@ -3269,7 +3269,7 @@ static int xdp_do_redirect_map(struct net_device *dev, struct xdp_buff *xdp,
 		err = -EINVAL;
 		goto err;
 	}
-	if (ri->map_to_flush && ri->map_to_flush != map)
+	if (ri->map_to_flush && unlikely(ri->map_to_flush != map))
 		xdp_do_flush_map();
 
 	err = __bpf_tx_xdp_map(dev, fwd, map, xdp, index);
