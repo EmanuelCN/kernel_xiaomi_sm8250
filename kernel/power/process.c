84784ddf98e380bc5be0cf6013f3f549d3bd0bb3
Panchajanya1999 <rsk52959@gmail.com>
2018-11-12 21:47:47 +0530

    power: process: Use lesser time to enter sleep
    Revise
    https://github.com/Panchajanya1999/msm-4.4/commit/803b0f827cfa5d34902a0e4441ca79c0a25f5e24
    
    Change-Id: I7fec9001dbc0d043893f9c3f4ef9237e7d5c9c78
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -26,7 +26,7 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 5 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
