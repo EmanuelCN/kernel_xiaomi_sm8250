608ffd52e8be07787fe6b89066bf5ca4252e809d
Rick Yiu <rickyiu@google.com>
2018-11-23 18:50:18 +0800

    sched: fix issue of cpu freq running at max always
    All cpus are running at max freq, the reason is that in the check of
    sugov_up_down_rate_limit() in cpufreq_schedutil.c, the time passed in
    is always 0, so the check is always true and hence the freq will not
    be updated. It is caused by sched_ktime_clock() will return 0 if
    CONFIG_SCHED_WALT is not set. Fix it by replacing sched_ktime_clock()
    with ktime_get_ns().
    
    Bug: 119932718
    Test: cpu freq could change after fix
    Change-Id: I62a0b35208dcd7a1d23da27f909cce3e59208d1f
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2120,10 +2120,7 @@ unsigned long
 cpu_util_freq_walt(int cpu, struct sched_walt_cpu_load *walt_load);
 #else
 #define sched_ravg_window TICK_NSEC
-static inline u64 sched_ktime_clock(void)
-{
-	return sched_clock();
-}
+#define sched_ktime_clock ktime_get_ns
 #endif
 
 #ifndef arch_scale_freq_capacity
