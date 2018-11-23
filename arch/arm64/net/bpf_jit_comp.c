e05fc4be36ef6aaefea6528a75466b4805ac4ec1
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-11-23 23:18:04 +0100

    arm64/bpf: don't allocate BPF JIT programs in module memory
    The arm64 module region is a 128 MB region that is kept close to
    the core kernel, in order to ensure that relative branches are
    always in range. So using the same region for programs that do
    not have this restriction is wasteful, and preferably avoided.
    
    Now that the core BPF JIT code permits the alloc/free routines to
    be overridden, implement them by vmalloc()/vfree() calls from a
    dedicated 128 MB region set aside for BPF programs. This ensures
    that BPF programs are still in branching range of each other, which
    is something the JIT currently depends upon (and is not guaranteed
    when using module_alloc() on KASLR kernels like we do currently).
    It also ensures that placement of BPF programs does not correlate
    with the placement of the core kernel or modules, making it less
    likely that leaking the former will reveal the latter.
    
    This also solves an issue under KASAN, where shadow memory is
    needlessly allocated for all BPF programs (which don't require KASAN
    shadow pages since they are not KASAN instrumented)
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    [@0ctobot: Avoid using module memory in CFI alignment check subsequent
    to cfd89c3 ("ANDROID: arm64: bpf: implement arch_bpf_jit_check_func")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -963,6 +963,19 @@ out:
 	return prog;
 }
 
+void *bpf_jit_alloc_exec(unsigned long size)
+{
+	return __vmalloc_node_range(size, PAGE_SIZE, BPF_JIT_REGION_START,
+				    BPF_JIT_REGION_END, GFP_KERNEL,
+				    PAGE_KERNEL_EXEC, 0, NUMA_NO_NODE,
+				    __builtin_return_address(0));
+}
+
+void bpf_jit_free_exec(void *addr)
+{
+	return vfree(addr);
+}
+
 #ifdef CONFIG_CFI_CLANG
 bool arch_bpf_jit_check_func(const struct bpf_prog *prog)
 {
@@ -970,17 +983,10 @@ bool arch_bpf_jit_check_func(const struct bpf_prog *prog)
 
 	/*
 	 * bpf_func must be correctly aligned and within the correct region.
-	 * module_alloc places JIT code in the module region, unless
-	 * ARM64_MODULE_PLTS is enabled, in which case we might end up using
-	 * the vmalloc region too.
 	 */
 	if (unlikely(!IS_ALIGNED(func, sizeof(u32))))
 		return false;
 
-	if (IS_ENABLED(CONFIG_ARM64_MODULE_PLTS) &&
-			is_vmalloc_addr(prog->bpf_func))
-		return true;
-
-	return (func >= MODULES_VADDR && func < MODULES_END);
+	return (func >= BPF_JIT_REGION_START && func < BPF_JIT_REGION_END);
 }
 #endif
