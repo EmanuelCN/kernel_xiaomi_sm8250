2acb2ad341c5a10bcaed3731858c28f4eb28cec3
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-29 23:23:49 -0800

    msm: msm_bus: Don't enable QoS clocks when none are present
    There's no point in enabling QoS clocks when are none of them for certain
    clients.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_fabric_rpmh.c
+++ drivers/soc/qcom/msm_bus/msm_bus_fabric_rpmh.c
@@ -967,7 +967,7 @@ static int msm_bus_dev_init_qos(struct device *dev, void *data)
 		goto exit_init_qos;
 	}
 
-	if (node_dev->ap_owned) {
+	if (node_dev->ap_owned && node_dev->num_node_qos_clks) {
 		struct msm_bus_node_device_type *bus_node_info;
 
 		bus_node_info =
