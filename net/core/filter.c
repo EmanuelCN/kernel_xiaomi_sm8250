afc1aa7ddd46ede2c3df46fd0ada275ec1086bb0
Joe Stringer <joe@wand.net.nz>
2018-11-30 15:32:20 -0800

    BACKPORT: bpf: Support sk lookup in netns with id 0
    David Ahern and Nicolas Dichtel report that the handling of the netns id
    0 is incorrect for the BPF socket lookup helpers: rather than finding
    the netns with id 0, it is resolving to the current netns. This renders
    the netns_id 0 inaccessible.
    
    To fix this, adjust the API for the netns to treat all negative s32
    values as a lookup in the current netns (including u64 values which when
    truncated to s32 become negative), while any values with a positive
    value in the signed 32-bit integer space would result in a lookup for a
    socket in the netns corresponding to that id. As before, if the netns
    with that ID does not exist, no socket will be found. Any netns outside
    of these ranges will fail to find a corresponding socket, as those
    values are reserved for future usage.
    
    Change-Id: Ib10a6977dbb38065ef7199b33aa99b96e020c9bb
    Signed-off-by: Joe Stringer <joe@wand.net.nz>
    Acked-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Acked-by: Joey Pabalinas <joeypabalinas@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -4794,7 +4794,8 @@ __bpf_skc_lookup(struct sk_buff *skb, struct bpf_sock_tuple *tuple, u32 len,
 	int sdif;
 
 	family = len == sizeof(tuple->ipv4) ? AF_INET : AF_INET6;
-	if (unlikely(family == AF_UNSPEC || netns_id > U32_MAX || flags))
+	if (unlikely(family == AF_UNSPEC || flags ||
+		     !((s32)netns_id < 0 || netns_id <= S32_MAX)))
 		goto out;
 
 	if (family == AF_INET)
@@ -4802,15 +4803,15 @@ __bpf_skc_lookup(struct sk_buff *skb, struct bpf_sock_tuple *tuple, u32 len,
 	else
 		sdif = inet6_sdif(skb);
 
-	if (netns_id) {
+	if ((s32)netns_id < 0) {
+		net = caller_net;
+		sk = sk_lookup(net, tuple, ifindex, sdif, family, proto);
+	} else {
 		net = get_net_ns_by_id(caller_net, netns_id);
 		if (unlikely(!net))
 			goto out;
 		sk = sk_lookup(net, tuple, ifindex, sdif, family, proto);
 		put_net(net);
-	} else {
-		net = caller_net;
-		sk = sk_lookup(net, tuple, ifindex, sdif, family, proto);
 	}
 
 out:
