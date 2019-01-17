5ca346ef4df850e69e821c6cdbc34b81d93a0ff4
Eric Dumazet <edumazet@google.com>
2019-01-17 11:23:33 -0800

    UPSTREAM: tcp: move icsk_rto init to tcp_disconnect()
    If we make sure a listener always has its icsk_rto
    field set to TCP_TIMEOUT_INIT, we do not need to rewrite
    this field after a new clone is created.
    
    tcp_disconnect() is very seldom used in real applications.
    
    Change-Id: I699da3b9cd4e2c74c86d4e558b6360472e845415
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2617,6 +2617,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 
 	tp->snd_cwnd = 2;
 	icsk->icsk_probes_out = 0;
+	icsk->icsk_rto = TCP_TIMEOUT_INIT;
 	tp->snd_ssthresh = TCP_INFINITE_SSTHRESH;
 	tp->snd_cwnd_cnt = 0;
 	tp->window_clamp = 0;
