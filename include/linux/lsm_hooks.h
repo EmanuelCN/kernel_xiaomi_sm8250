a577a45379e1511946b28fed7648ffda7909b46f
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2019-01-18 19:15:59 +0900

    UPSTREAM: LSM: Make lsm_early_cred() and lsm_early_task() local functions.
    Since current->cred == current->real_cred when ordered_lsm_init()
    is called, and lsm_early_cred()/lsm_early_task() need to be called
    between the amount of required bytes is determined and module specific
    initialization function is called, we can move these calls from
    individual modules to ordered_lsm_init().
    
    Change-Id: I61b65ce4f4991aa0c9e437cd1febdd1eceaa3469
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1553,11 +1553,6 @@ static inline void security_delete_hooks(struct security_hook_list *hooks,
 
 extern void __init capability_add_hooks(void);
 
-#ifdef CONFIG_SECURITY
-void __init lsm_early_cred(struct cred *cred);
-void __init lsm_early_task(struct task_struct *task);
-#endif
-
 extern int lsm_inode_alloc(struct inode *inode);
 
 #endif /* ! __LINUX_LSM_HOOKS_H */
