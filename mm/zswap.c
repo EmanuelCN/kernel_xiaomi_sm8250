3f38dd9988140c6fc68e9f98fe7dc4fe3593dc94
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-01-22 16:21:08 +0100

    zswap: ignore debugfs_create_dir() return value
    When calling debugfs functions, there is no need to ever check the
    return value.  The function can work or not, but the code logic should
    never do something different based on this.
    
    Cc: Seth Jennings <sjenning@redhat.com>
    Cc: linux-mm@kvack.org
    Acked-by: Dan Streetman <ddstreet@ieee.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -1262,8 +1262,6 @@ static int __init zswap_debugfs_init(void)
 		return -ENODEV;
 
 	zswap_debugfs_root = debugfs_create_dir("zswap", NULL);
-	if (!zswap_debugfs_root)
-		return -ENOMEM;
 
 	debugfs_create_u64("pool_limit_hit", 0444,
 			   zswap_debugfs_root, &zswap_pool_limit_hit);
