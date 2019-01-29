2d31649ba7ea35a34cf1ae5c9ba6991357ec5ab5
Peter Zijlstra <peterz@infradead.org>
2019-01-28 17:21:52 -0800

    UPSTREAM: bpf: check that BPF programs run with preemption disabled
    Introduce cant_sleep() macro for annotation of functions that
    cannot sleep.
    
    Use it in BPF_PROG_RUN to catch execution of BPF programs in
    preemptable context.
    
    Suggested-by: Jann Horn <jannh@google.com>
    Change-Id: If46f95bfbb77de85daf1db948927a72460010d07
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -632,7 +632,7 @@ static inline void bpf_jit_set_header_magic(struct bpf_binary_header *hdr)
 }
 #endif
 
-#define BPF_PROG_RUN(filter, ctx)  bpf_call_func(filter, ctx)
+#define BPF_PROG_RUN(filter, ctx)  ({ cant_sleep(); (*(filter)->bpf_func)(ctx, (filter)->insnsi); })
 
 #define BPF_SKB_CB_LEN QDISC_CB_PRIV_LEN
 
