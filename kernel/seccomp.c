48bb39ad9d968a76ecf0454655c9a3c6087ca114
Alexei Starovoitov <ast@kernel.org>
2019-02-21 10:40:14 -0800

    UPSTREAM: seccomp, bpf: disable preemption before calling into bpf prog
    All BPF programs must be called with preemption disabled.
    
    Fixes: 568f196756ad ("bpf: check that BPF programs run with preemption disabled")
    Reported-by: syzbot+8bf19ee2aa580de7a2a7@syzkaller.appspotmail.com
    Change-Id: Ia5fa93009d0e31261eab2890b435730f4e310c6a
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -210,6 +210,7 @@ static u32 seccomp_run_filters(const struct seccomp_data *sd,
 	 * All filters in the list are evaluated and the lowest BPF return
 	 * value always takes priority (ignoring the DATA).
 	 */
+	preempt_disable();
 	for (; f; f = f->prev) {
 		u32 cur_ret = BPF_PROG_RUN(f->prog, sd);
 
@@ -218,6 +219,7 @@ static u32 seccomp_run_filters(const struct seccomp_data *sd,
 			*match = f;
 		}
 	}
+	preempt_enable();
 	return ret;
 }
 #endif /* CONFIG_SECCOMP_FILTER */
