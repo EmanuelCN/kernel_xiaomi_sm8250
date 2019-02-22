fedcb99a9bc745eb1bef044ebe00b494333122b4
freak07 <michalechner92@googlemail.com>
2019-02-22 08:35:36 +0100

    mm: vmstat: use power efficient workingqueues
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1904,7 +1904,7 @@ static void vmstat_shepherd(struct work_struct *w)
 	}
 	put_online_cpus();
 
-	schedule_delayed_work(&shepherd,
+	queue_delayed_work(system_power_efficient_wq, &shepherd,
 		round_jiffies_relative(sysctl_stat_interval));
 }
 
@@ -1916,7 +1916,7 @@ static void __init start_shepherd_timer(void)
 		INIT_DEFERRABLE_WORK(per_cpu_ptr(&vmstat_work, cpu),
 			vmstat_update);
 
-	schedule_delayed_work(&shepherd,
+	queue_delayed_work(system_power_efficient_wq, &shepherd,
 		round_jiffies_relative(sysctl_stat_interval));
 }
 
