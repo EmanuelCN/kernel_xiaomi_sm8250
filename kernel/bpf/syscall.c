cd3a00fe5c9bd0ae19703db52fe34b9f690b0c9a
Alexei Starovoitov <ast@kernel.org>
2019-02-25 14:28:40 -0800

    UPSTREAM: bpf: expose program stats via bpf_prog_info
    Return bpf program run_time_ns and run_cnt via bpf_prog_info
    
    Change-Id: I76830ceaabd2de88a6d76c04c3e5a4c356c29ee6
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2191,6 +2191,7 @@ static int bpf_prog_get_info_by_fd(struct file *file,
 	struct bpf_prog_info __user *uinfo = u64_to_user_ptr(attr->info.info);
 	struct bpf_prog_info info;
 	u32 info_len = attr->info.info_len;
+	struct bpf_prog_stats stats;
 	char __user *uinsns;
 	u32 ulen;
 	int err;
@@ -2230,6 +2231,10 @@ static int bpf_prog_get_info_by_fd(struct file *file,
 	err = set_info_rec_size(&info);
 	if (err)
 		return err;
+
+	bpf_prog_get_stats(prog, &stats);
+	info.run_time_ns = stats.nsecs;
+	info.run_cnt = stats.cnt;
 
 	if (!capable(CAP_SYS_ADMIN)) {
 		info.jited_prog_len = 0;
