fccea85f1aef479bbd19c067f3c84c9042fc3b26
Andrey Ignatov <rdna@fb.com>
2019-02-27 13:28:48 -0800

    BACKPORT: bpf: Introduce bpf_sysctl_get_name helper
    Add bpf_sysctl_get_name() helper to copy sysctl name (/proc/sys/ entry)
    into provided by BPF_PROG_TYPE_CGROUP_SYSCTL program buffer.
    
    By default full name (w/o /proc/sys/) is copied, e.g. "net/ipv4/tcp_mem".
    
    If BPF_F_SYSCTL_BASE_NAME flag is set, only base name will be copied,
    e.g. "tcp_mem".
    
    Documentation for the new helper is provided in bpf.h UAPI.
    
    Change-Id: Icbe00bfd09e7914393adde88a9cc053b27151e34
    Signed-off-by: Andrey Ignatov <rdna@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2382,6 +2382,22 @@ union bpf_attr {
  *		Pointer to **struct bpf_sock**, or **NULL** in case of failure.
  *		For sockets with reuseport option, the **struct bpf_sock**
  *		result is from **reuse->socks**\ [] using the hash of the tuple.
+ *
+ * int bpf_sysctl_get_name(struct bpf_sysctl *ctx, char *buf, size_t buf_len, u64 flags)
+ *	Description
+ *		Get name of sysctl in /proc/sys/ and copy it into provided by
+ *		program buffer *buf* of size *buf_len*.
+ *
+ *		The buffer is always NUL terminated, unless it's zero-sized.
+ *
+ *		If *flags* is zero, full name (e.g. "net/ipv4/tcp_mem") is
+ *		copied. Use **BPF_F_SYSCTL_BASE_NAME** flag to copy base name
+ *		only (e.g. "tcp_mem").
+ *	Return
+ *		Number of character copied (not including the trailing NUL).
+ *
+ *		**-E2BIG** if the buffer wasn't big enough (*buf* will contain
+ *		truncated name in this case).
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -2575,6 +2591,9 @@ enum bpf_func_id {
 #define BPF_F_ADJ_ROOM_ENCAP_L3_IPV6	(1ULL << 2)
 #define BPF_F_ADJ_ROOM_ENCAP_L4_GRE	(1ULL << 3)
 #define BPF_F_ADJ_ROOM_ENCAP_L4_UDP	(1ULL << 4)
+
+/* BPF_FUNC_sysctl_get_name flags. */
+#define BPF_F_SYSCTL_BASE_NAME		(1ULL << 0)
 
 /* Mode for BPF_FUNC_skb_adjust_room helper. */
 enum bpf_adj_room_mode {
