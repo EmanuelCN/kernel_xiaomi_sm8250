1dfb730c47db050de2cb45f28c2b96a3c7cb0a0e
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-08 23:02:21 +0900

    blk: disable IO_STAT completely
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- block/blk.h
+++ block/blk.h
@@ -308,9 +308,7 @@ extern int blk_update_nr_requests(struct request_queue *, unsigned int);
  */
 static inline bool blk_do_io_stat(struct request *rq)
 {
-	return rq->rq_disk &&
-	       (rq->rq_flags & RQF_IO_STAT) &&
-		!blk_rq_is_passthrough(rq);
+	return false;
 }
 
 static inline void req_set_nomerge(struct request_queue *q, struct request *req)
