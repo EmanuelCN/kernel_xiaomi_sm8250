531c0f8810131311724cbb5750adcdec23fbf148
Martin KaFai Lau <kafai@fb.com>
2019-03-12 10:23:04 -0700

    BACKPORT: bpf: Add bpf_get_listener_sock(struct bpf_sock *sk) helper
    Add a new helper "struct bpf_sock *bpf_get_listener_sock(struct bpf_sock *sk)"
    which returns a bpf_sock in TCP_LISTEN state.  It will trace back to
    the listener sk from a request_sock if possible.  It returns NULL
    for all other cases.
    
    No reference is taken because the helper ensures the sk is
    in SOCK_RCU_FREE (where the TCP_LISTEN sock should be in).
    Hence, bpf_sk_release() is unnecessary and the verifier does not
    allow bpf_sk_release(listen_sk) to be called either.
    
    The following is also allowed because the bpf_prog is run under
    rcu_read_lock():
    
    	sk = bpf_sk_lookup_tcp();
    	/* if (!sk) { ... } */
    	listen_sk = bpf_get_listener_sock(sk);
    	/* if (!listen_sk) { ... } */
    	bpf_sk_release(sk);
    	src_port = listen_sk->src_port; /* Allowed */
    
    Change-Id: I143d228735c2561dab63e58ed404e2ec8052bade
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2314,6 +2314,14 @@ union bpf_attr {
  *             current value is ect (ECN capable). Works with IPv6 and IPv4.
  *     Return
  *             1 if set, 0 if not set.
+ *
+ * struct bpf_sock *bpf_get_listener_sock(struct bpf_sock *sk)
+ *	Description
+ *		Return a **struct bpf_sock** pointer in TCP_LISTEN state.
+ *		bpf_sk_release() is unnecessary and not allowed.
+ *	Return
+ *		A **struct bpf_sock** pointer on success, or NULL in
+ *		case of failure.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
