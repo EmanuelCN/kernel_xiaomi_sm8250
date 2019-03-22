6ff11edb38c82be2ed507c5c610da11d1687facc
Lorenz Bauer <lmb@cloudflare.com>
2019-03-22 09:54:02 +0800

    BACKPORT: bpf: add helper to check for a valid SYN cookie
    Using bpf_skc_lookup_tcp it's possible to ascertain whether a packet
    belongs to a known connection. However, there is one corner case: no
    sockets are created if SYN cookies are active. This means that the final
    ACK in the 3WHS is misclassified.
    
    Using the helper, we can look up the listening socket via
    bpf_skc_lookup_tcp and then check whether a packet is a valid SYN
    cookie ACK.
    
    Change-Id: If6df241e53af7fe53f842932fdcfd5afcc5aefd6
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2608,6 +2608,21 @@ union bpf_attr {
  *
  *	Return
  *		0 on success, or a negative erro in case of failure.
+ *
+ * int bpf_tcp_check_syncookie(struct bpf_sock *sk, void *iph, u32 iph_len, struct tcphdr *th, u32 th_len)
+ * 	Description
+ * 		Check whether iph and th contain a valid SYN cookie ACK for
+ * 		the listening socket in sk.
+ *
+ * 		iph points to the start of the IPv4 or IPv6 header, while
+ * 		iph_len contains sizeof(struct iphdr) or sizeof(struct ip6hdr).
+ *
+ * 		th points to the start of the TCP header, while th_len contains
+ * 		sizeof(struct tcphdr).
+ *
+ * 	Return
+ * 		0 if iph and th are a valid SYN cookie ACK, or a negative error
+ * 		otherwise.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
