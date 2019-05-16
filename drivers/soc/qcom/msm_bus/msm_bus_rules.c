ae7fb943acbd76b0eef3dcba9369460d16b9066b
Park Ju Hyung <qkrwngud825@gmail.com>
2019-05-16 22:02:35 +0900

    soc: qcom: msm_bus: Remove events tracing
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_rules.c
+++ drivers/soc/qcom/msm_bus/msm_bus_rules.c
@@ -9,8 +9,6 @@
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/msm-bus.h>
-#define CREATE_TRACE_POINTS
-#include <trace/events/trace_msm_bus.h>
 
 struct node_vote_info {
 	int id;
@@ -208,11 +206,6 @@ static void match_rule(struct rule_update_path_info *inp_node,
 				continue;
 
 			if (check_rule(rule, inp_node)) {
-				trace_bus_rules_matches(
-				(node->cur_rule ?
-					node->cur_rule->rule_id : -1),
-				inp_node->id, inp_node->ab,
-				inp_node->ib, inp_node->clk);
 				if (rule->state ==
 					RULE_STATE_NOT_APPLIED)
 					rule->state_change = true;
