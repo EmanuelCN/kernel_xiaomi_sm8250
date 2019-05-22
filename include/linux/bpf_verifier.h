15ff539b594c3fe2ab3257cdd1be26cdb4438837
Alexei Starovoitov <ast@kernel.org>
2019-05-21 20:17:06 -0700

    UPSTREAM: bpf: split explored_states
    split explored_states into prune_point boolean mark
    and link list of explored states.
    This removes STATE_LIST_MARK hack and allows marks to be separate from states.
    
    Change-Id: I974649a944b309ff4459b3b40f6485e18b69128e
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -241,6 +241,7 @@ struct bpf_insn_aux_data {
 	bool seen; /* this insn was processed by the verifier */
 	bool sanitize_stack_spill; /* subject to Spectre v4 sanitation */
 	u8 alu_state; /* used in combination with alu_limit */
+	bool prune_point;
 	unsigned int orig_idx; /* original instruction index */
 };
 
