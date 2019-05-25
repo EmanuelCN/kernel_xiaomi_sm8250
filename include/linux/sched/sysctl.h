4cef3e224ba2ee06132634b34377bae522f3b997
Wei Wang <wvw@google.com>
2019-05-24 19:18:36 -0700

    Revert "sched: Improve the scheduler"
    This reverts commit a3dd94a1bb80ec98924070f28ba80d93a4d559a6.
    
    Bug: 133481659
    Bug: 144961676
    Test: build
    Change-Id: Ib23609315f3446223521612621fe54469537c172
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Jimmy Shiu <jimmyshiu@google.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -159,8 +159,8 @@ extern int sysctl_schedstats(struct ctl_table *table, int write,
 				 loff_t *ppos);
 #define LIB_PATH_LENGTH 512
 extern char sched_lib_name[LIB_PATH_LENGTH];
+extern unsigned int sched_lib_mask_check;
 extern unsigned int sched_lib_mask_force;
-extern bool is_sched_lib_based_app(pid_t pid);
 
 #if defined(CONFIG_ENERGY_MODEL) && defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL)
 extern unsigned int sysctl_sched_energy_aware;
