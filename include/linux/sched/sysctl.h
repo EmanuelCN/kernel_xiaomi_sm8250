653daf1c371d07be476f75eda0d6ae8097de9168
Wei Wang <wvw@google.com>
2019-05-24 19:24:34 -0700

    Revert "sched/core: fix userspace affining threads incorrectly"
    This reverts commit d43b69c4ad2a977406c84d47fe8a5261e0099e78.
    
    Bug: 133481659
    Bug: 144961676
    Test: build
    Change-Id: I615023c611c4de1eb334e4374af7306991f4216b
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Jimmy Shiu <jimmyshiu@google.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -158,11 +158,6 @@ extern int sysctl_schedstats(struct ctl_table *table, int write,
 				 void __user *buffer, size_t *lenp,
 				 loff_t *ppos);
 
-#define LIB_PATH_LENGTH 512
-extern char sched_lib_name[LIB_PATH_LENGTH];
-extern unsigned int sched_lib_mask_check;
-extern unsigned int sched_lib_mask_force;
-
 #if defined(CONFIG_ENERGY_MODEL) && defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL)
 extern unsigned int sysctl_sched_energy_aware;
 extern int sched_energy_aware_handler(struct ctl_table *table, int write,
