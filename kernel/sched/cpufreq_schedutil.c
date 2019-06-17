c0a9d4a2e05ceed7a0f036b19487e5bfa351d632
Vincent Guittot <vincent.guittot@linaro.org>
2019-06-17 17:00:17 +0200

    sched/topology: Remove 'sd' from arch_scale_cpu_capacity()
    The 'struct sched_domain *sd' parameter to arch_scale_cpu_capacity() is
    unused since commit:
    
      765d0af19f5f ("sched/topology: Remove the ::smt_gain field from 'struct sched_domain'")
    
    Remove it.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Change-Id: I59d3c5fccf5c6c994e026f52ac2254cc0575c0b8
    [jaysonedson: Also removed from arch_scale_max_freq_capacity]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    [Ayrton: expand to cpufreq_schedutil.c and remove NULL group from arch_scale_cpu_capacity]
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -349,7 +349,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 static unsigned long sugov_get_util(struct sugov_cpu *sg_cpu)
 {
 	struct rq *rq = cpu_rq(sg_cpu->cpu);
-	unsigned long max = arch_scale_cpu_capacity(NULL, sg_cpu->cpu);
+	unsigned long max = arch_scale_cpu_capacity(sg_cpu->cpu);
 
 	sg_cpu->max = max;
 	sg_cpu->bw_dl = cpu_bw_dl(rq);
@@ -367,7 +367,7 @@ static unsigned long sugov_get_util(struct sugov_cpu *sg_cpu)
 	unsigned long util_cfs = cpu_util_freq(sg_cpu->cpu, NULL)
 				- cpu_util_rt(rq);
 #endif
-	unsigned long max = arch_scale_cpu_capacity(NULL, sg_cpu->cpu);
+	unsigned long max = arch_scale_cpu_capacity(sg_cpu->cpu);
 
 	sg_cpu->max = max;
 	sg_cpu->bw_dl = cpu_bw_dl(rq);
