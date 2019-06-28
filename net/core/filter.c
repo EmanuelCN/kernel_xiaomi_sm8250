6c66fd08b0725c6c6a378183a94106c6ccbeb53e
Toke Høiland-Jørgensen <toke@redhat.com>
2019-06-28 11:12:34 +0200

    UPSTREAM: devmap/cpumap: Use flush list instead of bitmap
    The socket map uses a linked list instead of a bitmap to keep track of
    which entries to flush. Do the same for devmap and cpumap, as this means we
    don't have to care about the map index when enqueueing things into the
    map (and so we can cache the map lookup).
    
    Change-Id: I0cf9483ddfbad1555c6314a4a3bd7d82c77333ff
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3150,7 +3150,6 @@ static int __bpf_tx_xdp_map(struct net_device *dev_rx, void *fwd,
 		err = dev_map_enqueue(dst, xdp, dev_rx);
 		if (err)
 			return err;
-		__dev_map_insert_ctx(map, index);
 		break;
 	}
 	case BPF_MAP_TYPE_CPUMAP: {
@@ -3159,7 +3158,6 @@ static int __bpf_tx_xdp_map(struct net_device *dev_rx, void *fwd,
 		err = cpu_map_enqueue(rcpu, xdp, dev_rx);
 		if (err)
 			return err;
-		__cpu_map_insert_ctx(map, index);
 		break;
 	}
 	case BPF_MAP_TYPE_XSKMAP: {
