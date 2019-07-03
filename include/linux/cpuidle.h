7fad1a5e86a9239727fd87a6ff248c2178b15338
Marcelo Tosatti <mtosatti@redhat.com>
2019-07-03 20:51:27 -0300

    UPSTREAM: governors: unify last_state_idx
    Since this field is shared by all governors, move it to
    cpuidle device structure.
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 7d4daeedd575bbc3c40c87fc6708a8b88c50fe7e
         git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    Signed-off-by: Ikjoon Jang <ikjn@chromium.org>
    
    Change-Id: I52d1f9190492c799cb5299601afbfc7b2a51d325

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -89,6 +89,7 @@ struct cpuidle_device {
 	unsigned int		poll_time_limit:1;
 	unsigned int		cpu;
 
+	int			last_state_idx;
 	int			last_residency;
 	struct cpuidle_state_usage	states_usage[CPUIDLE_STATE_MAX];
 	struct cpuidle_state_kobj *kobjs[CPUIDLE_STATE_MAX];
