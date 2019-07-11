c294f3ad9293dd051f118cc9ea6a78a91382e4b5
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-12 00:53:22 +0900

    msm: kgsl: Don't allocate memory dynamically for temp command buffers
    The temporary command buffer in _set_pagetable_gpu is only the size of a
    single page, and _set_pagetable_gpu is never executed concurrently. It
    is therefore easy to replace the dynamic command buffer allocation with
    a static one to improve performance by avoiding the latency of dynamic
    memory allocation.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- drivers/gpu/msm/adreno_iommu.c
+++ drivers/gpu/msm/adreno_iommu.c
@@ -408,19 +408,15 @@ static int _set_ctxt_gpu(struct adreno_ringbuffer *rb,
 static int _set_pagetable_gpu(struct adreno_ringbuffer *rb,
 			struct kgsl_pagetable *new_pt)
 {
+	static unsigned int link[PAGE_SIZE / sizeof(unsigned int)]
+		____cacheline_aligned_in_smp;
 	struct adreno_device *adreno_dev = ADRENO_RB_DEVICE(rb);
-	unsigned int *link = NULL, count;
+	unsigned int count;
 	int result;
 
-	link = kmalloc(PAGE_SIZE, GFP_KERNEL);
-	if (link == NULL)
-		return -ENOMEM;
-
 	/* If we are in a fault the MMU will be reset soon */
-	if (test_bit(ADRENO_DEVICE_FAULT, &adreno_dev->priv)) {
-		kfree(link);
+	if (test_bit(ADRENO_DEVICE_FAULT, &adreno_dev->priv))
 		return 0;
-	}
 
 	count = adreno_iommu_set_pt_generate_cmds(rb, link, new_pt);
 
@@ -434,7 +430,6 @@ static int _set_pagetable_gpu(struct adreno_ringbuffer *rb,
 	result = adreno_ringbuffer_issue_internal_cmds(rb,
 			KGSL_CMD_FLAGS_PMODE, link, count);
 
-	kfree(link);
 	return result;
 }
 
