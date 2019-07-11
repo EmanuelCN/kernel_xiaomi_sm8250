ff03baec1ce601387f9218193e9fba2204d90b7e
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:53:22 +0900

    msm: kgsl: use kmem_cache pool for draw objects
    These get allocated and freed millions of times on Android.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/gpu/msm/kgsl_drawobj.c
+++ drivers/gpu/msm/kgsl_drawobj.c
@@ -35,6 +35,9 @@
  */
 static struct kmem_cache *memobjs_cache;
 static struct kmem_cache *sparseobjs_cache;
+static struct kmem_cache *timeline_cache;
+static struct kmem_cache *sync_cache;
+static struct kmem_cache *cmd_cache;
 
 static void syncobj_destroy_object(struct kgsl_drawobj *drawobj)
 {
@@ -58,22 +61,22 @@ static void syncobj_destroy_object(struct kgsl_drawobj *drawobj)
 	}
 
 	kfree(syncobj->synclist);
-	kfree(syncobj);
+	kmem_cache_free(sync_cache, syncobj);
 }
 
 static void cmdobj_destroy_object(struct kgsl_drawobj *drawobj)
 {
-	kfree(CMDOBJ(drawobj));
+	kmem_cache_free(cmd_cache, CMDOBJ(drawobj));
 }
 
 static void timelineobj_destroy_object(struct kgsl_drawobj *drawobj)
 {
-	kfree(TIMELINEOBJ(drawobj));
+	kmem_cache_free(timeline_cache, TIMELINEOBJ(drawobj));
 }
 
 static void sparseobj_destroy_object(struct kgsl_drawobj *drawobj)
 {
-	kfree(SPARSEOBJ(drawobj));
+	kmem_cache_free(cmd_cache, SPARSEOBJ(drawobj));
 }
 
 void kgsl_drawobj_destroy_object(struct kref *kref)
@@ -842,7 +845,7 @@ kgsl_drawobj_timeline_create(struct kgsl_device *device,
 {
 	int ret;
 	struct kgsl_drawobj_timeline *timelineobj =
-		kzalloc(sizeof(*timelineobj), GFP_KERNEL);
+		kmem_cache_zalloc(timeline_cache, GFP_KERNEL);
 
 	if (!timelineobj)
 		return ERR_PTR(-ENOMEM);
@@ -850,7 +853,7 @@ kgsl_drawobj_timeline_create(struct kgsl_device *device,
 	ret = drawobj_init(device, context, &timelineobj->base,
 		TIMELINEOBJ_TYPE);
 	if (ret) {
-		kfree(timelineobj);
+		kmem_cache_free(timeline_cache, timelineobj);
 		return ERR_PTR(ret);
 	}
 
@@ -967,7 +970,7 @@ struct kgsl_drawobj_sync *kgsl_drawobj_sync_create(struct kgsl_device *device,
 		struct kgsl_context *context)
 {
 	struct kgsl_drawobj_sync *syncobj =
-		kzalloc(sizeof(*syncobj), GFP_KERNEL);
+		kmem_cache_zalloc(sync_cache, GFP_KERNEL);
 	int ret;
 
 	if (!syncobj)
@@ -975,7 +978,7 @@ struct kgsl_drawobj_sync *kgsl_drawobj_sync_create(struct kgsl_device *device,
 
 	ret = drawobj_init(device, context, &syncobj->base, SYNCOBJ_TYPE);
 	if (ret) {
-		kfree(syncobj);
+		kmem_cache_free(sync_cache, syncobj);
 		return ERR_PTR(ret);
 	}
 
@@ -1001,7 +1004,8 @@ struct kgsl_drawobj_cmd *kgsl_drawobj_cmd_create(struct kgsl_device *device,
 		struct kgsl_context *context, unsigned int flags,
 		unsigned int type)
 {
-	struct kgsl_drawobj_cmd *cmdobj = kzalloc(sizeof(*cmdobj), GFP_KERNEL);
+	struct kgsl_drawobj_cmd *cmdobj =
+		kmem_cache_zalloc(cmd_cache, GFP_KERNEL);
 	int ret;
 
 	if (!cmdobj)
@@ -1010,7 +1014,7 @@ struct kgsl_drawobj_cmd *kgsl_drawobj_cmd_create(struct kgsl_device *device,
 	ret = drawobj_init(device, context, &cmdobj->base,
 		(type & (CMDOBJ_TYPE | MARKEROBJ_TYPE)));
 	if (ret) {
-		kfree(cmdobj);
+		kmem_cache_free(cmd_cache, cmdobj);
 		return ERR_PTR(ret);
 	}
 
@@ -1441,14 +1445,21 @@ void kgsl_drawobjs_cache_exit(void)
 {
 	kmem_cache_destroy(memobjs_cache);
 	kmem_cache_destroy(sparseobjs_cache);
+	kmem_cache_destroy(timeline_cache);
+	kmem_cache_destroy(sync_cache);
+	kmem_cache_destroy(cmd_cache);
 }
 
 int kgsl_drawobjs_cache_init(void)
 {
-	memobjs_cache = KMEM_CACHE(kgsl_memobj_node, 0);
-	sparseobjs_cache = KMEM_CACHE(kgsl_sparseobj_node, 0);
 
-	if (!memobjs_cache || !sparseobjs_cache)
+        memobjs_cache = KMEM_CACHE(kgsl_memobj_node, SLAB_HWCACHE_ALIGN);
+        timeline_cache = KMEM_CACHE(kgsl_drawobj_timeline, SLAB_HWCACHE_ALIGN);
+        sync_cache = KMEM_CACHE(kgsl_drawobj_sync, SLAB_HWCACHE_ALIGN);
+        cmd_cache = KMEM_CACHE(kgsl_drawobj_cmd, SLAB_HWCACHE_ALIGN);
+	sparseobjs_cache = KMEM_CACHE(kgsl_sparseobj_node, SLAB_HWCACHE_ALIGN);
+
+	if (!memobjs_cache || !timeline_cache || !sync_cache || !cmd_cache || !sparseobjs_cache)
 		return -ENOMEM;
 
 	return 0;
