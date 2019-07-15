4cc5c88bb1334404bcc8a9bff3ac854321c6d40a
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-07-15 13:03:20 +0200

    PCI: irq: Introduce rearm_wake_irq()
    Introduce a new function, rearm_wake_irq(), allowing a wakeup IRQ
    to be armed for systen wakeup detection again without running any
    action handlers associated with it after it has been armed for
    wakeup detection and triggered.
    
    That is useful for IRQs, like ACPI SCI, that may deliver wakeup
    as well as non-wakeup interrupts when armed for systen wakeup
    detection.  In those cases, it may be possible to determine whether
    or not the delivered interrupt is a systen wakeup one without
    running the entire action handler (or handlers, if the IRQ is
    shared) for the IRQ, and if the interrupt turns out to be a
    non-wakeup one, the IRQ can be rearmed with the help of the
    new function.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -247,6 +247,7 @@ extern void suspend_device_irqs(void);
 extern void resume_device_irqs(void);
 extern void unaffine_perf_irqs(void);
 extern void reaffine_perf_irqs(bool from_hotplug);
+extern void rearm_wake_irq(unsigned int irq);
 
 /**
  * struct irq_affinity_notify - context for notification of IRQ affinity changes
