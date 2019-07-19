d977a2e30c1e8f234ca25a05f65c54a8b4ecdc26
freak07 <michalechner92@googlemail.com>
2019-07-19 10:45:51 +0200

    platform: ipa: Use power efficient workingqueues

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -2934,7 +2934,8 @@ static void tethering_stats_poll_queue(struct work_struct *work)
 
 	/* Schedule again only if there's an active polling interval */
 	if (ipa3_rmnet_ctx.polling_interval != 0)
-		schedule_delayed_work(&ipa_tether_stats_poll_wakequeue_work,
+		queue_delayed_work(system_power_efficient_wq, 
+			&ipa_tether_stats_poll_wakequeue_work,
 			msecs_to_jiffies(ipa3_rmnet_ctx.polling_interval*1000));
 }
 
@@ -3026,7 +3027,8 @@ int rmnet_ipa3_poll_tethering_stats(struct wan_ioctl_poll_tethering_stats *data)
 		return 0;
 	}
 
-	schedule_delayed_work(&ipa_tether_stats_poll_wakequeue_work, 0);
+	queue_delayed_work(system_power_efficient_wq, 
+						&ipa_tether_stats_poll_wakequeue_work, 0);
 	return 0;
 }
 
