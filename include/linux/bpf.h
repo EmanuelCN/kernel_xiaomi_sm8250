383ef1d438eda6c81e009e70a41515cccacd1bc9
Toke Høiland-Jørgensen <toke@redhat.com>
2019-07-26 18:06:52 +0200

    UPSTREAM: include/bpf.h: Remove map_insert_ctx() stubs
    When we changed the device and CPU maps to use linked lists instead of
    bitmaps, we also removed the need for the map_insert_ctx() helpers to keep
    track of the bitmaps inside each map. However, it seems I forgot to remove
    the function definitions stubs, so remove those here.
    
    Change-Id: I2ed6b2a2583472a52da1056b9a7c1aeec7cba88f
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Yonghong Song <yhs@fb.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -722,7 +722,6 @@ struct xdp_buff;
 struct sk_buff;
 
 struct bpf_dtab_netdev *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
-void __dev_map_insert_ctx(struct bpf_map *map, u32 index);
 void __dev_map_flush(struct bpf_map *map);
 int dev_map_enqueue(struct bpf_dtab_netdev *dst, struct xdp_buff *xdp,
 		    struct net_device *dev_rx);
@@ -730,7 +729,6 @@ int dev_map_generic_redirect(struct bpf_dtab_netdev *dst, struct sk_buff *skb,
 			     struct bpf_prog *xdp_prog);
 
 struct bpf_cpu_map_entry *__cpu_map_lookup_elem(struct bpf_map *map, u32 key);
-void __cpu_map_insert_ctx(struct bpf_map *map, u32 index);
 void __cpu_map_flush(struct bpf_map *map);
 int cpu_map_enqueue(struct bpf_cpu_map_entry *rcpu, struct xdp_buff *xdp,
 		    struct net_device *dev_rx);
@@ -815,10 +813,6 @@ static inline struct net_device  *__dev_map_lookup_elem(struct bpf_map *map,
 	return NULL;
 }
 
-static inline void __dev_map_insert_ctx(struct bpf_map *map, u32 index)
-{
-}
-
 static inline void __dev_map_flush(struct bpf_map *map)
 {
 }
@@ -846,10 +840,6 @@ static inline
 struct bpf_cpu_map_entry *__cpu_map_lookup_elem(struct bpf_map *map, u32 key)
 {
 	return NULL;
-}
-
-static inline void __cpu_map_insert_ctx(struct bpf_map *map, u32 index)
-{
 }
 
 static inline void __cpu_map_flush(struct bpf_map *map)
