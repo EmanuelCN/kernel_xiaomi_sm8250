47f3d59612c766de78e34412903cc3e747d921b0
Danny Lin <danny@kdrag0n.dev>
2019-08-04 03:40:30 +0000

    exec: Add node tampering blacklist function
    We'll be adding checks to block writes from processes which tamper with
    values that we control from within the kernel, especially ones that
    userspace writes to for boosting. Add a central function to perform the
    process check to reduce code duplication.
    
    This blacklists the following processes which are known to tamper with
    such values:
      - init
      - libperfmgr (power@1.3-servi and NodeLooperThrea)
      - perfd (perf@1.0-servic)
      - init.qcom.post_boot.sh (init.qcom.post_)
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -152,4 +152,15 @@ extern int do_execveat(int, struct filename *,
 		       int);
 int do_execve_file(struct file *file, void *__argv, void *__envp);
 
+static inline bool task_is_booster(struct task_struct *tsk)
+{
+	char comm[sizeof(tsk->comm)];
+
+	get_task_comm(comm, tsk);
+	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+	       !strcmp(comm, "power@1.3-servi") ||
+	       !strcmp(comm, "perf@1.0-servic") ||
+	       !strcmp(comm, "init.qcom.post_");
+}
+
 #endif /* _LINUX_BINFMTS_H */
