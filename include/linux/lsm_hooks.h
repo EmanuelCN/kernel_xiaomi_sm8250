846c378d426988d66f871c52435129264c338f89
Matthew Garrett <matthewgarrett@google.com>
2019-08-19 17:17:38 -0700

    UPSTREAM: security: Add a "locked down" LSM hook
    Add a mechanism to allow LSMs to make a policy decision around whether
    kernel functionality that would allow tampering with or examining the
    runtime state of the kernel should be permitted.
    
    Change-Id: Ie70dc489691e2f8413fe593c633a00e6e97bd63a
    Signed-off-by: Matthew Garrett <mjg59@google.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1426,6 +1426,11 @@
  * @bpf_prog_free_security:
  *	Clean up the security information stored inside bpf prog.
  *
+ * @locked_down
+ *     Determine whether a kernel feature that potentially enables arbitrary
+ *     code execution in kernel space should be permitted.
+ *
+ *     @what: kernel feature being accessed
  */
 union security_list_options {
 	int (*binder_set_context_mgr)(const struct cred *mgr);
@@ -1789,6 +1794,7 @@ union security_list_options {
 	int (*perf_event_write)(struct perf_event *event);
 
 #endif
+	int (*locked_down)(enum lockdown_reason what);
 };
 
 struct security_hook_heads {
@@ -2030,6 +2036,7 @@ struct security_hook_heads {
 	struct hlist_head perf_event_read;
 	struct hlist_head perf_event_write;
 #endif
+	struct hlist_head locked_down;
 } __randomize_layout;
 
 /*
