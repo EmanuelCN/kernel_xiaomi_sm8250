e1f928d2515282f7229ac0c09d532b4aa40e56ea
Ben Dooks <ben.dooks@codethink.co.uk>
2019-10-15 14:48:22 +0100

    rcu: Move rcu_{expedited,normal} definitions into rcupdate.h
    This commit moves the rcu_{expedited,normal} definitions from
    kernel/rcu/update.c to include/linux/rcupdate.h to make sure they are
    in sync, and also to avoid the following warning from sparse:
    
    kernel/ksysfs.c:150:5: warning: symbol 'rcu_expedited' was not declared. Should it be static?
    kernel/ksysfs.c:167:5: warning: symbol 'rcu_normal' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -904,7 +904,6 @@ rcu_head_after_call_rcu(struct rcu_head *rhp, rcu_callback_t f)
 	return false;
 }
 
-
 /* Transitional pre-consolidation compatibility definitions. */
 
 static inline void synchronize_rcu_bh(void)
@@ -956,5 +955,9 @@ static inline void cond_synchronize_sched(unsigned long oldstate)
 {
 	cond_synchronize_rcu(oldstate);
 }
+
+/* kernel/ksysfs.c definitions */
+extern int rcu_expedited;
+extern int rcu_normal;
 
 #endif /* __LINUX_RCUPDATE_H */
