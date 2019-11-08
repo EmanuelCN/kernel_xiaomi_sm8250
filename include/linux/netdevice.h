f3b2f901455b1c77047952ce55a024760d4dec5f
Eric Dumazet <edumazet@google.com>
2019-11-07 16:27:14 -0800

    UPSTREAM: net: provide dev_lstats_read() helper
    Many network drivers use hand-coded implementation of the same thing,
    let's factorize things so that u64_stats_t adoption is done once.
    
    Change-Id: I694032b38d89421892aec589291421b8ed82342b
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2462,6 +2462,8 @@ static inline void dev_lstats_add(struct net_device *dev, unsigned int len)
 	u64_stats_update_end(&lstats->syncp);
 }
 
+void dev_lstats_read(struct net_device *dev, u64 *packets, u64 *bytes);
+
 #define __netdev_alloc_pcpu_stats(type, gfp)				\
 ({									\
 	typeof(type) __percpu *pcpu_stats = alloc_percpu_gfp(type, gfp);\
