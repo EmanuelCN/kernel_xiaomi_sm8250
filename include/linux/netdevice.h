402b43b89582b9654555112afe019abfed65a4fa
Eric Dumazet <edumazet@google.com>
2019-11-07 16:27:15 -0800

    UPSTREAM: net: provide dev_lstats_add() helper
    Many network drivers need it and hand-coded the same function.
    
    In order to ease u64_stats_t adoption, it is time to factorize.
    
    Change-Id: I31489e072d0a26d8099544576f662b2531caf5d3
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2452,6 +2452,16 @@ struct pcpu_sw_netstats {
 	struct u64_stats_sync   syncp;
 };
 
+static inline void dev_lstats_add(struct net_device *dev, unsigned int len)
+{
+	struct pcpu_lstats *lstats = this_cpu_ptr(dev->lstats);
+
+	u64_stats_update_begin(&lstats->syncp);
+	lstats->bytes += len;
+	lstats->packets++;
+	u64_stats_update_end(&lstats->syncp);
+}
+
 #define __netdev_alloc_pcpu_stats(type, gfp)				\
 ({									\
 	typeof(type) __percpu *pcpu_stats = alloc_percpu_gfp(type, gfp);\
