81581e7eb389a9b04d6bf20d4efe79f6f2aadb94
Adam W. Willis <return.of.octobot@gmail.com>
2019-11-28 13:45:53 +0100

    mm: Reduce VM_MAX_READAHEAD to 128KB and apply init protection
    This reverts commit 6847a92 ("mm: change max readahead size to 512KB")
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -106,6 +106,9 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	if (ret < 0)
 		return ret;
 
+	if (!strcmp(current->comm, "init"))
+		ra_kb = VM_MAX_READAHEAD;
+
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
 
 	return ret;
