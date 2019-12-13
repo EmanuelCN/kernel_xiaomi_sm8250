7d84e2401ae9006caab36bb446bb8ecc92293b24
Stephen Smalley <sds@tycho.nsa.gov>
2019-12-13 15:28:38 -0500

    UPSTREAM: selinux: randomize layout of key structures
    Randomize the layout of key selinux data structures.
    Initially this is applied to the selinux_state, selinux_ss,
    policydb, and task_security_struct data structures.
    
    NB To test/use this mechanism, one must install the
    necessary build-time dependencies, e.g. gcc-plugin-devel on Fedora,
    and enable CONFIG_GCC_PLUGIN_RANDSTRUCT in the kernel configuration.
    
    Change-Id: I3f17abfda5e93d01103a3fb30cb5b8aefe60781c
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    [PM: double semi-colon fixed]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -109,7 +109,7 @@ struct selinux_state {
 
 	struct selinux_avc *avc;
 	struct selinux_ss *ss;
-};
+} __randomize_layout;
 
 void selinux_ss_init(struct selinux_ss **ss);
 void selinux_avc_init(struct selinux_avc **avc);
