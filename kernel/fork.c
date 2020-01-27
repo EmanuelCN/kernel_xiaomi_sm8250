726037345ec1fb5b81d7881815a750169d30e6ee
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-01-28 00:44:05 +0530

    devfreq_boost: Add support for boosting CPU_LLCC_BW device
    Signed-off-by: Official-Ayrton990 <15030201@itcelaya.edu.mx>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2369,6 +2369,7 @@ long _do_fork(unsigned long clone_flags,
 
 	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 50);
 	}
 
 	/*
