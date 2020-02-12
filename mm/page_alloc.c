2aec05084f45e13de5c7167813d4e8e862154b5d
Martin Liu <liumartin@google.com>
2020-02-12 23:21:10 +0800

    Revert "psi: Add emergency trigger"
    This reverts commit a7517e81e239ef81d2d7e2cc416b65d0042a52c8.
    
    Reason: revert non upstream code
    Bug: 148903434
    Test: boot
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I17ef7ebfcf9cbee16e4b771cb5aa75bbaf11b4a1
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4664,6 +4664,9 @@ retry:
 	if (costly_order && !(gfp_mask & __GFP_RETRY_MAYFAIL))
 		goto nopage;
 
+	if (order <= PAGE_ALLOC_COSTLY_ORDER && should_ulmk_retry())
+		goto retry;
+
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
 		goto retry;
@@ -4681,8 +4684,6 @@ retry:
 				&compaction_retries))
 		goto retry;
 
-	if (order <= PAGE_ALLOC_COSTLY_ORDER && should_ulmk_retry())
-		goto retry;
 
 	/*
 	 * Deal with possible cpuset update races or zonelist updates to avoid
