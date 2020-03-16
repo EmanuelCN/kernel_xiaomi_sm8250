72c485d91b711fcd9268ad0cb06d7eac3d91dcb0
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-16 20:48:24 +0900

    vbswap: pass struct bio_vec as a pointer
    This object is referenced only for reading
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/block/vbswap/vbswap.c
+++ drivers/block/vbswap/vbswap.c
@@ -53,7 +53,7 @@ static void vbswap_init_disksize(u64 disksize)
 	vbswap_initialized = 1;
 }
 
-static int vbswap_bvec_read(struct bio_vec bvec,
+static int vbswap_bvec_read(struct bio_vec *bvec,
 			    u32 index, struct bio *bio)
 {
 	struct page *page;
@@ -64,11 +64,11 @@ static int vbswap_bvec_read(struct bio_vec bvec,
 		return -EIO;
 	}
 
-	page = bvec.bv_page;
+	page = bvec->bv_page;
 
 	user_mem = kmap_atomic(page);
 	swap_header_page_mem = kmap_atomic(swap_header_page);
-	memcpy(user_mem, swap_header_page_mem, bvec.bv_len);
+	memcpy(user_mem, swap_header_page_mem, bvec->bv_len);
 	kunmap_atomic(swap_header_page_mem);
 	kunmap_atomic(user_mem);
 	flush_dcache_page(page);
@@ -76,7 +76,7 @@ static int vbswap_bvec_read(struct bio_vec bvec,
 	return 0;
 }
 
-static int vbswap_bvec_write(struct bio_vec bvec,
+static int vbswap_bvec_write(struct bio_vec *bvec,
 			     u32 index, struct bio *bio)
 {
 	struct page *page;
@@ -87,7 +87,7 @@ static int vbswap_bvec_write(struct bio_vec bvec,
 		return -EIO;
 	}
 
-	page = bvec.bv_page;
+	page = bvec->bv_page;
 
 	user_mem = kmap_atomic(page);
 	swap_header_page_mem = kmap_atomic(swap_header_page);
@@ -98,7 +98,7 @@ static int vbswap_bvec_write(struct bio_vec bvec,
 	return 0;
 }
 
-static int vbswap_bvec_rw(struct bio_vec bvec,
+static int vbswap_bvec_rw(struct bio_vec *bvec,
 			  u32 index, struct bio *bio, int rw)
 {
 	int ret;
@@ -165,7 +165,7 @@ static void __vbswap_make_request(struct bio *bio, int rw)
 			 "(%d, %d, %d)\n",
 			 __func__, __LINE__, rw, index, bvec.bv_len);
 
-		ret = vbswap_bvec_rw(bvec, index, bio, rw);
+		ret = vbswap_bvec_rw(&bvec, index, bio, rw);
 		if (ret < 0) {
 			if (ret != -ENOSPC)
 				pr_err("%s %d: vbswap_bvec_rw failed."
