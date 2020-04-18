4c88f02af0198164c5c62e834c3c0eb5f65bb1f8
Rick Yiu <rickyiu@google.com>
2020-04-18 15:36:24 +0800

    sched: separate capacity margin for boosted tasks
    With the introduction of placement hint patch, boosted tasks will not
    scheduled from big cores. We tune capacity margin to let important
    boosted tasks get scheduled on big cores. However, the capacity margin
    affects all group of tasks, so that non-boosted tasks get more chances
    to be scheduled on big cores, too. This could be solved by separating
    capacity margin for boosted tasks.
    
    Bug: 152925197
    Test: margin set correctly
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I0e059c56efa9bc8513f0ef4b0f6ab8f5d04a592a
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -24,6 +24,10 @@
 
 #include <trace/events/sched.h>
 
+#ifdef CONFIG_SMP
+static inline unsigned long boosted_task_util(struct task_struct *task);
+#endif
+
 /*
  * Targeted preemption latency for CPU-bound tasks:
  *
@@ -127,10 +131,16 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 #define fits_capacity(cap, max)	((cap) * 1280 < (max) * 1024)
 
-unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
-unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
+unsigned int sched_capacity_margin_up[CPU_NR] = {
+			[0 ... CPU_NR-1] = 1078}; /* ~5% margin */
+unsigned int sched_capacity_margin_down[CPU_NR] = {
+			[0 ... CPU_NR-1] = 1205}; /* ~15% margin */
+unsigned int sched_capacity_margin_up_boosted[CPU_NR] = {
+	3658, 3658, 3658, 3658, 3658, 3658, 1078, 1024
+}; /* 72% margin for small, 5% for big, 0% for big+ */
+unsigned int sched_capacity_margin_down_boosted[CPU_NR] = {
+	3658, 3658, 3658, 3658, 3658, 3658, 3658, 3658
+}; /* not used for small cores, 72% margin for big, 72% margin for big+ */
 
 unsigned int sched_small_task_threshold = 102;
 __read_mostly unsigned int sysctl_sched_force_lb_enable = 1;
@@ -3861,11 +3871,15 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	 * CPU.
 	 */
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
-		margin = sched_capacity_margin_down[cpu];
+		margin = schedtune_task_boost(p) > 0 ?
+			sched_capacity_margin_down_boosted[task_cpu(p)] :
+			sched_capacity_margin_down[task_cpu(p)];
 	else
-		margin = sched_capacity_margin_up[task_cpu(p)];
+		margin = schedtune_task_boost(p) > 0 ?
+			sched_capacity_margin_up_boosted[task_cpu(p)] :
+			sched_capacity_margin_up[task_cpu(p)];
 
-	return capacity * 1024 > uclamp_task_util(p) * margin;
+	return capacity * 1024 > uclamp_task(p) * margin;
 }
 
 static inline bool task_demand_fits(struct task_struct *p, int cpu)
