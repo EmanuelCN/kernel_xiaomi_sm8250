e0047f600f486b521adeb179a328b8366cf6b0f3
Yaroslav Furman <yaro330@gmail.com>
2020-04-28 17:44:34 +0300

    scsi: ufshcd: mark IRQ as performance critical
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -11508,7 +11508,7 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 	mb();
 
 	/* IRQ registration */
-	err = devm_request_irq(dev, irq, ufshcd_intr, IRQF_SHARED,
+	err = devm_request_irq(dev, irq, ufshcd_intr, IRQF_SHARED | IRQF_PERF_AFFINE,
 				dev_name(dev), hba);
 	if (err) {
 		dev_err(hba->dev, "request irq failed\n");
