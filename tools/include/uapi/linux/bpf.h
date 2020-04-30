99523f98ecd06d7970db9ccefd09abfeb78cd3ab
Stanislav Fomichev <sdf@google.com>
2020-04-30 16:31:52 -0700

    UPSTREAM: bpf: Bpf_{g,s}etsockopt for struct bpf_sock_addr
    Currently, bpf_getsockopt and bpf_setsockopt helpers operate on the
    'struct bpf_sock_ops' context in BPF_PROG_TYPE_SOCK_OPS program.
    Let's generalize them and make them available for 'struct bpf_sock_addr'.
    That way, in the future, we can allow those helpers in more places.
    
    As an example, let's expose those 'struct bpf_sock_addr' based helpers to
    BPF_CGROUP_INET{4,6}_CONNECT hooks. That way we can override CC before the
    connection is made.
    
    v3:
    * Expose custom helpers for bpf_sock_addr context instead of doing
      generic bpf_sock argument (as suggested by Daniel). Even with
      try_socket_lock that doesn't sleep we have a problem where context sk
      is already locked and socket lock is non-nestable.
    
    v2:
    * s/BPF_PROG_TYPE_CGROUP_SOCKOPT/BPF_PROG_TYPE_SOCK_OPS/
    
    Change-Id: Iddab2e590ad8927f2083b32a981c4dbbe7e25cc8
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200430233152.199403-1-sdf@google.com

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -1564,13 +1564,18 @@ union bpf_attr {
  * 	Return
  * 		0
  *
- * int bpf_setsockopt(struct bpf_sock_ops *bpf_socket, int level, int optname, char *optval, int optlen)
+ * int bpf_setsockopt(void *bpf_socket, int level, int optname, void *optval, int optlen)
  * 	Description
  * 		Emulate a call to **setsockopt()** on the socket associated to
  * 		*bpf_socket*, which must be a full socket. The *level* at
  * 		which the option resides and the name *optname* of the option
  * 		must be specified, see **setsockopt(2)** for more information.
  * 		The option value of length *optlen* is pointed by *optval*.
+ *
+ * 		*bpf_socket* should be one of the following:
+ * 		* **struct bpf_sock_ops** for **BPF_PROG_TYPE_SOCK_OPS**.
+ * 		* **struct bpf_sock_addr** for **BPF_CGROUP_INET4_CONNECT**
+ * 		  and **BPF_CGROUP_INET6_CONNECT**.
  *
  * 		This helper actually implements a subset of **setsockopt()**.
  * 		It supports the following *level*\ s:
@@ -1766,7 +1771,7 @@ union bpf_attr {
  * 	Return
  * 		0 on success, or a negative error in case of failure.
  *
- * int bpf_getsockopt(struct bpf_sock_ops *bpf_socket, int level, int optname, char *optval, int optlen)
+ * int bpf_getsockopt(void *bpf_socket, int level, int optname, void *optval, int optlen)
  * 	Description
  * 		Emulate a call to **getsockopt()** on the socket associated to
  * 		*bpf_socket*, which must be a full socket. The *level* at
@@ -1774,6 +1779,11 @@ union bpf_attr {
  * 		must be specified, see **getsockopt(2)** for more information.
  * 		The retrieved value is stored in the structure pointed by
  * 		*opval* and of length *optlen*.
+ *
+ * 		*bpf_socket* should be one of the following:
+ * 		* **struct bpf_sock_ops** for **BPF_PROG_TYPE_SOCK_OPS**.
+ * 		* **struct bpf_sock_addr** for **BPF_CGROUP_INET4_CONNECT**
+ * 		  and **BPF_CGROUP_INET6_CONNECT**.
  *
  * 		This helper actually implements a subset of **getsockopt()**.
  * 		It supports the following *level*\ s:
