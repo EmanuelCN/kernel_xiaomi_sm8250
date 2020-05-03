2a342bf8bf97fe90d8ab3b6cee51a3dc32c725cb
celtare21 <celtare21@gmail.com>
2020-05-03 15:17:39 +0200

    soc: qcom: Remove tracing
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_arb_adhoc.c
+++ drivers/soc/qcom/msm_bus/msm_bus_arb_adhoc.c
@@ -12,7 +12,6 @@
 #include <linux/msm-bus.h>
 #include "msm_bus_core.h"
 #include "msm_bus_adhoc.h"
-#include <trace/events/trace_msm_bus.h>
 
 #define NUM_CL_HANDLES	50
 #define NUM_LNODES	3
@@ -1146,8 +1145,6 @@ static int update_context(uint32_t cl, bool active_only,
 		goto exit_update_context;
 	}
 
-	trace_bus_update_request_end(pdata->name);
-
 exit_update_context:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
@@ -1208,8 +1205,6 @@ static int update_request_adhoc(uint32_t cl, unsigned int index)
 		pr_err("%s: Err updating path\n", __func__);
 		goto exit_update_request;
 	}
-
-	trace_bus_update_request_end(pdata->name);
 
 exit_update_request:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
@@ -1276,7 +1271,6 @@ static int update_bw_adhoc(struct msm_bus_client_handle *cl, u64 ab, u64 ib)
 
 	if (log_transaction)
 		getpath_debug(cl->mas, cl->first_hop, cl->active_only);
-	trace_bus_update_request_end(cl->name);
 exit_update_request:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 
@@ -1321,7 +1315,6 @@ static int update_bw_context(struct msm_bus_client_handle *cl, u64 act_ab,
 	cl->cur_act_ab = act_ab;
 	cl->cur_dual_ib = slp_ib;
 	cl->cur_dual_ab = slp_ab;
-	trace_bus_update_request_end(cl->name);
 exit_change_context:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
