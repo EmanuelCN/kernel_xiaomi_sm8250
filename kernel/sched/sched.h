fde2a0cb539f9f7e80167d8e79d571962abca53c
Luca Abeni <luca.abeni@santannapisa.it>
2020-05-20 15:42:42 +0200

    sched/deadline: Make DL capacity-aware
    The current SCHED_DEADLINE (DL) scheduler uses a global EDF scheduling
    algorithm w/o considering CPU capacity or task utilization.
    This works well on homogeneous systems where DL tasks are guaranteed
    to have a bounded tardiness but presents issues on heterogeneous
    systems.
    
    A DL task can migrate to a CPU which does not have enough CPU capacity
    to correctly serve the task (e.g. a task w/ 70ms runtime and 100ms
    period on a CPU w/ 512 capacity).
    
    Add the DL fitness function dl_task_fits_capacity() for DL admission
    control on heterogeneous systems. A task fits onto a CPU if:
    
        CPU original capacity / 1024 >= task runtime / task deadline
    
    Use this function on heterogeneous systems to try to find a CPU which
    meets this criterion during task wakeup, push and offline migration.
    
    On homogeneous systems the original behavior of the DL admission
    control should be retained.
    
    Signed-off-by: Luca Abeni <luca.abeni@santannapisa.it>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Link: https://lkml.kernel.org/r/20200520134243.19352-5-dietmar.eggemann@arm.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -402,6 +402,21 @@ static inline bool __dl_overflow(struct dl_bw *dl_b, unsigned long cap,
 	       cap_scale(dl_b->bw, cap) < dl_b->total_bw - old_bw + new_bw;
 }
 
+/*
+ * Verify the fitness of task @p to run on @cpu taking into account the
+ * CPU original capacity and the runtime/deadline ratio of the task.
+ *
+ * The function will return true if the CPU original capacity of the
+ * @cpu scaled by SCHED_CAPACITY_SCALE >= runtime/deadline ratio of the
+ * task and false otherwise.
+ */
+static inline bool dl_task_fits_capacity(struct task_struct *p, int cpu)
+{
+	unsigned long cap = arch_scale_cpu_capacity(cpu);
+
+	return cap_scale(p->dl.dl_deadline, cap) >= p->dl.dl_runtime;
+}
+
 extern void dl_change_utilization(struct task_struct *p, u64 new_bw);
 extern void init_dl_bw(struct dl_bw *dl_b);
 extern int  sched_dl_global_validate(void);
