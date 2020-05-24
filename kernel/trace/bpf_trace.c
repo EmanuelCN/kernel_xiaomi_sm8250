d33b094bd3cf789acbd0f600af17fcec446f724f
John Fastabend <john.fastabend@gmail.com>
2020-05-24 09:50:55 -0700

    UPSTREAM: bpf: Extend bpf_base_func_proto helpers with probe_* and *current_task*
    Often it is useful when applying policy to know something about the
    task. If the administrator has CAP_SYS_ADMIN rights then they can
    use kprobe + networking hook and link the two programs together to
    accomplish this. However, this is a bit clunky and also means we have
    to call both the network program and kprobe program when we could just
    use a single program and avoid passing metadata through sk_msg/skb->cb,
    socket, maps, etc.
    
    To accomplish this add probe_* helpers to bpf_base_func_proto programs
    guarded by a perfmon_capable() check. New supported helpers are the
    following,
    
     BPF_FUNC_get_current_task
     BPF_FUNC_probe_read_user
     BPF_FUNC_probe_read_kernel
     BPF_FUNC_probe_read_user_str
     BPF_FUNC_probe_read_kernel_str
    
    Change-Id: I1108328ac2b17a367d554861aa7f20f994147ce9
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/159033905529.12355.4368381069655254932.stgit@john-Precision-5820-Tower
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -152,7 +152,7 @@ BPF_CALL_3(bpf_probe_read_user, void *, dst, u32, size,
 	return ret;
 }
 
-static const struct bpf_func_proto bpf_probe_read_user_proto = {
+const struct bpf_func_proto bpf_probe_read_user_proto = {
 	.func		= bpf_probe_read_user,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
@@ -172,7 +172,7 @@ BPF_CALL_3(bpf_probe_read_user_str, void *, dst, u32, size,
 	return ret;
 }
 
-static const struct bpf_func_proto bpf_probe_read_user_str_proto = {
+const struct bpf_func_proto bpf_probe_read_user_str_proto = {
 	.func		= bpf_probe_read_user_str,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
@@ -200,7 +200,7 @@ BPF_CALL_3(bpf_probe_read_kernel, void *, dst, u32, size,
 	return bpf_probe_read_kernel_common(dst, size, unsafe_ptr, false);
 }
 
-static const struct bpf_func_proto bpf_probe_read_kernel_proto = {
+const struct bpf_func_proto bpf_probe_read_kernel_proto = {
 	.func		= bpf_probe_read_kernel,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
@@ -252,7 +252,7 @@ BPF_CALL_3(bpf_probe_read_kernel_str, void *, dst, u32, size,
 	return bpf_probe_read_kernel_str_common(dst, size, unsafe_ptr, false);
 }
 
-static const struct bpf_func_proto bpf_probe_read_kernel_str_proto = {
+const struct bpf_func_proto bpf_probe_read_kernel_str_proto = {
 	.func		= bpf_probe_read_kernel_str,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
@@ -876,7 +876,7 @@ BPF_CALL_0(bpf_get_current_task)
 	return (long) current;
 }
 
-static const struct bpf_func_proto bpf_get_current_task_proto = {
+const struct bpf_func_proto bpf_get_current_task_proto = {
 	.func		= bpf_get_current_task,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
