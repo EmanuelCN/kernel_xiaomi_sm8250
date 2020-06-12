5210d358feb170c385f1703da97758aeefd733e1
Vincent Guittot <vincent.guittot@linaro.org>
2020-06-12 17:47:03 +0200

    sched/pelt: Cleanup PELT divider
    Factorize in a single place the calculation of the divider to be used to
    to compute *_avg from *_sum value
    
    Suggested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200612154703.23555-1-vincent.guittot@linaro.org

--- kernel/sched/pelt.h
+++ kernel/sched/pelt.h
@@ -37,7 +37,7 @@ update_irq_load_avg(struct rq *rq, u64 running)
 }
 #endif
 
-#define PELT_MIN_DIVIDER	(LOAD_AVG_MAX - 1024)
+#define PELT_MIN_DIVIDER       (LOAD_AVG_MAX - 1024)
 
 static inline u32 get_pelt_divider(struct sched_avg *avg)
 {
