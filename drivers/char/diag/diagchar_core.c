d7dec358f4b96f68cbf9ccf3a67c2cd7566614f9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-06-17 11:37:19 +0530

    diag: Allow disabling diag debugfs
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -26,7 +26,9 @@
 #include "diagfwd.h"
 #include "diagfwd_cntl.h"
 #include "diag_dci.h"
+#ifdef CONFIG_DEBUG_FS
 #include "diag_debugfs.h"
+#endif
 #include "diag_masks.h"
 #include "diagfwd_bridge.h"
 #include "diag_usb.h"
@@ -4548,9 +4550,11 @@ static int __init diagchar_init(void)
 	ret = diag_real_time_info_init();
 	if (ret)
 		goto fail;
+#ifdef CONFIG_DEBUG_FS
 	ret = diag_debugfs_init();
 	if (ret)
 		goto fail;
+#endif
 	ret = diag_masks_init();
 	if (ret)
 		goto fail;
@@ -4599,7 +4603,9 @@ static int __init diagchar_init(void)
 
 fail:
 	pr_err("diagchar is not initialized, ret: %d\n", ret);
+#ifdef CONFIG_DEBUG_FS
 	diag_debugfs_cleanup();
+#endif
 	diagchar_cleanup();
 	diag_mux_exit();
 	diagfwd_peripheral_exit();
@@ -4625,7 +4631,9 @@ static void diagchar_exit(void)
 	diag_masks_exit();
 	diag_md_session_exit();
 	diag_remote_exit();
+#ifdef CONFIG_DEBUG_FS
 	diag_debugfs_cleanup();
+#endif
 	diagchar_cleanup();
 	pr_info("done diagchar exit\n");
 }
