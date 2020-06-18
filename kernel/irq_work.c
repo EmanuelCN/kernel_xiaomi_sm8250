14d7196c03d50e837346e2c2a16b07cfe9677855
Peter Zijlstra <peterz@infradead.org>
2020-06-18 22:28:37 +0200

    BACKPORT: irq_work: Optimize irq_work_single()
    Trade one atomic op for a full memory barrier.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    [ Tashar02: Backport to k4.19.
    	    Adapt to irq_work_run_list() and acquire `flags` from
    	    __call_single_node union ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -33,7 +33,7 @@ static bool irq_work_claim(struct irq_work *work)
 	oflags = atomic_fetch_or(IRQ_WORK_CLAIMED, &work->flags);
 	/*
 	 * If the work is already pending, no need to raise the IPI.
-	 * The pairing atomic_fetch_andnot() in irq_work_run() makes sure
+	 * The pairing smp_mb() in irq_work_run() makes sure
 	 * everything we did before is visible.
 	 */
 	if (oflags & IRQ_WORK_PENDING)
@@ -144,22 +144,27 @@ static void irq_work_run_list(struct llist_head *list)
 	llist_for_each_entry_safe(work, tmp, llnode, llnode) {
 		int flags;
 		/*
-		 * Clear the PENDING bit, after this point the @work
-		 * can be re-used.
-		 * Make it immediately visible so that other CPUs trying
-		 * to claim that work don't rely on us to handle their data
-		 * while we are in the middle of the func.
+		 * Clear the PENDING bit, after this point the @work can be re-used.
+		 * The PENDING bit acts as a lock, and we own it, so we can clear it
+		 * without atomic ops.
 		 */
-		flags = atomic_fetch_andnot(IRQ_WORK_PENDING, &work->flags);
-
-		lockdep_irq_work_enter(work);
-		work->func(work);
-		lockdep_irq_work_exit(work);
-		/*
-		 * Clear the BUSY bit and return to the free state if
-		 * no-one else claimed it meanwhile.
-		 */
+		flags = atomic_read(&work->flags);
 		flags &= ~IRQ_WORK_PENDING;
+		atomic_set(&work->flags, flags);
+
+		/*
+		 * See irq_work_claim().
+		 */
+		smp_mb();
+
+		lockdep_irq_work_enter(flags);
+		work->func(work);
+		lockdep_irq_work_exit(flags);
+
+		/*
+		 * Clear the BUSY bit, if set, and return to the free state if no-one
+		 * else claimed it meanwhile.
+		 */
 		(void)atomic_cmpxchg(&work->flags, flags, flags & ~IRQ_WORK_BUSY);
 	}
 }
