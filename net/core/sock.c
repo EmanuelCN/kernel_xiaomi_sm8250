4ef97bd85d04edc4b6061d93504d68986f6f17d4
Dmitry Yakunin <zeil@yandex-team.ru>
2020-06-20 18:30:50 +0300

    UPSTREAM: sock: Move sock_valbool_flag to header
    This is preparation for usage in bpf_setsockopt.
    
    Change-Id: Icee780dc355acb0b0ae77accbeac146ccbdd99f8
    Signed-off-by: Dmitry Yakunin <zeil@yandex-team.ru>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200620153052.9439-1-zeil@yandex-team.ru

--- net/core/sock.c
+++ net/core/sock.c
@@ -655,14 +655,6 @@ out:
 	return ret;
 }
 
-static inline void sock_valbool_flag(struct sock *sk, int bit, int valbool)
-{
-	if (valbool)
-		sock_set_flag(sk, bit);
-	else
-		sock_reset_flag(sk, bit);
-}
-
 bool sk_mc_loop(struct sock *sk)
 {
 	if (dev_recursion_level())
