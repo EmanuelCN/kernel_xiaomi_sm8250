3f8a39f440d8e2236cd799c9e07dd984f3e525b2
David Ahern <dsahern@kernel.org>
2020-07-14 15:56:35 +0200

    BACKPORT: net: Refactor xdp_convert_buff_to_frame
    Move the guts of xdp_convert_buff_to_frame to a new helper,
    xdp_update_frame_from_buff so it can be reused removing code duplication
    
    Suggested-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Co-developed-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Change-Id: Ifd1e2d12e8ab6c22fdddbf781d7d6bf16c15d995
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Link: https://lore.kernel.org/bpf/90a68c283d7ebeb48924934c9b7ac79492300472.1594734381.git.lorenzo@kernel.org

--- include/net/xdp.h
+++ include/net/xdp.h
@@ -124,6 +124,34 @@ void xdp_convert_frame_to_buff(struct xdp_frame *frame, struct xdp_buff *xdp)
 	xdp->frame_sz = frame->frame_sz;
 }
 
+static inline
+int xdp_update_frame_from_buff(struct xdp_buff *xdp,
+			       struct xdp_frame *xdp_frame)
+{
+	int metasize, headroom;
+
+	/* Assure headroom is available for storing info */
+	headroom = xdp->data - xdp->data_hard_start;
+	metasize = xdp->data - xdp->data_meta;
+	metasize = metasize > 0 ? metasize : 0;
+	if (unlikely((headroom - metasize) < sizeof(*xdp_frame)))
+		return -ENOSPC;
+
+	/* Catch if driver didn't reserve tailroom for skb_shared_info */
+	if (unlikely(xdp->data_end > xdp_data_hard_end(xdp))) {
+		XDP_WARN("Driver BUG: missing reserved tailroom");
+		return -ENOSPC;
+	}
+
+	xdp_frame->data = xdp->data;
+	xdp_frame->len  = xdp->data_end - xdp->data;
+	xdp_frame->headroom = headroom - sizeof(*xdp_frame);
+	xdp_frame->metasize = metasize;
+	xdp_frame->frame_sz = xdp->frame_sz;
+
+	return 0;
+}
+
 /* Convert xdp_buff to xdp_frame */
 static inline
 struct xdp_frame *xdp_convert_buff_to_frame(struct xdp_buff *xdp)
