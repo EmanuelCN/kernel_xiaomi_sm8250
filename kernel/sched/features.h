6eb8be258e00cc906cc80a5351f6367f9b9b34ea
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-09-11 13:30:51 -0500

    sched: features: Disable EAS_PREFER_IDLE
    This option tends to assign tasks to the best (in energy terms)
    idle CPU, but if there is no totally idle core, then the task
    will be assigned to a very high power core, generating a lot of
    heat and a lot of power consumption for literally NO REASON.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -104,7 +104,8 @@ SCHED_FEAT(FIND_BEST_TARGET, true)
  *   the EAS path for wakeup task placement. Otherwise, put
  *   those tasks through the mainline slow path.
  */
-SCHED_FEAT(EAS_PREFER_IDLE, true)
+
+SCHED_FEAT(EAS_PREFER_IDLE, false)
 
 /*
  * Request max frequency from schedutil whenever a RT task is running.
