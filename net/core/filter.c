73e0e77468a571480ec0883ce26513f65c632783
Martin KaFai Lau <kafai@fb.com>
2020-09-24 17:03:56 -0700

    UPSTREAM: bpf: Change bpf_sk_release and bpf_sk_*cgroup_id to accept ARG_PTR_TO_BTF_ID_SOCK_COMMON
    The previous patch allows the networking bpf prog to use the
    bpf_skc_to_*() helpers to get a PTR_TO_BTF_ID socket pointer,
    e.g. "struct tcp_sock *".  It allows the bpf prog to read all the
    fields of the tcp_sock.
    
    This patch changes the bpf_sk_release() and bpf_sk_*cgroup_id()
    to take ARG_PTR_TO_BTF_ID_SOCK_COMMON such that they will
    work with the pointer returned by the bpf_skc_to_*() helpers
    also.  For example, the following will work:
    
    	sk = bpf_skc_lookup_tcp(skb, tuple, tuplen, BPF_F_CURRENT_NETNS, 0);
    	if (!sk)
    		return;
    	tp = bpf_skc_to_tcp_sock(sk);
    	if (!tp) {
    		bpf_sk_release(sk);
    		return;
    	}
    	lsndtime = tp->lsndtime;
    	/* Pass tp to bpf_sk_release() will also work */
    	bpf_sk_release(tp);
    
    Since PTR_TO_BTF_ID could be NULL, the helper taking
    ARG_PTR_TO_BTF_ID_SOCK_COMMON has to check for NULL at runtime.
    
    A btf_id of "struct sock" may not always mean a fullsock.  Regardless
    the helper's running context may get a non-fullsock or not,
    considering fullsock check/handling is pretty cheap, it is better to
    keep the same verifier expectation on helper that takes ARG_PTR_TO_BTF_ID*
    will be able to handle the minisock situation.  In the bpf_sk_*cgroup_id()
    case,  it will try to get a fullsock by using sk_to_full_sk() as its
    skb variant bpf_sk"b"_*cgroup_id() has already been doing.
    
    bpf_sk_release can already handle minisock, so nothing special has to
    be done.
    
    Change-Id: Ia9823f535d2a69b6ed4c16f599faa3f8c3ac3a42
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200925000356.3856047-1-kafai@fb.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -4146,18 +4146,17 @@ static inline u64 __bpf_sk_cgroup_id(struct sock *sk)
 {
 	struct cgroup *cgrp;
 
+	sk = sk_to_full_sk(sk);
+	if (!sk || !sk_fullsock(sk))
+		return 0;
+
 	cgrp = sock_cgroup_ptr(&sk->sk_cgrp_data);
 	return cgroup_id(cgrp);
 }
 
 BPF_CALL_1(bpf_skb_cgroup_id, const struct sk_buff *, skb)
 {
-	struct sock *sk = skb_to_full_sk(skb);
-
-	if (!sk || !sk_fullsock(sk))
-		return 0;
-
-	return __bpf_sk_cgroup_id(sk);
+	return __bpf_sk_cgroup_id(skb->sk);
 }
 
 static const struct bpf_func_proto bpf_skb_cgroup_id_proto = {
@@ -4173,6 +4172,10 @@ static inline u64 __bpf_sk_ancestor_cgroup_id(struct sock *sk,
 	struct cgroup *ancestor;
 	struct cgroup *cgrp;
 
+	sk = sk_to_full_sk(sk);
+	if (!sk || !sk_fullsock(sk))
+		return 0;
+
 	cgrp = sock_cgroup_ptr(&sk->sk_cgrp_data);
 	ancestor = cgroup_ancestor(cgrp, ancestor_level);
 	if (!ancestor)
@@ -4184,12 +4187,7 @@ static inline u64 __bpf_sk_ancestor_cgroup_id(struct sock *sk,
 BPF_CALL_2(bpf_skb_ancestor_cgroup_id, const struct sk_buff *, skb, int,
 	   ancestor_level)
 {
-	struct sock *sk = skb_to_full_sk(skb);
-
-	if (!sk || !sk_fullsock(sk))
-		return 0;
-
-	return __bpf_sk_ancestor_cgroup_id(sk, ancestor_level);
+	return __bpf_sk_ancestor_cgroup_id(skb->sk, ancestor_level);
 }
 
 static const struct bpf_func_proto bpf_skb_ancestor_cgroup_id_proto = {
@@ -4209,7 +4207,7 @@ static const struct bpf_func_proto bpf_sk_cgroup_id_proto = {
 	.func           = bpf_sk_cgroup_id,
 	.gpl_only       = false,
 	.ret_type       = RET_INTEGER,
-	.arg1_type      = ARG_PTR_TO_SOCKET,
+	.arg1_type      = ARG_PTR_TO_BTF_ID_SOCK_COMMON,
 };
 
 BPF_CALL_2(bpf_sk_ancestor_cgroup_id, struct sock *, sk, int, ancestor_level)
@@ -4221,7 +4219,7 @@ static const struct bpf_func_proto bpf_sk_ancestor_cgroup_id_proto = {
 	.func           = bpf_sk_ancestor_cgroup_id,
 	.gpl_only       = false,
 	.ret_type       = RET_INTEGER,
-	.arg1_type      = ARG_PTR_TO_SOCKET,
+	.arg1_type      = ARG_PTR_TO_BTF_ID_SOCK_COMMON,
 	.arg2_type      = ARG_ANYTHING,
 };
 #endif
@@ -5780,7 +5778,7 @@ static const struct bpf_func_proto bpf_sk_lookup_udp_proto = {
 
 BPF_CALL_1(bpf_sk_release, struct sock *, sk)
 {
-	if (sk_is_refcounted(sk))
+	if (sk && sk_is_refcounted(sk))
 		sock_gen_put(sk);
 	return 0;
 }
@@ -5789,7 +5787,7 @@ static const struct bpf_func_proto bpf_sk_release_proto = {
 	.func		= bpf_sk_release,
 	.gpl_only	= false,
 	.ret_type	= RET_INTEGER,
-	.arg1_type	= ARG_PTR_TO_SOCK_COMMON,
+	.arg1_type	= ARG_PTR_TO_BTF_ID_SOCK_COMMON,
 };
 
 BPF_CALL_5(bpf_xdp_sk_lookup_udp, struct xdp_buff *, ctx,
