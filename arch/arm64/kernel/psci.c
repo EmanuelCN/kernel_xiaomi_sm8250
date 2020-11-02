7da61cca1860f630f4ae357deba771982e608ce5
Danny Lin <danny@kdrag0n.dev>
2020-11-01 16:58:55 -0800

    Treewide: Suppress overly verbose log spam
    It's hard to see anything that's actually useful with so much verbose
    spam in the log buffer.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- arch/arm64/kernel/psci.c
+++ arch/arm64/kernel/psci.c
@@ -98,7 +98,7 @@ static int cpu_psci_cpu_kill(unsigned int cpu)
 	do {
 		err = psci_ops.affinity_info(cpu_logical_map(cpu), 0);
 		if (err == PSCI_0_2_AFFINITY_LEVEL_OFF) {
-			pr_info("CPU%d killed (polled %d ms)\n", cpu,
+			pr_debug("CPU%d killed (polled %d ms)\n", cpu,
 				jiffies_to_msecs(jiffies - start));
 			return 0;
 		}
