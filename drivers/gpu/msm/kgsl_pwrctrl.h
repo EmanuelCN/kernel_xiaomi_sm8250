8a38a24b1058789bfd7684f5531563a47ba37d12
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-05 01:24:23 -0800

    msm: kgsl: Remove POPP
    POPP constantly attempts to lower the GPU's frequency behind the
    governor's back in order to save power; however, the GPU governor in use
    (msm-adreno-tz) is very good at determining the GPU's load and selecting
    an appropriate frequency to run the GPU at.
    
    POPP was created long ago, perhaps when msm-adreno-tz didn't exist or
    didn't work so well, so it is clearly deprecated. Remove it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -126,7 +126,6 @@ struct gpu_cx_ipeak_client {
  * @previous_pwrlevel - The power level before transition
  * @thermal_pwrlevel - maximum powerlevel constraint from thermal
  * @thermal_pwrlevel_floor - minimum powerlevel constraint from thermal
- * @default_pwrlevel - device wake up power level
  * @max_pwrlevel - maximum allowable powerlevel per the user
  * @min_pwrlevel - minimum allowable powerlevel per the user
  * @num_pwrlevels - number of available power levels
@@ -181,7 +180,6 @@ struct kgsl_pwrctrl {
 	unsigned int previous_pwrlevel;
 	unsigned int thermal_pwrlevel;
 	unsigned int thermal_pwrlevel_floor;
-	unsigned int default_pwrlevel;
 	unsigned int wakeup_maxpwrlevel;
 	unsigned int max_pwrlevel;
 	unsigned int min_pwrlevel;
