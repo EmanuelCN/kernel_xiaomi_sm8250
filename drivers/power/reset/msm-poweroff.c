ce3d1b50a4844b0efca3f29f834cdfb7b0d88f7f
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 21:19:23 -0800

    power: reset: msm: Always perform a warm reboot
    This ensures the ramoops buffer is preserved upon panic and that we will
    get a ramoops every time there is a reboot, which can be useful for
    debugging even if a panic did not occur (e.g., watchdog bite).
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ia12b9b4fdcb39bfaa8d38d6d88389e3e66e5f86c
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -74,7 +74,7 @@ static bool dload_mode_enabled;
 static void *emergency_dload_mode_addr;
 static bool scm_dload_supported;
 
-static bool force_warm_reboot;
+static bool force_warm_reboot = true;
 
 /* interface for exporting attributes */
 struct reset_attribute {
@@ -680,9 +680,6 @@ static int msm_restart_probe(struct platform_device *pdev)
 	set_dload_mode(download_mode);
 	if (!download_mode)
 		scm_disable_sdi();
-
-	force_warm_reboot = of_property_read_bool(dev->of_node,
-						"qcom,force-warm-reboot");
 
 	return 0;
 
