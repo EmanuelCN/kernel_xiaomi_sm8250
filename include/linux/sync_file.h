237279220caa98ea84ce12aa1acb378a8d788695
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-06 21:00:31 -0800

    dma-buf/sync_file: Speed up ioctl by omitting debug names
    A lot of CPU time is wasted on allocating, populating, and copying
    debug names back and forth with userspace when they're not actually
    needed. We can't just remove the name buffers from the various sync data
    structures though because we must preserve ABI compatibility with
    userspace, but instead we can just pretend the name fields of the
    user-shared structs aren't there. This massively reduces the sizes of
    memory allocated for these data structures and the amount of data passed
    between userspace, as well as eliminates a kzalloc() entirely from
    sync_file_ioctl_fence_info(), thus improving graphics performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- include/linux/sync_file.h
+++ include/linux/sync_file.h
@@ -34,14 +34,6 @@
  */
 struct sync_file {
 	struct file		*file;
-	/**
-	 * @user_name:
-	 *
-	 * Name of the sync file provided by userspace, for merged fences.
-	 * Otherwise generated through driver callbacks (in which case the
-	 * entire array is 0).
-	 */
-	char			user_name[32];
 #ifdef CONFIG_DEBUG_FS
 	struct list_head	sync_file_list;
 #endif
@@ -57,6 +49,5 @@ struct sync_file {
 
 struct sync_file *sync_file_create(struct dma_fence *fence);
 struct dma_fence *sync_file_get_fence(int fd);
-char *sync_file_get_name(struct sync_file *sync_file, char *buf, int len);
 
 #endif /* _LINUX_SYNC_H */
