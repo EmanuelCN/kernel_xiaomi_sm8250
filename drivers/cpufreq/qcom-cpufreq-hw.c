e11523eb5ad115916f7c465feed5cc7c43df7ea2
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-09 01:16:28 -0800

    cpufreq: qcom-cpufreq-hw: Set each CPU clock to its max when waking up
    The default frequency on Qualcomm CPUs is the lowest frequency supported
    by the CPU. This hurts latency when waking from suspend, as each CPU
    coming online runs at its lowest frequency until the governor can take
    over later. To speed up waking from suspend, hijack the CPUHP_AP_ONLINE
    hook and use it to set the highest available frequency on each CPU as
    they come online. This is done behind the governor's back but it's fine
    because the governor isn't running at this point in time for a CPU
    that's coming online.
    
    This speeds up waking from suspend significantly.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [ Port to 4.19 ]
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -99,6 +99,13 @@ struct cpufreq_cooling_cdev {
 	struct device_node *np;
 };
 
+struct cpufreq_qcom_boost {
+	struct cpufreq_qcom *c;
+	unsigned int max_index;
+};
+
+static DEFINE_PER_CPU(struct cpufreq_qcom_boost, cpufreq_boost_pcpu);
+
 static const u16 cpufreq_qcom_std_offsets[REG_ARRAY_SIZE] = {
 	[REG_ENABLE]		= 0x0,
 	[REG_FREQ_LUT_TABLE]	= 0x110,
@@ -453,6 +460,16 @@ static struct cpufreq_driver cpufreq_qcom_hw_driver = {
 	.ready		= qcom_cpufreq_ready,
 };
 
+static int cpuhp_qcom_online(unsigned int cpu)
+{
+	struct cpufreq_qcom_boost *b = &per_cpu(cpufreq_boost_pcpu, cpu);
+	struct cpufreq_qcom *c = b->c;
+
+	/* Set the max frequency by default before the governor takes over */
+	writel_relaxed(b->max_index, c->reg_bases[REG_PERF_STATE]);
+	return 0;
+}
+
 static bool of_find_freq(u32 *of_table, int of_len, long frequency)
 {
 	int i;
@@ -585,6 +602,10 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 
 	c->lut_max_entries = i;
 	c->table[i].frequency = CPUFREQ_TABLE_END;
+	for_each_cpu(cpu, &c->related_cpus) {
+		per_cpu(cpufreq_boost_pcpu, cpu).c = c;
+		per_cpu(cpufreq_boost_pcpu, cpu).max_index = i - 1;
+	}
 
 	if (of_table)
 			devm_kfree(dev, of_table);
@@ -906,6 +927,11 @@ static int qcom_cpufreq_hw_driver_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "CPUFreq HW driver failed to register\n");
 		return rc;
 	}
+
+	rc = cpuhp_setup_state_nocalls(CPUHP_AP_ONLINE, "qcom-cpufreq:online",
+				       cpuhp_qcom_online, NULL);
+	if (rc)
+		dev_err(&pdev->dev, "CPUHP callback setup failed, rc=%d\n", rc);
 
 	dev_dbg(&pdev->dev, "QCOM CPUFreq HW driver initialized\n");
 	of_platform_populate(pdev->dev.of_node, NULL, NULL, &pdev->dev);
