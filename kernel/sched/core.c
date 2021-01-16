808a8cad48c94a62bafc5019988f816d6121b1c3
Peter Zijlstra <peterz@infradead.org>
2021-01-16 11:56:37 +0100

    sched: Relax the set_cpus_allowed_ptr() semantics
    Now that we have KTHREAD_IS_PER_CPU to denote the critical per-cpu
    tasks to retain during CPU offline, we can relax the warning in
    set_cpus_allowed_ptr(). Any spurious kthread that wants to get on at
    the last minute will get pushed off before it can run.
    
    While during CPU online there is no harm, and actual benefit, to
    allowing kthreads back on early, it simplifies hotplug code and fixes
    a number of outstanding races.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Lai jiangshan <jiangshanlai@gmail.com>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210121103507.240724591@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2522,7 +2522,9 @@ static int __set_cpus_allowed_ptr_locked(struct task_struct *p,
 
 	if (kthread || is_migration_disabled(p)) {
 		/*
-		 * Kernel threads are allowed on online && !active CPUs.
+		 * Kernel threads are allowed on online && !active CPUs,
+		 * however, during cpu-hot-unplug, even these might get pushed
+		 * away if not KTHREAD_IS_PER_CPU.
 		 *
 		 * Specifically, migration_disabled() tasks must not fail the
 		 * cpumask_any_and_distribute() pick below, esp. so on
@@ -2587,16 +2589,6 @@ static int __set_cpus_allowed_ptr_locked(struct task_struct *p,
 	}
 #endif
         __do_set_cpus_allowed(p, ctx);
-
-	if (p->flags & PF_KTHREAD) {
-		/*
-		 * For kernel threads that do indeed end up on online &&
-		 * !active we want to ensure they are strict per-CPU threads.
-		 */
-		WARN_ON(cpumask_intersects(ctx->new_mask, cpu_online_mask) &&
-			!cpumask_intersects(ctx->new_mask, cpu_active_mask) &&
-			p->nr_cpus_allowed != 1);
-	}
 
 	return affine_move_task(rq, p, rf, dest_cpu, ctx->flags);
 
@@ -8099,6 +8091,13 @@ int sched_cpu_deactivate(unsigned int cpu)
 	int ret;
 
 	set_cpu_active(cpu, false);
+
+	/*
+	 * From this point forward, this CPU will refuse to run any task that
+	 * is not: migrate_disable() or KTHREAD_IS_PER_CPU, and will actively
+	 * push those tasks away until this gets cleared, see
+	 * sched_cpu_dying().
+	 */
 	balance_push_set(cpu, true);
 
 	/*
