7201696402cd4d93454d71e6517a4dddf615057c
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 22:11:48 -0800

    drm/msm: Reduce latency while completing non-blocking commits
    In order to complete a commit, we must first wait for the previous
    commit to finish up. This is done by sleeping, during which time the CPU
    can enter a deep idle state and take a while to finish processing the
    commit after the wait is over. We can alleviate this by optimistically
    assuming that the kthread this commit worker is running on won't migrate
    to another CPU partway through. We only do this for the non-blocking
    case where the commit completion is done in an asynchronous worker
    because the generic DRM code already does this for atomic ioctls. Since
    non-blocking commits are the common case, preemptively kick the prime
    CPU out of idle when the atomic ioctl runs so that it'll be ready to
    process the DRM IRQ and display commit kthreads.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -557,30 +557,32 @@ static void complete_commit(struct msm_commit *c)
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
-	ktime_t start, end;
-	s64 duration;
 	struct msm_commit *c = container_of(work, typeof(*c), commit_work);
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
+	};
 
-	start = ktime_get();
-	frame_stat_collector(0, COMMIT_START_TS);
-
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to resume after waiting for the prior commit to finish.
+	 */
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
 	SDE_ATRACE_BEGIN("complete_commit");
 	complete_commit(c);
 	SDE_ATRACE_END("complete_commit");
+	pm_qos_remove_request(&req);
 
-        if (c->nonblock) {
-                /* Offload the cleanup onto little CPUs (an unbound wq) */
-                INIT_WORK(&c->clean_work, complete_commit_cleanup);
-                queue_work(system_unbound_wq, &c->clean_work);
-        } else {
-                complete_commit_cleanup(&c->clean_work);
-        }
-
-	end = ktime_get();
-	duration = ktime_to_ns(ktime_sub(end, start));
-	frame_stat_collector(duration, COMMIT_END_TS);
-
+	if (c->nonblock) {
+		/* Offload the cleanup onto little CPUs (an unbound wq) */
+		INIT_WORK(&c->clean_work, complete_commit_cleanup);
+		queue_work(system_unbound_wq, &c->clean_work);
+	} else {
+		complete_commit_cleanup(&c->clean_work);
+	}
 }
+
 
 static struct msm_commit *commit_init(struct drm_atomic_state *state,
 	bool nonblock)
