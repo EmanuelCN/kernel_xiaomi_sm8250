1a90faf64d9420650e680b2a5a7fba4d735dfec4
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 18:02:14 -0800

    msm: kgsl: Reduce latency while processing ioctls
    The GPU's ioctls sleep a lot due to memory allocation, during which time
    the CPU can enter a deep idle state and take a while to finish executing
    the ioctl afterwards. We can alleviate this by optimistically assuming
    that the task running the ioctl won't migrate CPUs while it runs and
    restricting the current CPU to shallow idle states.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/msm/kgsl_ioctl.c
+++ drivers/gpu/msm/kgsl_ioctl.c
@@ -149,7 +149,8 @@ long kgsl_ioctl_helper(struct file *filep, unsigned int cmd, unsigned long arg,
 	return ret;
 }
 
-long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+static long __kgsl_ioctl(struct file *filep, unsigned int cmd,
+			 unsigned long arg)
 {
 	struct kgsl_device_private *dev_priv = filep->private_data;
 	struct kgsl_device *device = dev_priv->device;
@@ -174,6 +175,27 @@ long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 		else if (device->ftbl->ioctl != NULL)
 			return device->ftbl->ioctl(dev_priv, cmd, arg);
 	}
+
+	return ret;
+}
+
+long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+{
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to finish running the ioctl whenever the ioctl runs a
+	 * command that sleeps, such as for memory allocation.
+	 */
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
+	};
+	long ret;
+
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
+	ret = __kgsl_ioctl(filep, cmd, arg);
+	pm_qos_remove_request(&req);
 
 	return ret;
 }
