af0b0813a9b6f3156c54a6fdb6e66ca2bc00a55c
Tyler Nijmeh <tylernij@gmail.com>
2021-02-04 18:42:17 +0300

    mm: Do not reserve memory for admins
    Android systems don't need protected memory for admin logins.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3816,11 +3816,6 @@ subsys_initcall(init_user_reserve);
  */
 static int init_admin_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_admin_reserve_kbytes = min(free_kbytes / 32, 1UL << 13);
 	return 0;
 }
 subsys_initcall(init_admin_reserve);
