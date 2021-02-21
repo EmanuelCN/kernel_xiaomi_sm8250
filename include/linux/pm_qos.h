563cc5d11ca5a0587ba26be5ee06d3a1de6ef86b
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-21 15:56:25 -0800

    qos: Remove pm_qos_update_request_timeout() API
    Using a timeout for a PM QoS request can lead to disastrous results on
    power consumption. It's always possible to find a fixed scope in which a
    PM QoS request should be applied, so timeouts aren't ever strictly
    needed; they're usually just a lazy way of using PM QoS. Remove the API
    so that it cannot be abused any longer.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -8,7 +8,6 @@
 #include <linux/plist.h>
 #include <linux/notifier.h>
 #include <linux/device.h>
-#include <linux/workqueue.h>
 #include <linux/cpumask.h>
 #include <linux/interrupt.h>
 
@@ -64,7 +63,6 @@ struct pm_qos_request {
 #endif
 	struct plist_node node;
 	int pm_qos_class;
-	struct delayed_work work; /* for pm_qos_update_request_timeout */
 };
 
 struct pm_qos_flags_request {
@@ -144,8 +142,6 @@ void pm_qos_add_request(struct pm_qos_request *req, int pm_qos_class,
 			s32 value);
 void pm_qos_update_request(struct pm_qos_request *req,
 			   s32 new_value);
-void pm_qos_update_request_timeout(struct pm_qos_request *req,
-				   s32 new_value, unsigned long timeout_us);
 void pm_qos_remove_request(struct pm_qos_request *req);
 
 int pm_qos_request(int pm_qos_class);
