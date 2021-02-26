874f1641780d11034ce6e3fd01fce5e0a811a73c
LibXZR <xzr467706992@163.com>
2021-02-26 15:35:38 +0800

    walt: sched_avg: Nuke hysteresis feature
    * This feature could disable cpuidle when powerhal writes
    /proc/sys/kernel/sched_busy_hyst_ns
    /proc/sys/kernel/sched_busy_hysteresis_enable_cpus
    
    * Nuke it to save power

--- kernel/sched/sched_avg.c
+++ kernel/sched/sched_avg.c
@@ -221,6 +221,8 @@ u64 sched_lpm_disallowed_time(int cpu)
 	u64 now = sched_clock();
 	u64 bias_end_time = atomic64_read(&per_cpu(busy_hyst_end_time, cpu));
 
+	return 0;
+
 	if (now < bias_end_time)
 		return bias_end_time - now;
 
