f14373d7995761e7e33ae1af7f42a6d337cac3b8
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2021-03-06 15:07:39 -0600

    sched: Remove unnecessary variable from schedule_tail()
    Since 565790d28b1 (sched: Fix balance_callback(), 2020-05-11), there
    is no longer a need to reuse the result value of the call to finish_task_switch()
    inside schedule_tail(), therefore the variable used to hold that value
    (rq) is no longer needed.
    
    Signed-off-by: Edmundo Carmona Antoranz <eantoranz@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210306210739.1370486-1-eantoranz@gmail.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4722,8 +4722,6 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 asmlinkage __visible void schedule_tail(struct task_struct *prev)
 	__releases(rq->lock)
 {
-	struct rq *rq;
-
 	/*
 	 * New tasks start with FORK_PREEMPT_COUNT, see there and
 	 * finish_task_switch() for details.
@@ -4733,7 +4731,7 @@ asmlinkage __visible void schedule_tail(struct task_struct *prev)
 	 * PREEMPT_COUNT kernels).
 	 */
 
-	rq = finish_task_switch(prev);
+	finish_task_switch(prev);
 	preempt_enable();
 
 	if (current->set_child_tid)
