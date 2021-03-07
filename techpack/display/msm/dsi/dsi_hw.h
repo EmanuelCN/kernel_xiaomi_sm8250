c4dacb834a3914a745b38346dc103c6907ae9694
Tung Vu <vutung.ct@gmail.com>
2021-03-07 11:32:52 +0700

    techpack, display: use readl_relaxed_no_log to improve performance

--- techpack/display/msm/dsi/dsi_hw.h
+++ techpack/display/msm/dsi/dsi_hw.h
@@ -7,51 +7,51 @@
 #define _DSI_HW_H_
 #include <linux/io.h>
 
-#define DSI_R32(dsi_hw, off) readl_relaxed((dsi_hw)->base + (off))
+#define DSI_R32(dsi_hw, off) readl_relaxed_no_log((dsi_hw)->base + (off))
 #define DSI_W32(dsi_hw, off, val) \
 	do {\
 		pr_debug("[DSI_%d][%s] - [0x%08x]\n", \
 			(dsi_hw)->index, #off, (uint32_t)(val)); \
-		writel_relaxed((val), (dsi_hw)->base + (off)); \
+		writel_relaxed_no_log((val), (dsi_hw)->base + (off)); \
 	} while (0)
 
 #define DSI_MMSS_MISC_R32(dsi_hw, off) \
-	readl_relaxed((dsi_hw)->mmss_misc_base + (off))
+	readl_relaxed_no_log((dsi_hw)->mmss_misc_base + (off))
 #define DSI_MMSS_MISC_W32(dsi_hw, off, val) \
 	do {\
 		pr_debug("[DSI_%d][%s] - [0x%08x]\n", \
 			(dsi_hw)->index, #off, val); \
-		writel_relaxed((val), (dsi_hw)->mmss_misc_base + (off)); \
+		writel_relaxed_no_log((val), (dsi_hw)->mmss_misc_base + (off)); \
 	} while (0)
 
 #define DSI_MISC_R32(dsi_hw, off) \
-	readl_relaxed((dsi_hw)->phy_clamp_base + (off))
+	readl_relaxed_no_log((dsi_hw)->phy_clamp_base + (off))
 #define DSI_MISC_W32(dsi_hw, off, val) \
 	do {\
 		pr_debug("[DSI_%d][%s] - [0x%08x]\n", \
 			(dsi_hw)->index, #off, val); \
-		writel_relaxed((val), (dsi_hw)->phy_clamp_base + (off)); \
+		writel_relaxed_no_log((val), (dsi_hw)->phy_clamp_base + (off)); \
 	} while (0)
 #define DSI_DISP_CC_R32(dsi_hw, off) \
-	readl_relaxed((dsi_hw)->disp_cc_base + (off))
+	readl_relaxed_no_log((dsi_hw)->disp_cc_base + (off))
 #define DSI_DISP_CC_W32(dsi_hw, off, val) \
 	do {\
 		pr_debug("[DSI_%d][%s] - [0x%08x]\n", \
 			(dsi_hw)->index, #off, val); \
-		writel_relaxed((val), (dsi_hw)->disp_cc_base + (off)); \
+		writel_relaxed_no_log((val), (dsi_hw)->disp_cc_base + (off)); \
 	} while (0)
 
-#define DSI_R64(dsi_hw, off) readq_relaxed((dsi_hw)->base + (off))
-#define DSI_W64(dsi_hw, off, val) writeq_relaxed((val), (dsi_hw)->base + (off))
+#define DSI_R64(dsi_hw, off) readq_relaxed_no_log((dsi_hw)->base + (off))
+#define DSI_W64(dsi_hw, off, val) writeq_relaxed_no_log((val), (dsi_hw)->base + (off))
 
 #define PLL_CALC_DATA(addr0, addr1, data0, data1)      \
 	(((data1) << 24) | ((((addr1)/4) & 0xFF) << 16) | \
 	 ((data0) << 8) | (((addr0)/4) & 0xFF))
 
 #define DSI_DYN_REF_REG_W(base, offset, addr0, addr1, data0, data1)   \
-	writel_relaxed(PLL_CALC_DATA(addr0, addr1, data0, data1), \
+	writel_relaxed_no_log(PLL_CALC_DATA(addr0, addr1, data0, data1), \
 			(base) + (offset))
 
-#define DSI_GEN_R32(base, offset) readl_relaxed(base + (offset))
-#define DSI_GEN_W32(base, offset, val) writel_relaxed((val), base + (offset))
+#define DSI_GEN_R32(base, offset) readl_relaxed_no_log(base + (offset))
+#define DSI_GEN_W32(base, offset, val) writel_relaxed_no_log((val), base + (offset))
 #endif /* _DSI_HW_H_ */
