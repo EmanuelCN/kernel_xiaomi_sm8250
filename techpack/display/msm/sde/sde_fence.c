a5d61a8b39c2161292570acdd23c836f82006994
Park Ju Hyung <qkrwngud825@gmail.com>
2021-03-14 10:26:57 -0400

    techpack: display: add some bp hints to hot paths
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -189,13 +189,13 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	signed int fd = -EINVAL;
 	struct sde_fence_context *ctx = fence_ctx;
 
-	if (!ctx) {
+	if (unlikely(!ctx)) {
 		SDE_ERROR("invalid context\n");
 		goto exit;
 	}
 
 	sde_fence = kzalloc(sizeof(*sde_fence), GFP_KERNEL);
-	if (!sde_fence)
+	if (unlikely(!sde_fence))
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
@@ -205,7 +205,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fd */
 	fd = get_unused_fd_start_flags(1, 0);
-	if (fd < 0) {
+	if (unlikely(fd < 0)) {
 		SDE_ERROR("failed to get_unused_fd_flags(), sde_fence:%s:%u\n",
 			  sde_fence->ctx->name, val);
 		dma_fence_put(&sde_fence->base);
@@ -214,7 +214,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fence */
 	sync_file = sync_file_create(&sde_fence->base);
-	if (sync_file == NULL) {
+	if (unlikely(sync_file == NULL)) {
 		put_unused_fd(fd);
 		fd = -EINVAL;
 		SDE_ERROR("couldn't create fence, sde_fence:%s:%u\n",
@@ -324,7 +324,7 @@ int sde_fence_create(struct sde_fence_context *ctx, uint64_t *val,
 	int fd, rc = -EINVAL;
 	unsigned long flags;
 
-	if (!ctx || !val) {
+	if (unlikely(!ctx || !val)) {
 		SDE_ERROR("invalid argument(s), fence %d, pval %d\n",
 				ctx != NULL, val != NULL);
 		return rc;
