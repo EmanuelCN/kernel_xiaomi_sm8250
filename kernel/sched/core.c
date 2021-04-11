ddcdbd1c0d754d2d4c5ff7b448c30330951914be
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2021-04-11 08:55:37 -0400

    [FIXUP] 01bb33a ("sched/core: fix userspace affining threads incorrectly")
    Certain userspace applications, to achieve max performance, affines its
    threads to cpus that run the fastest. This is not always the
    correct strategy. For e.g. in certain architectures all the
    cores have the same max freq but few of them have a bigger
    cache. Affining to the cpus that have bigger cache is advantageous
    but such an application would end up affining them to all the cores.
    Similarly if an architecture has just one cpu that runs at max freq,
    it ends up crowding all its thread on that single core, which is
    detrimental for performance.
    
    To address this issue, we need to detect a suspicious looking affinity
    request from userspace and check if it links in a particular library.
    The latter can easily be detected by traversing executable vm areas
    that map a file and checking for that library name.
    When such a affinity request is found, change it to use a proper
    affinity. The suspicious affinity request, the proper affinity request
    and the library name can be configured by the userspace.
    
    Change-Id: I6bb8c310ca54c03261cc721f28dfd6023ab5591a
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5825,10 +5825,8 @@ unsigned int sched_lib_mask_force;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
-	char *libname, *lib_list;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
-	char *tmp_lib_name;
 	bool found = false;
 	struct task_struct *p;
 	struct mm_struct *mm;
@@ -5836,16 +5834,11 @@ bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
-	tmp_lib_name = kmalloc(LIB_PATH_LENGTH, GFP_KERNEL);
-	if (!tmp_lib_name)
-		return false;
-
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
-		kfree(tmp_lib_name);
 		return false;
 	}
 
@@ -5865,15 +5858,10 @@ bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
-			strlcpy(tmp_lib_name, sched_lib_name, LIB_PATH_LENGTH);
-			lib_list = tmp_lib_name;
-			while ((libname = strsep(&lib_list, ","))) {
-				libname = skip_spaces(libname);
-				if (strnstr(name, libname,
+			if (strnstr(name, sched_lib_name,
 					strnlen(name, LIB_PATH_LENGTH))) {
-					found = true;
-					goto release_sem;
-				}
+				found = true;
+				break;
 			}
 		}
 	}
@@ -5883,7 +5871,6 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
-	kfree(tmp_lib_name);
 	return found;
 }
 
