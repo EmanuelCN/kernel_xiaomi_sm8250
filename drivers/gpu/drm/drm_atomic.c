bba13ac434ae93e7dd2bebd6bab98d80e53f415b
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-11 16:39:28 -0400

    sched/boost: Boost CPU and DDR bus when sched_boost is triggered
    In addition to inhibiting CAF's sched_boost functionality, we will
    repurpose /proc/sys/kernel/sched_boost to act as a userspace hint
    for (CPU_INPUT|DEVFREQ)_BOOST to sustain boosts in order to cover
    extended user interactions which exceed the inital
    INPUT_BOOST_DURATION_MS window.
    
    Suggested-by: Yaroslav Furman <yaro330@gmail.com>
    Co-authored-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    [@0ctobot: Squash idkwhoiam322/weeb_kernel_oneplus_sm8150@652b6ae
    with idkwhoiam322/weeb_kernel_oneplus_sm8150@2289c62 and
    idkwhoiam322/weeb_kernel_oneplus_sm8150@65ce97e]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -35,6 +35,7 @@ w/*
 #include <linux/sync_file.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2599,11 +2600,12 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
-		
+
+	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPU_LLCCBW);
-		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW_DDR);
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		if (sysctl_sched_boost)
+			cpu_input_boost_kick();
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
