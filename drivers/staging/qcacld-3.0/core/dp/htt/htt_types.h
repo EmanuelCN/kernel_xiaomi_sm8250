b233128e0f50c156f39a243e838dbd06d6cb5b2f
Julian Liu <wlootlxt123@gmail.com>
2021-05-03 23:06:38 +0530

    qcacld-3.0: Free a bunch of pkts at once
    It is too bad to do a tight loop every adding pkt. When the hotspot is turned on, I notice that the
    htt_htc_misc_pkt_list_trim() function consumes at least 5% of CPU time. By caching the head of pkt
    queue and freeing multiple pkts at once to reduce CPU consumption.
    
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/staging/qcacld-3.0/core/dp/htt/htt_types.h
+++ drivers/staging/qcacld-3.0/core/dp/htt/htt_types.h
@@ -452,6 +452,9 @@ struct htt_pdev_t {
 
 	/* Flag to indicate whether new htt format is supported */
 	bool new_htt_format_enabled;
+	
+	struct htt_htc_pkt_union *last_misc_pkt;
+	int last_misc_num;
 };
 
 #define HTT_EPID_GET(_htt_pdev_hdl)  \
