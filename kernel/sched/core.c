2c28cf0dafc8f17eb2452d636a4a3be6efad785e
Qais Yousef <qais.yousef@arm.com>
2021-05-10 15:50:32 +0100

    sched/uclamp: Fix locking around cpu_util_update_eff()
    [ Upstream commit 93b73858701fd01de26a4a874eb95f9b7156fd4b ]
    
    cpu_cgroup_css_online() calls cpu_util_update_eff() without holding the
    uclamp_mutex or rcu_read_lock() like other call sites, which is
    a mistake.
    
    The uclamp_mutex is required to protect against concurrent reads and
    writes that could update the cgroup hierarchy.
    
    The rcu_read_lock() is required to traverse the cgroup data structures
    in cpu_util_update_eff().
    
    Surround the caller with the required locks and add some asserts to
    better document the dependency in cpu_util_update_eff().
    
    Fixes: 7226017ad37a ("sched/uclamp: Fix a bug in propagating uclamp value in new cgroups")
    Reported-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210510145032.1934078-3-qais.yousef@arm.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8265,9 +8265,13 @@ static int cpu_cgroup_css_online(struct cgroup_subsys_state *css)
 
 #ifdef CONFIG_UCLAMP_TASK_GROUP
 	/* Propagate the effective uclamp value for the new group */
+	mutex_lock(&uclamp_mutex);
+	rcu_read_lock();
 	cpu_util_update_eff(css);
+	rcu_read_unlock();
+	mutex_unlock(&uclamp_mutex);
 #ifdef CONFIG_UCLAMP_ASSIST
-	uclamp_set(css);
+        uclamp_set(css);
 #endif
 #endif
 
@@ -8357,6 +8361,9 @@ static void cpu_util_update_eff(struct cgroup_subsys_state *css)
 	unsigned int eff[UCLAMP_CNT];
 	enum uclamp_id clamp_id;
 	unsigned int clamps;
+
+	lockdep_assert_held(&uclamp_mutex);
+	SCHED_WARN_ON(!rcu_read_lock_held());
 
 	css_for_each_descendant_pre(css, top_css) {
 		uc_parent = css_tg(css)->parent
