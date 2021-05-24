e0ebf4814b6113f7ce6e8922c44cf114886826f3
Naman Padhiar <npadhiar@codeaurora.org>
2021-05-24 19:04:44 +0530

    icnss2: Avoid race between SOC wake requests
    Add check for ref count inside work that if SOC wake
    request is already sent, increment ref count and return
    without actually sending request to FW.
    
    Change-Id: Ia70e1699001c885b2a76b173ca1106f5555d55ea
    Signed-off-by: Naman Padhiar <npadhiar@codeaurora.org>

--- drivers/soc/qcom/icnss2/main.c
+++ drivers/soc/qcom/icnss2/main.c
@@ -812,6 +812,7 @@ static int icnss_driver_event_fw_ready_ind(struct icnss_priv *priv, void *data)
 
 	set_bit(ICNSS_FW_READY, &priv->state);
 	clear_bit(ICNSS_MODE_ON, &priv->state);
+	atomic_set(&priv->soc_wake_ref_count, 0);
 
 	icnss_pr_info("WLAN FW is ready: 0x%lx\n", priv->state);
 
@@ -1046,6 +1047,12 @@ static int icnss_event_soc_wake_request(struct icnss_priv *priv, void *data)
 
 	if (!priv)
 		return -ENODEV;
+
+	if (atomic_inc_not_zero(&priv->soc_wake_ref_count)) {
+		icnss_pr_dbg("SOC awake after posting work, Ref count: %d",
+			     atomic_read(&priv->soc_wake_ref_count));
+		return 0;
+	}
 
 	ret = wlfw_send_soc_wake_msg(priv, QMI_WLFW_WAKE_REQUEST_V01);
 	if (!ret)
