c386af865b4bb299b0a0450cb125b50360b79195
Ganapathiraju Sarath Varma <ganavarm@codeaurora.org>
2021-06-01 10:17:59 +0530

    audio-kernel: enable wsa883x compilation on sxr2130
    Add support of wsa883x on sxr2130.
    
    Change-Id: I9c50480f79c0f05e23315a115f7d8db602eecc6d
    Signed-off-by: Faiz Nabi Kuchay <fkuchay@codeaurora.org>

--- asoc/codecs/wsa883x/Android.mk
+++ asoc/codecs/wsa883x/Android.mk
@@ -6,10 +6,13 @@
 ifeq ($(call is-board-platform,lahaina),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_LAHAINA=m
 endif
+ifeq ($(call is-board-platform,kona),true)
+AUDIO_SELECT  := CONFIG_SND_SOC_KONA=m
+endif
 
 AUDIO_CHIPSET := audio
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,lahaina lito),true)
+ifeq ($(call is-board-platform-in-list, kona lahaina lito),true)
 
 LOCAL_PATH := $(call my-dir)
 
