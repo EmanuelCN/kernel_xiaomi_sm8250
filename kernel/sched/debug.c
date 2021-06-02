3962181311b0b18d08c15e08d802cf8dddf36f68
Dietmar Eggemann <dietmar.eggemann@arm.com>
2021-06-02 16:58:08 +0200

    sched/fair: Fix util_est UTIL_AVG_UNCHANGED handling
    The util_est internal UTIL_AVG_UNCHANGED flag which is used to prevent
    unnecessary util_est updates uses the LSB of util_est.enqueued. It is
    exposed via _task_util_est() (and task_util_est()).
    
    Commit 92a801e5d5b7 ("sched/fair: Mask UTIL_AVG_UNCHANGED usages")
    mentions that the LSB is lost for util_est resolution but
    find_energy_efficient_cpu() checks if task_util_est() returns 0 to
    return prev_cpu early.
    
    _task_util_est() returns the max value of util_est.ewma and
    util_est.enqueued or'ed w/ UTIL_AVG_UNCHANGED.
    So task_util_est() returning the max of task_util() and
    _task_util_est() will never return 0 under the default
    SCHED_FEAT(UTIL_EST, true).
    
    To fix this use the MSB of util_est.enqueued instead and keep the flag
    util_est internal, i.e. don't export it via _task_util_est().
    
    The maximal possible util_avg value for a task is 1024 so the MSB of
    'unsigned int util_est.enqueued' isn't used to store a util value.
    
    As a caveat the code behind the util_est_se trace point has to filter
    UTIL_AVG_UNCHANGED to see the real util_est.enqueued value which should
    be easy to do.
    
    This also fixes an issue report by Xuewen Yan that util_est_update()
    only used UTIL_AVG_UNCHANGED for the subtrahend of the equation:
    
      last_enqueued_diff = ue.enqueued - (task_util() | UTIL_AVG_UNCHANGED)
    
    Fixes: b89997aa88f0b sched/pelt: Fix task util_est update filtering
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Reviewed-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20210602145808.1562603-1-dietmar.eggemann@arm.com

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -835,7 +835,7 @@ void proc_sched_show_task(struct task_struct *p, struct pid_namespace *ns,
 	P(se.avg.util_avg);
 	P(se.avg.last_update_time);
 	P(se.avg.util_est.ewma);
-	P(se.avg.util_est.enqueued);
+	PM(se.avg.util_est.enqueued, ~UTIL_AVG_UNCHANGED);
 #endif
 	P(policy);
 	P(prio);
