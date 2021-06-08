95e6e75d363a999d1b5fbbf50ec7ec7fd0035ec2
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-08 08:43:10 +0530

    hwtracing: Add entries from kona-coresight
    cat kona-coresight.dtsi | grep primecell-periphid | cut -c29- | sed "s/>;//g;s/^/ETM4x_AMBA_ID(/g;s/$/),/g" | sort -u
    
    eng.stk: adapt and add clocks for kona
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/hwtracing/google/coresight-clk-amba-placeholder.c
+++ drivers/hwtracing/google/coresight-clk-amba-placeholder.c
@@ -36,6 +36,15 @@ static const struct amba_id coresight_ids[] = {
 	ETM4x_AMBA_ID(0x000bb95a), /* Cortex-A72 */
 	ETM4x_AMBA_ID(0x000bb959), /* Cortex-A73 */
 	ETM4x_AMBA_ID(0x000bb9da), /* Cortex-A35 */
+	ETM4x_AMBA_ID(0x0003b968),
+	ETM4x_AMBA_ID(0x000bb908),
+	ETM4x_AMBA_ID(0x000bb909),
+	ETM4x_AMBA_ID(0x000bb961),
+	ETM4x_AMBA_ID(0x000bb962),
+	ETM4x_AMBA_ID(0x000bb966),
+	ETM4x_AMBA_ID(0x000bb968),
+	ETM4x_AMBA_ID(0x000bb969),
+	ETM4x_AMBA_ID(0x000bb999),
 	/* dynamic-replicator IDs */
 	{
 		.id = 0x000bb909,
