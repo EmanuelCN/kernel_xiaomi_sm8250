74b246a16d47e2ec6139e98becac54290d5ab75c
Yu Zhao <yuzhao@google.com>
2021-06-26 17:11:32 -0600

    CHROMIUM: mm: multigenerational lru: add arch_has_hw_pte_young()
    Add arch_has_hw_pte_young() to indicate whether the h/w accessed bit
    is supported. For those CPUs that don't, e.g., arm64 v8.0, we skip
    walk_mm_list() and solely rely on lru_gen_scan_around(). This still
    retains ~80% of the performance improvement.
    
    BUG=b:123039911
    TEST=Built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: I32e1380bc5966b4cb3ba5a9796c95af61fa8fc4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2991722
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -58,9 +58,10 @@
 #define ARM64_WORKAROUND_1542418		37
 #define ARM64_WORKAROUND_1542419		38
 #define ARM64_SPECTRE_BHB                       39
+#define ARM64_HW_AF				40
 
-
-/* kabi: reserve 39 - 63 for future cpu capabilities */
+/* kabi: reserve 41 - 63 for future cpu capabilities */
 #define ARM64_NCAPS				63
+
 
 #endif /* __ASM_CPUCAPS_H */
