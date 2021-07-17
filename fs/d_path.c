819b2029c56daf6a3ea51132534b82e8391678da
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-17 09:55:50 -0700

    mm: Eliminate d_path_outlen() and further speed up show_map_vma()
    d_path_outlen() isn't needed because we know that d_path() always
    populates the given buffer backwards starting from the last byte; with
    this, we can easily calculate the length of the generated string by
    using the returned pointer from d_path() and the size of the buffer
    given to d_path(). This eliminates the need for d_path_outlen() and
    removes the bizarre strlen() usage, which makes things simpler and
    faster. We also now avoid a memmove() when d_path() completely uses up
    its provided buffer.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- fs/d_path.c
+++ fs/d_path.c
@@ -249,9 +249,9 @@ static void get_fs_root_rcu(struct fs_struct *fs, struct path *root)
  *
  * "buflen" should be positive.
  */
-char *d_path_outlen(const struct path *path, char *buf, int *buflen)
+char *d_path(const struct path *path, char *buf, int buflen)
 {
-	char *res = buf + *buflen;
+	char *res = buf + buflen;
 	struct path root;
 	int error;
 
@@ -268,21 +268,16 @@ char *d_path_outlen(const struct path *path, char *buf, int *buflen)
 	 */
 	if (path->dentry->d_op && path->dentry->d_op->d_dname &&
 	    (!IS_ROOT(path->dentry) || path->dentry != path->mnt->mnt_root))
-		return path->dentry->d_op->d_dname(path->dentry, buf, *buflen);
+		return path->dentry->d_op->d_dname(path->dentry, buf, buflen);
 
 	rcu_read_lock();
 	get_fs_root_rcu(current->fs, &root);
-	error = path_with_deleted(path, &root, &res, buflen);
+	error = path_with_deleted(path, &root, &res, &buflen);
 	rcu_read_unlock();
 
 	if (error < 0)
 		res = ERR_PTR(error);
 	return res;
-}
-
-char *d_path(const struct path *path, char *buf, int buflen)
-{
-	return d_path_outlen(path, buf, &buflen);
 }
 EXPORT_SYMBOL(d_path);
 
