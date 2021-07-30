a0fa5c12b6329249a389d786ad033aa2e65fc9b2
Will Deacon <will@kernel.org>
2021-07-30 12:24:31 +0100

    cpuset: Cleanup cpuset_cpus_allowed_fallback() use in select_fallback_rq()
    select_fallback_rq() only needs to recheck for an allowed CPU if the
    affinity mask of the task has changed since the last check.
    
    Return a 'bool' from cpuset_cpus_allowed_fallback() to indicate whether
    the affinity mask was updated, and use this to elide the allowed check
    when the mask has been left alone.
    
    No functional change.
    
    Suggested-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lore.kernel.org/r/20210730112443.23245-5-will@kernel.org

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -57,7 +57,7 @@ extern void cpuset_force_rebuild(void);
 extern void cpuset_update_active_cpus(void);
 extern void cpuset_wait_for_hotplug(void);
 extern void cpuset_cpus_allowed(struct task_struct *p, struct cpumask *mask);
-extern void cpuset_cpus_allowed_fallback(struct task_struct *p);
+extern bool cpuset_cpus_allowed_fallback(struct task_struct *p);
 extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
 #define cpuset_current_mems_allowed (current->mems_allowed)
 void cpuset_init_current_mems_allowed(void);
@@ -183,8 +183,9 @@ static inline void cpuset_cpus_allowed(struct task_struct *p,
 	cpumask_copy(mask, task_cpu_possible_mask(p));
 }
 
-static inline void cpuset_cpus_allowed_fallback(struct task_struct *p)
+static inline bool cpuset_cpus_allowed_fallback(struct task_struct *p)
 {
+	return false;
 }
 
 static inline nodemask_t cpuset_mems_allowed(struct task_struct *p)
