e82e9dbf2053e5c3d5f66dc1a94e87ba6f46b5ba
Abhishek Shah <abhshah@codeaurora.org>
2021-08-16 17:02:43 +0530

    BACKPORT: devfreq: governor_memlat: avoid deadlock due to cpu_grp->mons_lock usage
    lockdep is detecting possible circular locking dependency
    due to cpu_grp->mons_lock mutex as shown below:
    
    Chain exists of:
      &cpu_grp->mons_lock --> state_lock#3 --> devfreq_list_lock
    
    Possible unsafe locking scenario:
    
          CPU0                    CPU1
          ----                    ----
     lock(devfreq_list_lock);
                                  lock(state_lock#3);
                                  lock(devfreq_list_lock);
     lock(&cpu_grp->mons_lock);
    
    *** DEADLOCK ***
    
    Below is partial call stacks (in reverse order) showing relevant
    locking paths:
    
    Call stack for CPU0:
    start_hwmon+0x6c/0x5d0			[may acquire &cpu_grp->mons_lock]
    devfreq_memlat_ev_handler+0x2f4/0x3f8
    devfreq_add_device+0x418/0x538		[may acquire devfreq_list_lock]
    devfreq_add_icc+0x41c/0x528
    devfreq_icc_probe+0x20/0x30
    
    Call stack for CPU1:
    devfreq_add_governor+0x3c/0x260		[may acquire devfreq_list_lock]
    register_memlat+0x84/0x100
    memlat_mon_probe+0x414/0x550		[may acquire &cpu_grp->mons_lock]
    arm_memlat_mon_driver_probe+0x120/0x3b8
    
    Practically, this race is not possible, since devfreq_add_device first
    tries to find the governor, and only if it is succeeds,
    devfreq_memlat_ev_handler is called. And the governor would be found
    only if devfreq_add_governor has added it priorly.
    We have mechanism(initcall_level) in place to make sure that
    governor is added before devfreq_add_device happens.
    
    In attempt to quiet the lockdep warning, below fix is adopted:
    cpu_grp gets allocated by memlat_cpu_grp_probe, but it is used by its
    multiple memlat_mon children's memlat_mon_probe. cpu_grp->mons_lock is
    used to prevent race between them for access to cpu_grp and members.
    Use a new lock - cpu_grp->init_mons_lock - for the probe routine,
    and continue using cpu_grp->mons_lock in other routines.
    
    Change-Id: Ie59c28de0a40914fb120a305067ef0fe504a4455
    Signed-off-by: Abhishek Shah <abhshah@codeaurora.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2014-2018, 2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.
  */
 
 #define pr_fmt(fmt) "arm-memlat-mon: " fmt
@@ -124,6 +124,7 @@ struct memlat_mon {
  * @mons:			All of the memlat_mon structs representing
  *				the different voters who share this cpu_grp.
  * @mons_lock:		A lock used to protect the @mons.
+ * @init_mons_lock:	A lock used to protect the @mons in probe routine.
  */
 struct memlat_cpu_grp {
 	cpumask_t		cpus;
@@ -140,6 +141,7 @@ struct memlat_cpu_grp {
 	unsigned int		num_active_mons;
 	struct memlat_mon	*mons;
 	struct mutex		mons_lock;
+	struct mutex		init_mons_lock;
 };
 
 struct memlat_mon_spec {
@@ -621,6 +623,7 @@ static int memlat_cpu_grp_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	mutex_init(&cpu_grp->mons_lock);
+	mutex_init(&cpu_grp->init_mons_lock);
 	cpu_grp->update_ms = DEFAULT_UPDATE_MS;
 
 	dev_set_drvdata(dev, cpu_grp);
@@ -651,7 +654,7 @@ static int memlat_mon_probe(struct platform_device *pdev, bool is_compute)
 		return -ENODEV;
 	}
 
-	mutex_lock(&cpu_grp->mons_lock);
+	mutex_lock(&cpu_grp->init_mons_lock);
 	mon = &cpu_grp->mons[cpu_grp->num_inited_mons];
 	mon->is_active = false;
 	mon->requested_update_ms = 0;
@@ -765,7 +768,7 @@ static int memlat_mon_probe(struct platform_device *pdev, bool is_compute)
 		cpu_grp->num_inited_mons++;
 
 unlock_out:
-	mutex_unlock(&cpu_grp->mons_lock);
+	mutex_unlock(&cpu_grp->init_mons_lock);
 	return ret;
 }
 
