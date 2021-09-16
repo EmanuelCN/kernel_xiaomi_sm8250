6a9a151fb9d44811bdeda7b5704150cdd4c6a197
palaych <palaychm@yandex.ru>
2021-09-17 04:16:17 +0900

    kernel: sched: Reset changes to CAF
    Squashed:
    
    commit 3d6fb5edb0051372a674a7c6198dfe070fd5636e
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:09:59 2021 +0900
    
        Revert "UPSTREAM: sched/topology: Allow sched_asym_cpucapacity to be disabled"
    
        This reverts commit 2ffafc7ac59c6307b2fcc1b27b0062f852c04bc4.
    
    commit f1bab20bbfb4651b7cc9d5e5b99aafb136189102
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:06:18 2021 +0900
    
        Revert "sched/fair: Fix unfairness caused by missing load decay"
    
        This reverts commit 434ea8c1d1bf296a2597aeb28f6ccf62ae82f235.
    
    commit 43097a5b0cb4b53747cef8e55eaf240461a8c20d
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:05:57 2021 +0900
    
        Revert "sched/fair: Make sure to update tg contrib for blocked load"
    
        This reverts commit 4af84445075da26439598984d1a01c1cdadb0e2d.
    
    commit 56322e6d740ba130fd42a838497cf08c3d9a14d0
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:50:32 2021 +0900
    
        Revert "BACKPORT: sched/fair: Fix overutilized update in enqueue_task_fair()"
    
        This reverts commit 444683e9df5852431539e1814df912759cf80370.
    
    commit ad0675fb9b981a6bacc2aa292127b182772c1174
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:50:14 2021 +0900
    
        Revert "sched/fair: Optimize select_idle_cpu"
    
        This reverts commit b5cd7f229609cf328664aedb056a6fd744e0068b.
    
    commit 5fca60a014dc5dbab84d66165dd172debf4e1582
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:05:25 2021 +0900
    
        Revert "sched/fair: Fix CFS bandwidth hrtimer expiry type"
    
        This reverts commit aa36bd8fc187f513af2d250b82a3c913053ceaf1.
    
    commit d2ef6647e378b47a98a15d9438625c2e6a3bd7af
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:04:08 2021 +0900
    
        Revert "sched/debug: Fix cgroup_path[] serialization"
    
        This reverts commit 302d674cfacd2a89ba16beb973c757cb977e32a0.
    
    commit 1d24a54812b425c8a0f9937b38f2e30fd90ac11b
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:03:31 2021 +0900
    
        Revert "sched/deadline: Fix sched_dl_global_validate()"
    
        This reverts commit 6db84b27221204c37a53dd7c58b01b5d7e91ce0b.
    
    commit bed98aab39e483b4dd67edc811c108f2dc81dd10
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:02:07 2021 +0900
    
        Revert "sched: Reenable interrupts in do_sched_yield()"
    
        This reverts commit b6b6ba5754eee1907497504e4b31e22c78f7670f.
    
    commit b708512b41cb32482cdf7936cef4775bd68fc0ee
    Author: palaych <palaychm@yandex.ru>
    Date:   Thu Sep 16 21:47:17 2021 +0900
    
        Revert "UPSTREAM: sched: Fix out-of-bound access in uclamp"
    
        This reverts commit a2c5a95d599c9b4a864d97520df42baefbfdb822.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5021,7 +5021,7 @@ static const u64 cfs_bandwidth_slack_period = 5 * NSEC_PER_MSEC;
 static int runtime_refresh_within(struct cfs_bandwidth *cfs_b, u64 min_expire)
 {
 	struct hrtimer *refresh_timer = &cfs_b->period_timer;
-	s64 remaining;
+	u64 remaining;
 
 	/* if the call-back is running a quota refresh is already occurring */
 	if (hrtimer_callback_running(refresh_timer))
@@ -5029,7 +5029,7 @@ static int runtime_refresh_within(struct cfs_bandwidth *cfs_b, u64 min_expire)
 
 	/* is a quota refresh about to occur? */
 	remaining = ktime_to_ns(hrtimer_expires_remaining(refresh_timer));
-	if (remaining < (s64)min_expire)
+	if (remaining < min_expire)
 		return 1;
 
 	return 0;
@@ -5519,7 +5519,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
-	int task_new = !(flags & ENQUEUE_WAKEUP);
 	bool prefer_idle = sched_feat(EAS_PREFER_IDLE) ?
 				(schedtune_prefer_idle(p) > 0) : 0;
 	int idle_h_nr_running = idle_policy(p->policy);
@@ -5617,7 +5616,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		 * overutilized. Hopefully the cpu util will be back to
 		 * normal before next overutilized check.
 		 */
-		if (!task_new && !(prefer_idle && rq->nr_running == 1))
+		if ((flags & ENQUEUE_WAKEUP) && !(prefer_idle && rq->nr_running == 1))
 			update_overutilized_status(rq);
 	}
 
@@ -12372,22 +12371,16 @@ static void propagate_entity_cfs_rq(struct sched_entity *se)
 {
 	struct cfs_rq *cfs_rq;
 
-	list_add_leaf_cfs_rq(cfs_rq_of(se));
-
 	/* Start to propagate at parent */
 	se = se->parent;
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
 
-		if (!cfs_rq_throttled(cfs_rq)){
-			update_load_avg(cfs_rq, se, UPDATE_TG);
-			list_add_leaf_cfs_rq(cfs_rq);
-			continue;
-		}
-
-		if (list_add_leaf_cfs_rq(cfs_rq))
+		if (cfs_rq_throttled(cfs_rq))
 			break;
+
+		update_load_avg(cfs_rq, se, UPDATE_TG);
 	}
 }
 #else
