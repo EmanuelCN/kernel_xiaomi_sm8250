50a5efd1898ea4b5b64694913ff9b09978e79e22
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-25 16:14:23 +0530

    msm: vidc_3x: Remove pm_qos usage
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/media/platform/msm/vidc_3x/msm_vidc_res_parse.c
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_res_parse.c
@@ -1341,9 +1341,6 @@ int read_platform_resources_from_dt(
 	dprintk(VIDC_DBG, "Power collapse supported = %s\n",
 		res->sw_power_collapsible ? "yes" : "no");
 
-	of_property_read_u32(pdev->dev.of_node,
-			"qcom,pm-qos-latency-us", &res->pm_qos_latency_us);
-
 	res->slave_side_cp = of_property_read_bool(pdev->dev.of_node,
 					"qcom,slave-side-cp");
 	dprintk(VIDC_DBG, "Slave side cp = %s\n",
