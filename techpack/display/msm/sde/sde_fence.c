35c05ad4128a038375476d60244048022ca89c0a
Juhyung Park <qkrwngud825@gmail.com>
2021-10-02 17:35:56 +0900

    techpack: display: Remove sde_fence_context->name
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -110,14 +110,12 @@ static inline struct sde_fence *to_sde_fence(struct dma_fence *fence)
 
 static const char *sde_fence_get_driver_name(struct dma_fence *fence)
 {
-	return "sde_fence";
+	return "sde";
 }
 
 static const char *sde_fence_get_timeline_name(struct dma_fence *fence)
 {
-	struct sde_fence *f = to_sde_fence(fence);
-
-	return f->ctx->name;
+	return "timeline";
 }
 
 static bool sde_fence_enable_signaling(struct dma_fence *fence)
@@ -208,8 +206,6 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	/* create fd */
 	fd = get_unused_fd_start_flags(1, 0);
 	if (unlikely(fd < 0)) {
-		SDE_ERROR("failed to get_unused_fd_flags(), sde_fence:%s:%u\n",
-			  sde_fence->ctx->name, val);
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
@@ -219,8 +215,6 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	if (unlikely(sync_file == NULL)) {
 		put_unused_fd(fd);
 		fd = -EINVAL;
-		SDE_ERROR("couldn't create fence, sde_fence:%s:%u\n",
-			  sde_fence->ctx->name, val);
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
@@ -252,7 +246,6 @@ struct sde_fence_context *sde_fence_init(const char *name, uint32_t drm_id)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
 	ctx->context = dma_fence_context_alloc(1);
