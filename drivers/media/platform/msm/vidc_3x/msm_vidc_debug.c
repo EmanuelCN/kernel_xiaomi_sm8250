95c37c662b463727153fad7d14149920ca9d23aa
celtare21 <celtare21@gmail.com>
2021-10-09 23:09:49 +0530

    vidc_3x: Disable msm_vidc_fw_debug_mode
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: farizmaul <ricoayuba88@gmail.com>

--- drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
@@ -21,7 +21,7 @@
 int msm_vidc_debug = VIDC_ERR | VIDC_WARN;
 int msm_vidc_debug_out = VIDC_OUT_PRINTK;
 int msm_vidc_fw_debug = 0x18;
-int msm_vidc_fw_debug_mode = 1;
+int msm_vidc_fw_debug_mode = 0;
 int msm_vidc_fw_low_power_mode = 1;
 int msm_vidc_hw_rsp_timeout = 1000;
 bool msm_vidc_fw_coverage = true;
