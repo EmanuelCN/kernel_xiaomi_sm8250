b6e1cc6ea47275b7a48393c0d150061a8c4da1d7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-10-18 05:57:40 +0600

    Revert "locking/rwsem: Don't hog RCU read lock while optimistically spinning"
    Abandon this for upstream changes.
    
    This reverts commit 8534b5725b1b2c4d277593967f559842754d00cd.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/locking/rwsem-xadd.c
+++ kernel/locking/rwsem-xadd.c
@@ -277,35 +277,31 @@ static noinline bool rwsem_spin_on_owner(struct rw_semaphore *sem)
 {
 	struct task_struct *owner = READ_ONCE(sem->owner);
 
-	if (!owner || !is_rwsem_owner_spinnable(owner))
+	if (!is_rwsem_owner_spinnable(owner))
 		return false;
 
-	while (true) {
-		bool on_cpu, same_owner;
-
+	rcu_read_lock();
+	while (owner && (READ_ONCE(sem->owner) == owner)) {
 		/*
-		 * Ensure sem->owner still matches owner. If that fails,
+		 * Ensure we emit the owner->on_cpu, dereference _after_
+		 * checking sem->owner still matches owner, if that fails,
 		 * owner might point to free()d memory, if it still matches,
 		 * the rcu_read_lock() ensures the memory stays valid.
 		 */
-		rcu_read_lock();
-		same_owner = sem->owner == owner;
-		if (same_owner)
-			on_cpu = owner_on_cpu(owner);
-		rcu_read_unlock();
-
-		if (!same_owner)
-			break;
+		barrier();
 
 		/*
 		 * abort spinning when need_resched or owner is not running or
 		 * owner's cpu is preempted.
 		 */
-		if (!on_cpu || need_resched())
+		if (need_resched() || !owner_on_cpu(owner)) {
+			rcu_read_unlock();
 			return false;
+		}
 
 		cpu_relax();
 	}
+	rcu_read_unlock();
 
 	/*
 	 * If there is a new owner or the owner is not set, we continue
