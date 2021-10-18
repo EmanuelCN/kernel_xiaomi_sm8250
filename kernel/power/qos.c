0916c59d38c4f29537206b39bfbce22a4832e340
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-17 23:50:29 -0700

    qos: Fix PM QoS requests almost never shutting off
    Andrzej Perczak discovered that his CPUs would almost never enter an
    idle state deeper than C0, and pinpointed the cause of the issue to be
    commit "qos: Speed up pm_qos_set_value_for_cpus()". As it turns out, the
    optimizations introduced in that commit contain two issues that are
    responsible for this behavior: pm_qos_remove_request() fails to refresh
    the affected per-CPU targets, and IRQ migrations fail to refresh their
    old affinity's targets.
    
    Removing a request fails to refresh the per-CPU targets because
    `new_req->node.prio` isn't updated to the PM QoS class' default value
    upon removal, and so it contains its old value from when it was active.
    This causes the `changed` loop in pm_qos_set_value_for_cpus() to check
    against a stale PM QoS request value and erroneously determine that the
    request in question doesn't alter the current per-CPU targets.
    
    As for IRQ migrations, only the new CPU affinity mask gets updated,
    which causes the CPUs present in the old affinity mask but not the new
    one to retain their targets, specifically when a migration occurs while
    the associated PM QoS request is active.
    
    To fix these issues while retaining optimal speed, update PM QoS
    requests' CPU affinity inside pm_qos_set_value_for_cpus() so that the
    old affinity can be known, and skip the `changed` loop when the request
    in question is being removed.
    
    Reported-by: Andrzej Perczak <kartapolska@gmail.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -269,14 +269,16 @@ static const struct file_operations pm_qos_debug_fops = {
 
 static inline int pm_qos_set_value_for_cpus(struct pm_qos_request *new_req,
 					    struct pm_qos_constraints *c,
-					    unsigned long *cpus, bool dev_req)
+					    unsigned long *cpus,
+					    unsigned long new_cpus,
+					    enum pm_qos_req_action new_action,
+						bool dev_req)
 {
 	s32 qos_val[NR_CPUS] = {
 		[0 ... (NR_CPUS - 1)] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
 	};
 	struct pm_qos_request *req;
 	unsigned long new_req_cpus;
-	bool changed = false;
 	int cpu;
 
 	/*
@@ -299,16 +301,27 @@ static inline int pm_qos_set_value_for_cpus(struct pm_qos_request *new_req,
 	if (dev_req)
 		return 0;
 
-	new_req_cpus = atomic_read(&new_req->cpus_affine);
-	for_each_cpu(cpu, to_cpumask(&new_req_cpus)) {
-		if (c->target_per_cpu[cpu] != new_req->node.prio) {
-			changed = true;
-			break;
-		}
+	if (new_cpus) {
+		/* cpus_affine changed, so the old CPUs need to be refreshed */
+		new_req_cpus = atomic_read(&new_req->cpus_affine) | new_cpus;
+		atomic_set(&new_req->cpus_affine, new_cpus);
+	} else {
+		new_req_cpus = atomic_read(&new_req->cpus_affine);
 	}
 
-	if (!changed)
-		return 0;
+	if (new_action != PM_QOS_REMOVE_REQ) {
+		bool changed = false;
+
+		for_each_cpu(cpu, to_cpumask(&new_req_cpus)) {
+			if (c->target_per_cpu[cpu] != new_req->node.prio) {
+				changed = true;
+				break;
+			}
+		}
+
+		if (!changed)
+			return 0;
+	}
 
 	plist_for_each_entry(req, &c->list, node) {
 		unsigned long affected_cpus;
@@ -333,19 +346,11 @@ static inline int pm_qos_set_value_for_cpus(struct pm_qos_request *new_req,
 	return 0;
 }
 
-/**
- * pm_qos_update_target - manages the constraints list and calls the notifiers
- *  if needed
- * @c: constraints data struct
- * @node: request to add to the list, to update or to remove
- * @action: action to take on the constraints list
- * @value: value of the request to add or update
- *
- * This function returns 1 if the aggregated constraint value has changed, 0
- *  otherwise.
- */
-int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
-			 enum pm_qos_req_action action, int value, bool dev_req)
+static int pm_qos_update_target_cpus(struct pm_qos_constraints *c,
+				     struct plist_node *node,
+				     enum pm_qos_req_action action, int value,
+				     unsigned long new_cpus,
+					 bool dev_req)
 {
 	struct pm_qos_request *req = container_of(node, typeof(*req), node);
 	int prev_value, curr_value, new_value;
@@ -382,7 +387,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 
 	curr_value = pm_qos_get_value(c);
 	pm_qos_set_value(c, curr_value);
-	ret = pm_qos_set_value_for_cpus(req, c, &cpus, dev_req);
+	ret = pm_qos_set_value_for_cpus(req, c, &cpus, new_cpus, action, dev_req);
 
 	spin_unlock(&pm_qos_lock);
 
@@ -403,6 +408,23 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 		ret = 0;
 	}
 	return ret;
+}
+
+/**
+ * pm_qos_update_target - manages the constraints list and calls the notifiers
+ *  if needed
+ * @c: constraints data struct
+ * @node: request to add to the list, to update or to remove
+ * @action: action to take on the constraints list
+ * @value: value of the request to add or update
+ *
+ * This function returns 1 if the aggregated constraint value has changed, 0
+ *  otherwise.
+ */
+int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
+			 enum pm_qos_req_action action, int value, bool dev_req)
+{
+	return pm_qos_update_target_cpus(c, node, action, value, 0, dev_req);
 }
 
 /**
@@ -549,9 +571,8 @@ static void pm_qos_irq_release(struct kref *ref)
 	struct pm_qos_constraints *c =
 				pm_qos_array[req->pm_qos_class]->constraints;
 
-	atomic_set(&req->cpus_affine, CPUMASK_ALL);
-	pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ,
-			c->default_value, false);
+	pm_qos_update_target_cpus(c, &req->node, PM_QOS_UPDATE_REQ,
+				  c->default_value, CPUMASK_ALL, false);
 }
 
 static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
@@ -562,8 +583,8 @@ static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
 	struct pm_qos_constraints *c =
 				pm_qos_array[req->pm_qos_class]->constraints;
 
-	atomic_set(&req->cpus_affine, *cpumask_bits(mask));
-	pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ, req->node.prio, false);
+	pm_qos_update_target_cpus(c, &req->node, PM_QOS_UPDATE_REQ,
+				  req->node.prio, *cpumask_bits(mask), false);
 }
 #endif
 
