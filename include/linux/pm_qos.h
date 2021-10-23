0ff48c497c0e97b57a8e53459f362cd918c53f61
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-24 00:07:02 +0530

    qos: Change cpus_affine to not be atomic
    There isn't a need for cpus_affine to be atomic, and reading/writing to
    it outside of the global pm_qos lock is racy anyway. As such, we can
    simply turn it into a primitive integer type.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -54,8 +54,8 @@ enum pm_qos_req_type {
 };
 
 struct pm_qos_request {
+	unsigned long cpus_affine;
 	enum pm_qos_req_type type;
-	atomic_t cpus_affine;
 #ifdef CONFIG_SMP
 	uint32_t irq;
 	/* Internal structure members */
