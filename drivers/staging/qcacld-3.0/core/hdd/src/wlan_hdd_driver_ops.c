1d6c6be8bbebef2349aeb151260f6dd6727e97ea
Arun Kumar Khandavalli <quic_akhandav@quicinc.com>
2021-11-01 15:44:32 +0530

    BACKPORT: qcacld-3.0: unblock the state control param thread at end of SSR
    Currently the state control param thread is unblocked 1st and then
    the psoc transition is completed, there could be a chance that SSR
    thread got scheduled out and wifi thread tried to comeup and issue
    interface up which could fail because psoc transition is till in
    progress.
    
    So, unblock the wifi thread waiting on state control param after
    stopping the psoc transition at the end of SSR.
    
    Change-Id: I4631fb9aae6a5416ba70b24fe3c053528bd3b7d9
    CRs-Fixed: 3066968
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -662,7 +663,6 @@ static int __hdd_soc_recovery_reinit(struct device *dev,
 	}
 
 	hdd_soc_load_unlock(dev);
-	hdd_start_complete(0);
 
 	return 0;
 
@@ -713,6 +713,7 @@ static int hdd_soc_recovery_reinit(struct device *dev,
 
 
 	osif_psoc_sync_trans_stop(psoc_sync);
+	hdd_start_complete(0);
 
 	return errno;
 }
