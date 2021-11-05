1b83cc6dee3759d2a05f4d27ebe939fad9bacab9
EcrosoftXiao <xy19990903@gmail.com>
2021-11-05 11:26:12 +0800

    techpack: display: Make dimlayer exposure switchable
    [Ayrton: Adapt to non fod devices, define sysfs attributes for switch]

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -243,6 +243,9 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+#ifdef CONFIG_DRM_SDE_EXPO
+	bool dimlayer_exposure;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
