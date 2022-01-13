6b335fe7c47455468fae9967d253f314dff0525a
Andrzej Perczak <linux@andrzejperczak.com>
2022-01-14 00:43:29 +0100

    mm: Tune parameters for Android
    Since Infinity Kernel is released as standalone package other ROMs can
    lack mm tuning done in rootdir. For this reason move those tweaks
    directly to kernel.
    
    Note that dirty_writeback_centisecs and drity_background_ratio have to be
    restricted from writing because Android < 12 modifies these values badly
    on boot.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- mm/swap.c
+++ mm/swap.c
@@ -1089,13 +1089,9 @@ EXPORT_SYMBOL(pagevec_lookup_range_nr_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
+	/* Tweak for Android devices using zram */
+	page_cluster = 0;
 
-	/* Use a smaller cluster for small-memory machines */
-	if (megs < 16)
-		page_cluster = 2;
-	else
-		page_cluster = 3;
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
