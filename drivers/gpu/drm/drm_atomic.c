b44bb41f8340047d16c6ffefdc87a16807003aba
EmanuelCN <emanuelghub@gmail.com>
2022-01-16 01:19:56 +0400

    Revert "sched/boost: Boost CPU and DDR bus when sched_boost is triggered"
    This reverts commit 409f8cc893d3c486f0e895c43bebb89a9d5e377a.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -32,9 +32,7 @@
 #include <drm/drm_print.h>
 #include <drm/drm_writeback.h>
 #include <linux/sync_file.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
-#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2603,8 +2601,6 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-		if (sysctl_sched_boost)
-			cpu_input_boost_kick();
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
