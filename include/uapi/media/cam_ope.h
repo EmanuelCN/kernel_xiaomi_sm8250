3cce45b5d02678dc050a1817732c90364f98aa39
Nirmal Abraham <quic_c_nabrah@quicinc.com>
2022-01-27 14:35:06 +0530

    msm: camera: update copyright markings
    Update QUIC copyright for new code changes.
    
    CRs-Fixed: 3118778
    Change-Id: I9e3631cc86c5e10e4e2020d4a9b2264ea282d437
    Signed-off-by: Nirmal Abraham <quic_c_nabrah@quicinc.com>

--- include/uapi/media/cam_ope.h
+++ include/uapi/media/cam_ope.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 /*
- * Copyright (c) 2019-2020, 2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __UAPI_OPE_H__
