c5fb6bd8e7d4fc5b1c4e10d0b310482e87df4e4c
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-06 10:19:39 +0900

    drm/msm: sde_hw_top: Replace atomic cmpxchg with non-atomic variant
    Sultan: "though a cmpxchg isn't needed there"
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I5835fb1e33c1fc79e8087e7aad0b12ae731277cb
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- techpack/display/msm/sde/sde_hw_top.c
+++ techpack/display/msm/sde/sde_hw_top.c
@@ -194,9 +194,10 @@ static bool sde_hw_setup_clk_force_ctrl(struct sde_hw_mdp *mdp,
 		return false;
 
 	ctrl_reg = (struct sde_clk_ctrl_reg *)&mdp->caps->clk_ctrls[clk_ctrl];
-	if (cmpxchg(&ctrl_reg->val, !enable, enable) == enable)
+	if (ctrl_reg->val == enable)
 		return enable;
 
+	ctrl_reg->val = enable;
 	reg_off = ctrl_reg->reg_off;
 	bit_off = ctrl_reg->bit_off;
 
