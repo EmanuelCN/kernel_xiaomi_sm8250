30db7630eed6cb863e4a7812164de58eaa6007b5
EmanuelCN <emanuelghub@gmail.com>
2022-03-17 23:02:53 +0100

    lib: bitmap: Fix git derps

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -622,11 +622,6 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 	return kvmalloc_array(n, size, flags | __GFP_ZERO);
 }
 
-static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
-{
-	return kvmalloc_array(n, size, flags | __GFP_ZERO);
-}
-
 extern void kvfree(const void *addr);
 extern void kvfree_sensitive(const void *addr, size_t len);
 
