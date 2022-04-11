eb49c1f432fa4ffc246bb0677204845678b0e2c1
Reinazhard <reinazhard@gmail.com>
2022-04-11 12:07:21 +0200

    Revert "mm: allocate swapin pages from movable zone"
    This reverts commit 84da19e51f242803b532d1256f87565d29a29506.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3322,8 +3322,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 
 	if (!page) {
 		if (skip_swapcache) {
-			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE | __GFP_CMA,
-					      vma, vmf->address);
+			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma,
+							vmf->address);
 			if (page) {
 				__SetPageLocked(page);
 				__SetPageSwapBacked(page);
