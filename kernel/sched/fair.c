ca2c5648674cc682306da05da1b724ac42f2542f
EmanuelCN <emanuelghub@gmail.com>
2022-04-15 21:56:01 +0200

    kernel: sched: Tune scheduler capacity margins

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -166,16 +166,22 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  * (default: ~20%)
  */
 unsigned int capacity_margin				= 1280;
-unsigned int sched_capacity_margin_up[CPU_NR] = {
-			[0 ... CPU_NR-1] = 1078}; /* ~5% margin */
-unsigned int sched_capacity_margin_down[CPU_NR] = {
-			[0 ... CPU_NR-1] = 1205}; /* ~15% margin */
-unsigned int sched_capacity_margin_up_boosted[CPU_NR] = {
-	3658, 3658, 3658, 3658, 3658, 3658, 1078, 1024
-}; /* 72% margin for small, 5% for big, 0% for big+ */
-unsigned int sched_capacity_margin_down_boosted[CPU_NR] = {
+unsigned int sched_capacity_margin_up[CPU_NR] =
+{
+	[0 ... CPU_NR - 1] = 1078 // ~5% margin
+};
+unsigned int sched_capacity_margin_down[CPU_NR] =
+{
+	[0 ... CPU_NR - 1] = 1078 // ~5% margin
+};
+unsigned int sched_capacity_margin_up_boosted[CPU_NR] =
+{
 	3658, 3658, 3658, 3658, 3658, 3658, 3658, 3658
-}; /* not used for small cores, 72% margin for big, 72% margin for big+ */
+}; // 72% margin for small, 5% for big, 0% for big+
+unsigned int sched_capacity_margin_down_boosted[CPU_NR] =
+{
+	3658, 3658, 3658, 3658, 3658, 3658, 3658, 3658
+}; // not used for small cores, 72% margin for big, 72% margin for big+
 
 /* 1ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_boost = 51;
