7c6a4e6d9d3cfa052d0fe89d1d6e1b1474387dad
EmanuelCN <emanuelghub@gmail.com>
2022-04-24 00:32:59 +0000

    Revert "Revert "kernel: Boost CPU when rendering a new frame""
    This reverts commit 4c00471d13311f9aa935fe604e3761ed845fec41.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,6 +34,7 @@
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/pm_qos.h>
 
 #include "drm_crtc_internal.h"
@@ -2602,6 +2603,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 
 	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 	}
 
