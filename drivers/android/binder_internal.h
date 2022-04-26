7258e0de43f38d516d7dd18228c3287bb2bc3b10
EmanuelCN <emanuelghub@gmail.com>
2022-04-26 18:39:14 +0200

    Revert "kernel: Add support for miui optimization features"
    This reverts commit b38084cfa674b51dd2a655618bd586b5a4f66489.

--- drivers/android/binder_internal.h
+++ drivers/android/binder_internal.h
@@ -60,8 +60,6 @@ struct binderfs_mount_opts {
  * @device_count:   The current number of allocated binder devices.
  * @proc_log_dir:   Pointer to the directory dentry containing process-specific
  *                  logs.
- * @proc_transaction_log_dir:   Pointer to the directory dentry containing miui binder
- *                  logs.
  */
 struct binderfs_info {
 	struct ipc_namespace *ipc_ns;
@@ -71,9 +69,6 @@ struct binderfs_info {
 	struct binderfs_mount_opts mount_opts;
 	int device_count;
 	struct dentry *proc_log_dir;
-#if IS_ENABLED(CONFIG_MIHW)
-	struct dentry *proc_transaction_log_dir;
-#endif
 };
 
 extern const struct file_operations binder_fops;
