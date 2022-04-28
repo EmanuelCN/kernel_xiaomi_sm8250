d1b6319f3de15abb92a85f6394a06f3baf461a6e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-28 19:22:16 +0530

    Revert "mm: VMA sequence count"
    This reverts commit 88a78dc2863d3710b68934bb25ef560fecf2d6b2.
    
    Change-Id: I5174b4520760ed1f40163740169344ee3d3a3bb1
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -698,10 +698,6 @@ int __vma_adjust(struct vm_area_struct *vma, unsigned long start,
 	long adjust_next = 0;
 	int remove_next = 0;
 
-	vm_write_begin(vma);
-	if (next)
-		vm_write_begin(next);
-
 	if (next && !insert) {
 		struct vm_area_struct *exporter = NULL, *importer = NULL;
 
@@ -912,7 +908,6 @@ again:
 			anon_vma_merge(vma, next);
 		mm->map_count--;
 		mpol_put(vma_policy(next));
-		vm_write_end(next);
                 vm_area_free(next);
 		if (next == mm->stack_vma)
 			mm->stack_vma = NULL;
@@ -929,8 +924,6 @@ again:
 			 * "vma->vm_next" gap must be updated.
 			 */
 			next = vma->vm_next;
-			if (next)
-				vm_write_begin(next);
 		} else {
 			/*
 			 * For the scope of the comment "next" and
@@ -976,10 +969,6 @@ again:
 	}
 	if (insert && file)
 		uprobe_mmap(insert);
-
-	if (next && next != vma)
-		vm_write_end(next);
-	vm_write_end(vma);
 
 	validate_mm(mm);
 
