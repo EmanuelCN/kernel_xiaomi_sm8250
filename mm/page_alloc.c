58603d96f724d77485ff798a2ad7cd9ccc1df639
EmanuelCN <emanuelghub@gmail.com>
2022-05-01 20:41:58 +0200

    Revert "mm: Don't warn on page allocation failures for OOM-killed processes"
    This reverts commit 7953175f028461de5a9ff24c70af43ab8a96cefa.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4725,10 +4725,8 @@ retry:
 	/* Avoid allocations with no watermarks from looping endlessly */
 	if (tsk_is_oom_victim(current) &&
 	    (alloc_flags == ALLOC_OOM ||
-	     (gfp_mask & __GFP_NOMEMALLOC))) {
-		gfp_mask |= __GFP_NOWARN;
+	     (gfp_mask & __GFP_NOMEMALLOC)))
 		goto nopage;
-	}
 
 	/* Retry as long as the OOM killer is making progress */
 	if (did_some_progress) {
