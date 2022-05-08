cb682ab781794def427939bc72a4e40bb1d56331
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-08 13:24:36 +0200

    techpack: msm: Remove bogus NULL check in _msm_drm_commit_work_cb() The work pointer will never be NULL. Remove this check.

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -547,17 +547,10 @@ static void complete_commit(struct msm_commit *c)
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
-	struct msm_commit *commit = NULL;
+	struct msm_commit *commit = container_of(work, typeof(*ccommit),
+						 commit_work);
 	ktime_t start, end;
 	s64 duration;
-
-	if (!work) {
-		DRM_ERROR("%s: Invalid commit work data!\n", __func__);
-		return;
-	}
-
-	commit = container_of(work, struct msm_commit, commit_work);
-
 	start = ktime_get();
 	frame_stat_collector(0, COMMIT_START_TS);
 
