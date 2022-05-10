04abe647938cba622b02505719f63a88b7adea79
John Galt <johngaltfirstrun@gmail.com>
2022-05-10 16:59:41 -0400

    fuse: force new feature on
    We know our users, no need to use flag.

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1546,7 +1546,9 @@ static ssize_t fuse_direct_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	struct fuse_file *ff = file->private_data;
 	struct fuse_io_priv io = FUSE_IO_PRIV_SYNC(iocb);
 	ssize_t res;
-	bool p_write = ff->open_flags & FOPEN_PARALLEL_WRITES ? true : false;
+	//HACK: due to lacking support in Android rom, and no issues without locking
+	//bool p_write = ff->open_flags & FOPEN_PARALLEL_WRITES ? true : false;
+	bool p_write = true;
 	bool exclusive_lock = !p_write ||
                        fuse_direct_write_extending_i_size(iocb, from) ?
                        true : false;
