8c9b15b263538640596ca5df5830fbdd122a185c
EmanuelCN <emanuelghub@gmail.com>
2022-05-11 22:51:19 +0200

    Revert "dma_buf: try to use kmem_cache pool for dmabuf allocations"
    This reverts commit d2320bec6b9cc5027d4fd951d76a21748d618b44.

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -49,15 +49,10 @@
 static atomic_long_t name_counter;
 
 static struct kmem_cache *kmem_attach_pool;
-static struct kmem_cache *kmem_dma_buf_pool;
 
 void __init init_dma_buf_kmem_pool(void)
 {
 	kmem_attach_pool = KMEM_CACHE(dma_buf_attachment, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-	kmem_dma_buf_pool = kmem_cache_create("dma_buf",
-		(sizeof(struct dma_buf) + sizeof(struct reservation_object)),
-		(sizeof(struct dma_buf) + sizeof(struct reservation_object)),
-		SLAB_HWCACHE_ALIGN | SLAB_PANIC, NULL);
 }
 
 static inline int is_dma_buf_file(struct file *);
@@ -86,10 +81,7 @@ static void dmabuf_dent_put(struct dma_buf *dmabuf)
 {
 	if (atomic_dec_and_test(&dmabuf->dent_count)) {
 		kfree(dmabuf->name);
-		if (dmabuf->from_kmem)
-			kmem_cache_free(kmem_dma_buf_pool, dmabuf);
-		else
-			kfree(dmabuf);
+		kfree(dmabuf);
 	}
 }
 
@@ -601,7 +593,6 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 	char *bufname;
 	int ret;
 	long cnt;
-	bool from_kmem;
 
 	if (!exp_info->resv)
 		alloc_size += sizeof(struct reservation_object);
@@ -629,16 +620,7 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 		goto err_module;
 	}
 
-	from_kmem = (alloc_size ==
-		     (sizeof(struct dma_buf) + sizeof(struct reservation_object)));
-
-	if (from_kmem) {
-		dmabuf = kmem_cache_zalloc(kmem_dma_buf_pool, GFP_KERNEL);
-		dmabuf->from_kmem = true;
-	} else {
-		dmabuf = kzalloc(alloc_size, GFP_KERNEL);
-	}
-
+	dmabuf = kzalloc(alloc_size, GFP_KERNEL);
 	if (!dmabuf) {
 		ret = -ENOMEM;
 		goto err_name;
@@ -686,10 +668,7 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 	return dmabuf;
 
 err_dmabuf:
-	if (from_kmem)
-		kmem_cache_free(kmem_dma_buf_pool, dmabuf);
-	else
-		kfree(dmabuf);
+	kfree(dmabuf);
 err_name:
 	kfree(bufname);
 err_module:
