56d1b82714a937a73dd954d7365bcae9f21bc74d
EmanuelCN <emanuelghub@gmail.com>
2022-05-16 07:45:39 +0000

    Revert "f2fs: print the details of slow fsync"
    This reverts commit 1a20f974e77ece7be3f04df56ba53a31aa5873c5.

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -19,7 +19,6 @@
 #include <linux/pagevec.h>
 #include <linux/uio.h>
 #include <linux/uuid.h>
-#include <linux/uidgid.h>
 #include <linux/file.h>
 #include <linux/nls.h>
 
@@ -264,8 +263,6 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 		.for_reclaim = 0,
 	};
 	unsigned int seq_id = 0;
-	ktime_t start_time, delta;
-	unsigned long long duration;
 
 #if defined(CONFIG_UFSTW)
 	bool turbo_set = false;
@@ -285,7 +282,6 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 		trace_android_fs_fsync_start(inode,
 				current->pid, path, current->comm);
 	}
-	start_time = ktime_get();
 
 	if (S_ISDIR(inode->i_mode))
 		goto go_write;
@@ -399,24 +395,6 @@ out:
 	if (turbo_set)
 		bdev_clear_turbo_write(sbi->sb->s_bdev);
 #endif
-	delta = ktime_sub(ktime_get(), start_time);
-	duration = (unsigned long long) ktime_to_ns(delta) / (1000 * 1000);
-
-	/* print slow fsync spend more than 1s */
-	if (duration > 1000) {
-		char *file_path, file_pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		file_path = android_fstrace_get_pathname(file_pathbuf,
-				MAX_TRACE_PATHBUF_LEN, inode);
-		pr_info("[f2fs] slow fsync: %llu ms, cp_reason: %s, "
-			"datasync = %d, ret = %d, comm: %s: (uid %u, gid %u), "
-			"entry: %s", duration, f2fs_cp_reasons[cp_reason],
-			datasync, ret, current->comm,
-			from_kuid_munged(&init_user_ns, current_fsuid()),
-			from_kgid_munged(&init_user_ns, current_fsgid()),
-			file_path);
-	}
-
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
 	f2fs_trace_ios(NULL, 1);
 	stat_inc_sync_file_count(sbi);
