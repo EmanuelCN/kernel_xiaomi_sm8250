eb1b11d30b6c16dc132412242f531ef24e7dd013
EmanuelCN <emanuelghub@gmail.com>
2022-05-22 01:36:56 +0200

    Revert "f2fs: Create f2fs root statistics using procfs"
    This reverts commit b55c37a640923681261d7b32d7549c95c4b262e7.

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -3,7 +3,6 @@
  * f2fs sysfs interface
  *
  * Copyright (c) 2012 Samsung Electronics Co., Ltd.
- * Copyright (C) 2021 XiaoMi, Inc.
  *             http://www.samsung.com/
  * Copyright (c) 2017 Chao Yu <chao@kernel.org>
  */
@@ -21,7 +20,7 @@
 #include "iostat.h"
 #include <trace/events/f2fs.h>
 
-struct proc_dir_entry *f2fs_proc_root;
+static struct proc_dir_entry *f2fs_proc_root;
 
 /* Sysfs support for f2fs */
 enum {
