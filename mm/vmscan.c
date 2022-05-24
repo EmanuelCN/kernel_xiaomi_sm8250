52523be0557c2bc0e672b956e44f4ba1faa42cf1
Juhyung Park <qkrwngud825@gmail.com>
2022-05-24 21:58:29 +0900

    mm: multi-gen LRU: call simple LMK if enabled
    OOM killer is disabled when simple LMK is enabled, so call it instead.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -54,6 +54,7 @@
 #include <linux/shmem_fs.h>
 #include <linux/ctype.h>
 #include <linux/debugfs.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -3968,15 +3969,21 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 	 * younger than min_ttl. However, another theoretical possibility is all
 	 * memcgs are either below min or empty.
 	 */
-	if (!success && mutex_trylock(&oom_lock)) {
-		struct oom_control oc = {
-			.gfp_mask = sc->gfp_mask,
-			.order = sc->order,
-		};
+	if (!success) {
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
+		simple_lmk_trigger();
+#else
+		if (mutex_trylock(&oom_lock)) {
+			struct oom_control oc = {
+				.gfp_mask = sc->gfp_mask,
+				.order = sc->order,
+			};
 
-		out_of_memory(&oc);
+			out_of_memory(&oc);
 
-		mutex_unlock(&oom_lock);
+			mutex_unlock(&oom_lock);
+		}
+#endif
 	}
 }
 
