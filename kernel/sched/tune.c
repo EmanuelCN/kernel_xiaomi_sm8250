96a54ddf36d8049626a56bf958af2a0e14a6541f
EmanuelCN <emanuelghub@gmail.com>
2022-05-30 22:52:55 +0300

    Revert "sched/tune: Increase the cgroup limit to 8"
    This reverts commit 4f9da92bbb4e046a9fda491baca1148c0826c06e.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -168,7 +168,7 @@ root_schedtune = {
  *    implementation especially for the computation of the per-CPU boost
  *    value
  */
-#define BOOSTGROUPS_COUNT 8
+#define BOOSTGROUPS_COUNT 6
 
 /* Array of configured boostgroups */
 static struct schedtune *allocated_group[BOOSTGROUPS_COUNT] = {
