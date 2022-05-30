151cb0353f3ae02ac9933e563fb1fff6626e1d08
EmanuelCN <emanuelghub@gmail.com>
2022-05-30 22:19:03 +0300

    Revert "mm: drop sched migrate api"
    This reverts commit b4b802c824fbead36a4641603eeb60021f7f5fa1.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1824,6 +1824,11 @@ static inline bool cpupri_check_rt(void)
 }
 #endif
 
+void sched_migrate_to_cpumask_start(struct cpumask *old_mask,
+				    const struct cpumask *dest);
+void sched_migrate_to_cpumask_end(const struct cpumask *old_mask,
+				  const struct cpumask *dest);
+
 #ifndef cpu_relax_yield
 #define cpu_relax_yield() cpu_relax()
 #endif
