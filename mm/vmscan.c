b917db2d94f0aa6e03abc309033c69bca6ca9d13
EmanuelCN <emanuelghub@gmail.com>
2022-06-03 19:06:29 +0200

    Revert "mm: move variable under a proper #ifdef"
    This reverts commit 158544263ba2c9bf18f2ea41cc19b69d6d409ad2.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2277,11 +2277,11 @@ static bool inactive_list_is_low(struct lruvec *lruvec, bool file,
 				 struct scan_control *sc, bool trace)
 {
 	enum lru_list active_lru = file * LRU_FILE + LRU_ACTIVE;
+	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
 	enum lru_list inactive_lru = file * LRU_FILE;
 	unsigned long inactive, active;
 	unsigned long inactive_ratio;
 #ifndef CONFIG_FIX_INACTIVE_RATIO
-	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
 	unsigned long refaults;
 	unsigned long gb;
 #endif
