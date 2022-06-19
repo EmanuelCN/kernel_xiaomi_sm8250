2e4db807002120c502274411a5b08025d627c1be
Chenyang Zhong <zhongcy95@gmail.com>
2022-06-18 22:29:15 -0400

    lib/lz4: fix token in slow decompress loop
    Looks like Chenxi Mao missed a line in the non-fast-decode path
    when doing the backport.
    
    ref:
    https://github.com/lz4/lz4/blob/v1.9.3/lib/lz4.c#L1930
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -399,6 +399,7 @@ static FORCE_INLINE int __LZ4_decompress_generic(
 #endif
 	/* Main Loop : decode sequences */
 	while (1) {
+		token = *ip++;
 		length = token >> ML_BITS;
 
 		/* ip < iend before the increment */
