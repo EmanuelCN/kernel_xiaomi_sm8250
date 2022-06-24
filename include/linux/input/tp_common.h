be2df147a0f7ca46dd11c94975962db167cb92bc
Ivan Vecera <ivan@cera.cz>
2022-06-24 07:52:34 +0200

    drivers: input: touchscreen: Extend common touchpanel interface by fod_status
    * Export fod status by common touchpanel interface

--- include/linux/input/tp_common.h
+++ include/linux/input/tp_common.h
@@ -13,3 +13,4 @@ struct tp_common_ops {
 int tp_common_set_capacitive_keys_ops(struct tp_common_ops *ops);
 int tp_common_set_double_tap_ops(struct tp_common_ops *ops);
 int tp_common_set_reversed_keys_ops(struct tp_common_ops *ops);
+int tp_common_set_fod_status_ops(struct tp_common_ops *ops);
