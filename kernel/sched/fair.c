b06b605885965889588806e47c1d11a16b63d6d2
EmanuelCN <emanuelghub@gmail.com>
2022-07-13 22:05:01 +0200

    sched: fair: Tune capacity margins

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -167,15 +167,17 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 unsigned int capacity_margin				= 1280;
 unsigned int sched_capacity_margin_up[CPU_NR] = {
-			[0 ... CPU_NR-1] = 1078}; /* ~5% margin */
+			[0 ... CPU_NR-1] = 1078
+};
 unsigned int sched_capacity_margin_down[CPU_NR] = {
-			[0 ... CPU_NR-1] = 1205}; /* ~15% margin */
+			[0 ... CPU_NR-1] = 1205
+};
 unsigned int sched_capacity_margin_up_boosted[CPU_NR] = {
-	3658, 3658, 3658, 3658, 3658, 3658, 1078, 1024
-}; /* 72% margin for small, 5% for big, 0% for big+ */
+	3658, 3658, 3658, 3658, 1078, 1078, 1078, 1024
+};
 unsigned int sched_capacity_margin_down_boosted[CPU_NR] = {
 	3658, 3658, 3658, 3658, 3658, 3658, 3658, 3658
-}; /* not used for small cores, 72% margin for big, 72% margin for big+ */
+};
 
 /* 1ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_boost = 51;
