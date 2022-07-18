c6a712ed1ae0c723031b670f6ee866a74f123f6c
EmanuelCN <emanuelghub@gmail.com>
2022-07-18 20:52:24 +0200

    treewide: Fix warnings

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1821,17 +1821,6 @@ urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 	return urandom_read_nowarn(file, buf, nbytes, ppos);
 }
 
-static ssize_t
-random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
-{
-	int ret;
-
-	ret = wait_for_random_bytes();
-	if (ret != 0)
-		return ret;
-	return urandom_read_nowarn(file, buf, nbytes, ppos);
-}
-
 static __poll_t
 random_poll(struct file *file, poll_table * wait)
 {
