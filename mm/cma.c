c072c7b93668c11341fe3473ac2747ab89ebd229
Kazuki Hashimoto <kazukih@tuta.io>
2022-07-31 03:52:44 +0900

    Revert "mm: cma: sleep between retries in cma_alloc"
    This reverts commit 8ffd0015dd9e44b597acd5c436d8de14de9de648.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/cma.c
+++ mm/cma.c
@@ -31,7 +31,6 @@
 #include <linux/highmem.h>
 #include <linux/io.h>
 #include <linux/kmemleak.h>
-#include <linux/delay.h>
 #include <linux/show_mem_notifier.h>
 #include <trace/events/cma.h>
 
@@ -459,7 +458,6 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 	size_t i;
 	struct page *page = NULL;
 	int ret = -ENOMEM;
-	int retry_after_sleep = 0;
 
 	if (!cma || !cma->count)
 		return NULL;
@@ -486,24 +484,8 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 				bitmap_maxno, start, bitmap_count, mask,
 				offset);
 		if (bitmap_no >= bitmap_maxno) {
-			if (retry_after_sleep < 2) {
-				start = 0;
-				/*
-				 * Page may be momentarily pinned by some other
-				 * process which has been scheduled out, eg.
-				 * in exit path, during unmap call, or process
-				 * fork and so cannot be freed there. Sleep
-				 * for 100ms and retry twice to see if it has
-				 * been freed later.
-				 */
-				mutex_unlock(&cma->lock);
-				msleep(100);
-				retry_after_sleep++;
-				continue;
-			} else {
-				mutex_unlock(&cma->lock);
-				break;
-			}
+			mutex_unlock(&cma->lock);
+			break;
 		}
 		bitmap_set(cma->bitmap, bitmap_no, bitmap_count);
 		/*
