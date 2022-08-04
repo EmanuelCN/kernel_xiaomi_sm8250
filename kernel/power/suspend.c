5e3525f793f95079704814cb974eac0eeec2aa3c
Alexander Winkowski <dereference23@outlook.com>
2022-08-04 09:15:25 +0000

    Revert "PM / suspend: Make cpuidle work in the "freeze" state"
    This reverts commit f3f125324fc1b8500cd20a2907628f7e5d88a708.
    
    Commit "cpuidle: don't disable cpuidle when entering suspend"
    did not address s2idle code path which broke the logic.
    
    Change-Id: Ib9e27891be7962de9c69c13a7148a15a3da0e1e0
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -16,7 +16,6 @@
 #include <linux/init.h>
 #include <linux/console.h>
 #include <linux/cpu.h>
-#include <linux/cpuidle.h>
 #include <linux/syscalls.h>
 #include <linux/gfp.h>
 #include <linux/io.h>
@@ -102,7 +101,6 @@ static void s2idle_enter(void)
 	raw_spin_unlock_irq(&s2idle_lock);
 
 	get_online_cpus();
-	cpuidle_resume();
 
 	/* Push all the CPUs into the idle loop. */
 	wake_up_all_idle_cpus();
@@ -110,7 +108,6 @@ static void s2idle_enter(void)
 	swait_event_exclusive(s2idle_wait_head,
 		    s2idle_state == S2IDLE_STATE_WAKE);
 
-	cpuidle_pause();
 	put_online_cpus();
 
 	raw_spin_lock_irq(&s2idle_lock);
