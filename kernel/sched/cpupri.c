cc1515ccacd848223eaebfa53e95e46281bffc27
John Stultz <jstultz@google.com>
2022-08-08 16:56:00 +0000

    BACKPORT: ANDROID: sched: Fix off-by-one with cpupri MAX_RT_PRIO evaluation
    This patch addresses an issue seen where SCHED_FIFO prio 99
    tasks were being woken up on a cpu where a long-running softirq
    was executing, and the RT task was not being migrated, causing
    long (10+ms wakeup latencies).
    
    Prior to upstream commit 934fc3314b39 ("sched/cpupri: Remap
    CPUPRI_NORMAL to MAX_RT_PRIO-1") the task->prio -> cpupri
    mapping is a little ackward.
    
    For RT tasks, its calculated as:
      cpupri = MAX_RT_PRIO - prio + 1;
    
    See:
    https://android.googlesource.com/kernel/common/+/refs/heads/android13-5.10/kernel/sched/cpupri.c#39
    
    This is added ontop of the also ackward detail that the
    task->prio is inverted (RT prio99 -> 0), means the cpupri
    mapping for RT tasks goes from 2->101. This makes it easy to
    evaluate the cpupri incorrectly.
    
    Which it turns out happened In commit 3adfd8e344ac ("ANDROID:
    sched: avoid placing RT threads on cores handling softirqs"):
      https://android.googlesource.com/kernel/common/+/3adfd8e344ac32845eb3c15743f90a7e3b49dcf3%5E%21/
    
    With the lines:
      int task_pri = convert_prio(p->prio);
      bool drop_nopreempts = task_pri <= MAX_RT_PRIO;
    
    Where the added logic to decide to migrate a rt task off of a
    cpu depended on this drop_nopreempts being true.
    
    This works properly for rt tasks from prio 99 to 1, but for the
    case of task->prio == 0 (userland rt prio 99 tasks) it breaks,
    as the cpupri will be MAX_RT_PRIO - 0 + 1, which then gets
    checked as <= MAX_RT_PRIO.
    
    This prevents the cpu from being dropped from the scheduling
    set and prevents the rt user prio 99 task from migrating, which
    results in high priority rt tasks being left on cpus where long
    running softirqs are executing, causing long latencies.
    
    This patch fixes the off by one by changing the evaulation
    to MAX_RT_PRIO + 1, so that user-prio 99 tasks will also be
    migrated if appropriate.
    
    Luckilly this odd cpupri mapping has been fixed upstream, making
    this patch no longer necessary in 5.15 and newer kernels.
    
    Fixes: 54869f74a2af ("ANDROID: sched: avoid placing RT threads on cores handling softirqs")
    Change-Id: Ia2db7cd461eb4c90f5850b791de1ae95582f7530
    Signed-off-by: John Stultz <jstultz@google.com>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cpupri.c
+++ kernel/sched/cpupri.c
@@ -86,7 +86,7 @@ int cpupri_find(struct cpupri *cp, struct task_struct *p,
 {
 	int idx = 0;
 	int task_pri = convert_prio(p->prio);
-	bool drop_nopreempts = task_pri <= MAX_RT_PRIO;
+	bool drop_nopreempts = task_pri <= MAX_RT_PRIO + 1;
 
 	BUG_ON(task_pri >= CPUPRI_NR_PRIORITIES);
 
