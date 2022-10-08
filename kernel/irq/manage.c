bc0112022a52efc27956223f2bc36c24ba7ec614
Kazuki Hashimoto <kazukih@tuta.io>
2022-10-08 11:36:56 +0900

    irq: Allow affining irqs to the LITTLE cluster
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -34,6 +34,7 @@ static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
 static int prime_cpu_index = -1;
+static int lp_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1298,8 +1299,10 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
-	else
+	else if (action->flags & IRQF_PRIME_AFFINE)
 		mask = cpu_prime_mask;
+	else
+		mask = cpu_lp_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1323,9 +1326,12 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
-	} else {
+	} else if (perf_flag & IRQF_PRIME_AFFINE) {
 		mask = cpu_prime_mask;
 		mask_index = &prime_cpu_index;
+	} else {
+		mask = cpu_lp_mask;
+		mask_index = &lp_cpu_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1402,6 +1408,7 @@ void reaffine_perf_irqs(bool from_hotplug)
 		perf_crit_suspended = false;
 		perf_cpu_index = -1;
 		prime_cpu_index = -1;
+		lp_cpu_index = -1;
 		list_for_each_entry(data, &perf_crit_irqs, list) {
 			struct irq_desc *desc = data->desc;
 
@@ -1686,7 +1693,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE | IRQF_LITTLE_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
