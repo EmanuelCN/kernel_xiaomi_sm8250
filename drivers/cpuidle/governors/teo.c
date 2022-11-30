f1376526c71fb11830ade5f6c82eb9740dcb3143
Kajetan Puchalski <kajetan.puchalski@arm.com>
2022-11-30 15:32:03 +0000

    cpuidle: teo: Optionally skip polling states in teo_find_shallower_state()
    Add a no_poll flag to teo_find_shallower_state() that will let the
    function optionally not consider polling states.
    This allows the caller to guard against the function inadvertently
    resulting in TEO putting the CPU in a polling state when that
    behaviour is undesirable.
    
    Signed-off-by: Kajetan Puchalski <kajetan.puchalski@arm.com>

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -258,15 +258,17 @@ static s64 teo_middle_of_bin(int idx, struct cpuidle_driver *drv)
  * @dev: Target CPU.
  * @state_idx: Index of the capping idle state.
  * @duration_us: Idle duration value to match.
+ * @no_poll: Don't consider polling states.
  */
 static int teo_find_shallower_state(struct cpuidle_driver *drv,
 				    struct cpuidle_device *dev, int state_idx,
-				    s64 duration_us)
+				    s64 duration_us, bool no_poll)
 {
 	int i;
 
 	for (i = state_idx - 1; i >= 0; i--) {
-		if (dev->states_usage[i].disable)
+		if (dev->states_usage[i].disable ||
+				(no_poll && drv->states[i].flags & CPUIDLE_FLAG_POLLING))
 			continue;
 
 		state_idx = i;
@@ -470,7 +472,7 @@ end:
 		 */
 		if (idx > idx0 &&
 		    drv->states[idx].target_residency > delta_tick_us)
-			idx = teo_find_shallower_state(drv, dev, idx, delta_tick_us);
+			idx = teo_find_shallower_state(drv, dev, idx, delta_tick_us, false);
 	}
 
 	return idx;
