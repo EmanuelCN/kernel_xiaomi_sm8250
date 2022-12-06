df6ffadf9d091ed82e491111a7344d247472337d
EmanuelCN <emanuelgithub@gmail.com>
2022-12-06 11:59:47 +0000

    Revert "PM / sleep: Skip OOM killer toggles when kernel is compiled for Android"
    This reverts commit 35b6a51cbd27b09aaa7361bd54a965234b512749.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -148,7 +148,6 @@ int freeze_processes(void)
 	pr_cont("\n");
 	BUG_ON(in_atomic());
 
-#ifndef CONFIG_ANDROID
 	/*
 	 * Now that the whole userspace is frozen we need to disbale
 	 * the OOM killer to disallow any further interference with
@@ -157,7 +156,6 @@ int freeze_processes(void)
 	 */
 	if (!error && !oom_killer_disable(msecs_to_jiffies(freeze_timeout_msecs)))
 		error = -EBUSY;
-#endif
 
 	if (error)
 		thaw_processes();
@@ -202,9 +200,7 @@ void thaw_processes(void)
 	pm_freezing = false;
 	pm_nosig_freezing = false;
 
-#ifndef CONFIG_ANDROID
 	oom_killer_enable();
-#endif
 
 	pr_info("Restarting tasks ... ");
 
