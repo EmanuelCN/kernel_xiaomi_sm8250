324d7e3a86f0cba158bf28230c25ce74711b972a
EmanuelCN <emanuelghub@gmail.com>
2022-12-11 02:15:13 +0200

    Revert "BACKPORT: mm: fix pageblock heuristic"
    This reverts commit fac3af87eb7100245c3a998276cb7b8a42d7ca47.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2278,8 +2278,7 @@ static void change_pageblock_range(struct page *pageblock_page,
  * is worse than movable allocations stealing from unmovable and reclaimable
  * pageblocks.
  */
-static bool can_steal_fallback(unsigned int order, int start_mt,
-				int fallback_type, unsigned int start_order)
+static bool can_steal_fallback(unsigned int order, int start_mt)
 {
 	/*
 	 * Leaving this order check is intended, although there is
@@ -2291,18 +2290,9 @@ static bool can_steal_fallback(unsigned int order, int start_mt,
 	if (order >= pageblock_order)
 		return true;
 
-	// don't let unmovable allocations cause migrations simply because of free pages
-	if ((start_mt != MIGRATE_UNMOVABLE &&
-		order >= pageblock_order / 2) ||
-		// only steal reclaimable page blocks for unmovable allocations
-		(start_mt == MIGRATE_UNMOVABLE &&
-		fallback_type != MIGRATE_MOVABLE &&
-		order >= pageblock_order / 2) ||
-		// reclaimable can steal aggressively
+	if (order >= pageblock_order / 2 ||
 		start_mt == MIGRATE_RECLAIMABLE ||
-		// allow unmovable allocs up to 64K without migrating blocks
-		(start_mt == MIGRATE_UNMOVABLE &&
-		start_order >= 5) ||
+		start_mt == MIGRATE_UNMOVABLE ||
 		page_group_by_mobility_disabled)
 		return true;
 
@@ -2457,8 +2447,7 @@ single_page:
  * fragmentation due to mixed migratetype pages in one pageblock.
  */
 int find_suitable_fallback(struct free_area *area, unsigned int order,
-			int migratetype, bool only_stealable, bool *can_steal,
-			unsigned int start_order)
+			int migratetype, bool only_stealable, bool *can_steal)
 {
 	int i;
 	int fallback_mt;
@@ -2475,7 +2464,7 @@ int find_suitable_fallback(struct free_area *area, unsigned int order,
 		if (list_empty(&area->free_list[fallback_mt]))
 			continue;
 
-		if (can_steal_fallback(order, migratetype, fallback_mt, start_order))
+		if (can_steal_fallback(order, migratetype))
 			*can_steal = true;
 
 		if (!only_stealable)
@@ -2646,7 +2635,7 @@ __rmqueue_fallback(struct zone *zone, int order, int start_migratetype,
 				--current_order) {
 		area = &(zone->free_area[current_order]);
 		fallback_mt = find_suitable_fallback(area, current_order,
-				start_migratetype, false, &can_steal, order);
+				start_migratetype, false, &can_steal);
 		if (fallback_mt == -1)
 			continue;
 
@@ -2672,7 +2661,7 @@ find_smallest:
 							current_order++) {
 		area = &(zone->free_area[current_order]);
 		fallback_mt = find_suitable_fallback(area, current_order,
-				start_migratetype, false, &can_steal, order);
+				start_migratetype, false, &can_steal);
 		if (fallback_mt != -1)
 			break;
 	}
