146f4d688a1f17a66b2622bb270f8b8fa145020b
Umang Chheda <quic_uchheda@quicinc.com>
2023-02-21 21:48:49 +0530

    rtc: pm8xxx: Disable IRQ in shutdown_cb to fix PBS stuck issue
    On SKUs with MSM + Co-proc there can be possibility of PBS
    getting stuck if following sequence of events happen :
    - LPM triggered from APPS.
    - IRQ triggered just before or after MID is changed to I2C by Co-proc
    - PBS might get stuck in processing the IRQ because it will not have
    a way to route it to APPS as the MID is changed to Co-proc.
    
    Fix this by disabling IRQ during shutdown sequence. This way
    PBS would not be stuck in processing the IRQ (on SPMI) ensuring the
    sequence to exit the LPM is executed.
    
    Change-Id: Ibfb244bf692da6a721797bba0a500bf9a0870c19
    Signed-off-by: Umang Chheda <quic_uchheda@quicinc.com>

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2010-2011, 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/of.h>
@@ -586,6 +587,13 @@ static SIMPLE_DEV_PM_OPS(pm8xxx_rtc_pm_ops,
 			 pm8xxx_rtc_suspend,
 			 pm8xxx_rtc_resume);
 
+static void pm8xxx_rtc_shutdown(struct platform_device *pdev)
+{
+	struct pm8xxx_rtc *rtc_dd = platform_get_drvdata(pdev);
+
+	devm_free_irq(rtc_dd->rtc_dev, rtc_dd->rtc_alarm_irq, rtc_dd);
+}
+
 static struct platform_driver pm8xxx_rtc_driver = {
 	.probe		= pm8xxx_rtc_probe,
 	.driver	= {
@@ -593,6 +601,7 @@ static struct platform_driver pm8xxx_rtc_driver = {
 		.pm		= &pm8xxx_rtc_pm_ops,
 		.of_match_table	= pm8xxx_id_table,
 	},
+	.shutdown	= pm8xxx_rtc_shutdown,
 };
 
 module_platform_driver(pm8xxx_rtc_driver);
