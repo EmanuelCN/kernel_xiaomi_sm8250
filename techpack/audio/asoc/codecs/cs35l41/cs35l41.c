0bf1361a640d98df51099653a266a9abb12ec789
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-05 21:25:18 +0100

    techpack: audio: cs35l41{_k81}: Get rid of xlog
    Change-Id: If4e76d231061e042162b847fa485b3dc52ac8950

--- techpack/audio/asoc/codecs/cs35l41/cs35l41.c
+++ techpack/audio/asoc/codecs/cs35l41/cs35l41.c
@@ -44,7 +44,6 @@
 #include "wm_adsp.h"
 #include "cs35l41.h"
 #include <sound/cs35l41.h>
-#include "send_data_to_xlog.h"
 static const char *const cs35l41_supplies[] = {
 	"VA",
 	"VP",
@@ -767,7 +766,6 @@ static irqreturn_t cs35l41_irq(int irq, void *data)
 	unsigned int status[4];
 	unsigned int masks[4];
 	unsigned int i;
-	char reason[] = "DSP";
 	dev_info(cs35l41->dev, "step into cs35l41 irq handler\n");
 
 	for (i = 0; i < ARRAY_SIZE(status); i++) {
@@ -888,7 +886,6 @@ static irqreturn_t cs35l41_irq(int irq, void *data)
 		//Analog mute PA if DC is detected
 		//regmap_write(cs35l41->regmap, CS35L41_AMP_OUT_MUTE,
 		//	     1 << CS35L41_AMP_MUTE_SHIFT);
-		send_DC_data_to_xlog(reason);
 		dev_crit(cs35l41->dev, "DC current detected");
 	}
 
