171adf1ad59c4f7a070255f7e9a0a584193a2f90
Michael Bestas <mkbestas@lineageos.org>
2023-04-02 21:48:21 +0300

    techpack: camera-bengal: Don't build for kona/lito
    Change-Id: Ia0d3fc55198f45bd272e826052d082ef941e7724

--- techpack/camera-bengal/config/khajecameraconf.h
+++ techpack/camera-bengal/config/khajecameraconf.h
@@ -3,6 +3,7 @@
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
  */
 
+#define CONFIG_SPECTRA_CAMERA_BENGAL 1
 #define CONFIG_SPECTRA_CAMERA      1
 #define CONFIG_SPECTRA_CAMERA_OPE  1
 #define CONFIG_SPECTRA_CAMERA_TFE  1
