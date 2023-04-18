647f85c717e65ecc2d6f0928b2752dd42b37546e
Qais Yousef <qais.yousef@arm.com>
2023-04-18 15:09:39 +0100

    sched/uclamp: Cater for uclamp in feec()'s early exit condition
    commit d81304bc6193554014d4372a01debdf65e1e9a4d upstream.
    
    If the utilization of the woken up task is 0, we skip the energy
    calculation because it has no impact.
    
    But if the task is boosted (uclamp_min != 0) will have an impact on task
    placement and frequency selection. Only skip if the util is truly
    0 after applying uclamp values.
    
    Change uclamp_task_cpu() signature to avoid unnecessary additional calls
    to uclamp_eff_get(). feec() is the only user now.
    
    Fixes: 732cd75b8c920 ("sched/fair: Select an energy-efficient CPU on task wake-up")
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220804143609.515789-8-qais.yousef@arm.com
    (cherry picked from commit d81304bc6193554014d4372a01debdf65e1e9a4d)
    Signed-off-by: Qais Yousef (Google) <qyousef@layalina.io>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4163,14 +4163,16 @@ unsigned long task_util_est(struct task_struct *p)
 }
 
 #ifdef CONFIG_UCLAMP_TASK
-static inline unsigned long uclamp_task_util(struct task_struct *p)
+static inline unsigned long uclamp_task_util(struct task_struct *p,
+					     unsigned long uclamp_min,
+					     unsigned long uclamp_max)
 {
-	return clamp(task_util_est(p),
-		     uclamp_eff_value(p, UCLAMP_MIN),
-		     uclamp_eff_value(p, UCLAMP_MAX));
+	return clamp(task_util_est(p), uclamp_min, uclamp_max);
 }
 #else
-static inline unsigned long uclamp_task_util(struct task_struct *p)
+static inline unsigned long uclamp_task_util(struct task_struct *p,
+					     unsigned long uclamp_min,
+					     unsigned long uclamp_max)
 {
 	return task_util_est(p);
 }
@@ -8571,8 +8573,7 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 		goto fail;
 
 	sync_entity_load_avg(&p->se);
-
-	if (!uclamp_task_util(p))
+	if (!uclamp_task_util(p, p_util_min, p_util_max))
 		goto unlock;
 
 	latency_sensitive = uclamp_latency_sensitive(p);
