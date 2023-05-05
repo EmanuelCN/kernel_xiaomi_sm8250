4b807a043ae94cdaab5675beb33af2fef4c31005
EmanuelCN <emanuelghub@gmail.com>
2023-05-05 11:13:33 +0000

    Revert "slmk: lower vmpressure trigger threshold"
    This reverts commit 76e4e4f49506ece1294264bd5de893fe58a2da35.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -307,7 +307,7 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 				    unsigned long pressure, void *data)
 {
-	if (pressure >= 80 && !atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
+	if (pressure == 100 && !atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
 		wake_up(&oom_waitq);
 
 	return NOTIFY_OK;
