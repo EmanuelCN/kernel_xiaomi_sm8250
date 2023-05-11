145e51526a40a22bfffd4127da6f9dc9e05e4928
EmanuelCN <emanuelghub@gmail.com>
2023-05-11 18:41:33 +0000

    Revert "rcu: Make need_resched() respond to urgent RCU-QS needs"
    This reverts commit 6ad9e9215fe1e988ddc2781dc4ee8e5012234bbe.

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2572,15 +2572,6 @@ void rcu_sched_clock_irq(int user)
 	trace_rcu_utilization(TPS("Start scheduler-tick"));
 	lockdep_assert_irqs_disabled();
 	raw_cpu_inc(rcu_data.ticks_this_gp);
-	/* The load-acquire pairs with the store-release setting to true. */
-	if (smp_load_acquire(this_cpu_ptr(&rcu_data.rcu_urgent_qs))) {
-		/* Idle and userspace execution already are quiescent states. */
-		if (!rcu_is_cpu_rrupt_from_idle() && !user) {
-			set_tsk_need_resched(current);
-			set_preempt_need_resched();
-		}
-		__this_cpu_write(rcu_data.rcu_urgent_qs, false);
-	}
 	rcu_flavor_sched_clock_irq(user);
 	/* The load-acquire pairs with the store-release setting to true. */
 	if (smp_load_acquire(this_cpu_ptr(&rcu_data.rcu_urgent_qs))) {
