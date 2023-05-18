0252d3ccad3de4234b87afb61a8b3f140c9d6825
EmanuelCN <emanuelghub@gmail.com>
2023-05-18 14:57:11 +0000

    Revert "techpack: msm: Place pm_qos requests on perf mask"
    This reverts commit 9a704906c53bfe9c985498f35f78940597825af4.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2751,7 +2751,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()) |
-					   *cpumask_bits(cpu_perf_mask))
+					   *cpumask_bits(cpu_prime_mask))
 	};
 	int ret;
 
