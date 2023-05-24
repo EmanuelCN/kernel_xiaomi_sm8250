427d2926d5c7ee26e0053d9cf3c2e777d510236b
EmanuelCN <emanuelghub@gmail.com>
2023-05-24 17:23:30 +0000

    mm: Nuke ULMK
    Revert "mm: retry more before OOM in the presence of slow shrinkers"
    This reverts commit bc20a5027e12dedba1e3d6d7e86081a060779821.
    
    Revert "mm: do not fail non-costly allocs if there are reclaimable pages"
    This reverts commit 24601112ff8dc778696693279da0479ef1702746.
    
    Revert "mm: consider killable task's anon as reclaimable"
    This reverts commit 4b7c5578418be00666e49ca8b50301e3f6c3ef2d.
    
    Revert "mm/oom-kill: Add option to panic when killing foreground app"
    This reverts commit 0ca93a33192e50f06010e49606686b486fed5d6f.
    
    Revert "mm: reap tasks only killed by low memory killer"
    This reverts commit f9b1a9b7553bb225f703a19dd97acbc13318c818.
    
    Revert "mm: oom: ensure emergency trigger is raised before enter into oom"
    This reverts commit 1d072ce7c80bf8e9a63372c40fb632204532d0af.
    
    Revert "mm: reorder oom kill logic in should_ulmk_retry()"
    This reverts commit 2f72134740f04f14226d7767f5bb13d62f13ed93.
    
    Revert "mm: don't throttle ULMK on direct reclaim"
    This reverts commit a3d4997c8cea7dcd24ac657cb721d2be8cf62f95.
    
    Revert "mm: oom: update ulmk_kill_jiffies from oom context kill"
    This reverts commit 8e0edd5e58e8aedde350241a863cefdcb01ad9d3.
    
    Revert "mm/oom-kill: Add debug policy"
    This reverts commit d1b6cf4f5f6c847acbc53858e16947eb90db7d10.
    
    Revert "mm/oom-kill: Run oom-killer if ULMK is stuck"
    This reverts commit 2aacc2971373c311a28d9a8866282129a1d0d4f8.
    
    Revert "kernel: trigger lmkd specific triggers during emergency"
    This reverts commit a632490cf26f56d79ace3795e876c0d8db29812c.
    
    Revert "mm: check for possibility of ulmk kills in should_ulmk_retry"
    This reverts commit 1f4c00bcf2a76e374a1704ab80bbd3e582071c6e.
    
    Revert "mm, oom: check LMK enabled for pagefault_out_of_memory()"
    This reverts commit eba0983ef4034b4c7239304a3e93576ce857d607.
    
    Revert "psi: Add emergency trigger"
    This reverts commit a7517e81e239ef81d2d7e2cc416b65d0042a52c8.
    
    Revert "mm: oom-kill: Add lmk_kill possible for ULMK"
    This reverts commit aa9e75a9ff9b3ed95641d704e6d907f706c672bc.

--- kernel/signal.c
+++ kernel/signal.c
@@ -1388,13 +1388,9 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 	rcu_read_unlock();
 
 	if (!ret && sig) {
-		check_panic_on_foreground_kill(p);
 		ret = do_send_sig_info(sig, info, p, type);
-		if (capable(CAP_KILL) && sig == SIGKILL) {
-			if (!strcmp(current->comm, ULMK_MAGIC))
-				add_to_oom_reaper(p);
-			ulmk_update_last_kill();
-		}
+		if (capable(CAP_KILL) && sig == SIGKILL)
+			add_to_oom_reaper(p);
 	}
 
 	return ret;
