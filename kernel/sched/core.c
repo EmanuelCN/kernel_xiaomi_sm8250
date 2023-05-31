47f41da3c42ccfbaa51e17bb2607e585460625ab
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:44 +0200

    sched/fair: Implement an EEVDF-like scheduling policy
    Where CFS is currently a WFQ based scheduler with only a single knob,
    the weight. The addition of a second, latency oriented parameter,
    makes something like WF2Q or EEVDF based a much better fit.
    
    Specifically, EEVDF does EDF like scheduling in the left half of the
    tree -- those entities that are owed service. Except because this is a
    virtual time scheduler, the deadlines are in virtual time as well,
    which is what allows over-subscription.
    
    EEVDF has two parameters:
    
     - weight, or time-slope: which is mapped to nice just as before
    
     - request size, or slice length: which is used to compute
       the virtual deadline as: vd_i = ve_i + r_i/w_i
    
    Basically, by setting a smaller slice, the deadline will be earlier
    and the task will be more eligible and ran earlier.
    
    Tick driven preemption is driven by request/slice completion; while
    wakeup preemption is driven by the deadline.
    
    Because the tree is now effectively an interval tree, and the
    selection is no longer 'leftmost', over-scheduling is less of a
    problem.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124603.931005524@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3065,6 +3065,7 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->low_latency			= 0;
 #endif
 	p->se.vlag			= 0;
+	p->se.slice			= sysctl_sched_min_granularity;
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2099,6 +2099,7 @@ extern void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags);
 extern const_debug unsigned int sysctl_sched_nr_migrate;
 extern const_debug unsigned int sysctl_sched_migration_cost;
 
+extern unsigned int sysctl_sched_min_granularity;
 #ifdef CONFIG_SCHED_HRTICK
 
 /*
@@ -3005,6 +3006,7 @@ static inline void restore_cgroup_boost_settings(void) { }
 #endif
 
 extern int alloc_related_thread_groups(void);
+extern int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se);
 
 extern void check_for_migration(struct rq *rq, struct task_struct *p);
 
