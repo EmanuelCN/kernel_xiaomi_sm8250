6f381bb13e21a528dae611820f4fe5bdc199aa06
EmanuelCN <emanuelghub@gmail.com>
2023-06-08 22:31:57 +0200

    Revert "irqchip: fix pcie device wakeup reason is unknown issue"
    This reverts commit 4fe1c3fe00ef7396add76001b88f8a7a8c199beb.

--- drivers/pci/controller/pci-msm-msi.c
+++ drivers/pci/controller/pci-msm-msi.c
@@ -11,9 +11,6 @@
 #include <linux/of_irq.h>
 #include <linux/of_pci.h>
 #include <linux/pci.h>
-#include <linux/wakeup_reason.h>
-
-extern int gic_resume_irq;
 
 struct msm_msi_irq {
 	unsigned int hwirq; /* MSI controller hwirq */
@@ -52,13 +49,6 @@ static void msm_msi_handler(struct irq_desc *desc)
 
 	msi = irq_desc_get_handler_data(desc);
 	virq = irq_find_mapping(msi->inner_domain, irq_desc_get_irq(desc));
-
-	if (gic_resume_irq) {
-		if(irq_desc_get_irq(desc) == gic_resume_irq) {
-			log_irq_wakeup_reason(virq);
-		}
-		gic_resume_irq = 0;
-	}
 
 	generic_handle_irq(virq);
 
