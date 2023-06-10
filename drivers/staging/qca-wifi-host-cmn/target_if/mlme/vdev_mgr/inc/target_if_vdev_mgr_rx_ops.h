40789f1d96ffabed38da72d4fd805257d83aa31e
EmanuelCN <emanuelghub@gmail.com>
2023-06-10 21:12:21 +0300

    Revert "qcacmn: Revert target_if_vdev_mgr_rsp_timer_cb"
    This reverts commit 766e7a89760a0f46249027628974a4bab6b6489e.

--- drivers/staging/qca-wifi-host-cmn/target_if/mlme/vdev_mgr/inc/target_if_vdev_mgr_rx_ops.h
+++ drivers/staging/qca-wifi-host-cmn/target_if/mlme/vdev_mgr/inc/target_if_vdev_mgr_rx_ops.h
@@ -152,8 +152,9 @@ QDF_STATUS target_if_vdev_mgr_wmi_event_unregister(
  * target_if_vdev_mgr_rsp_timer_cb() - function to handle vdev related timeouts
  * @vdev_rsp: pointer to vdev response timer
  *
- * Return: none
+ * Return: QDF_STATUS_SUCCESS on success, QDF_STATUS_E_FAILURE on error
  */
-void target_if_vdev_mgr_rsp_timer_cb(struct vdev_response_timer *vdev_rsp);
+QDF_STATUS
+target_if_vdev_mgr_rsp_timer_cb(struct vdev_response_timer *vdev_rsp);
 
 #endif /* __TARGET_IF_VDEV_MGR_RX_OPS_H__ */
