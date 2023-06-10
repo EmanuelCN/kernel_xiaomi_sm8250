898ffbb43aeb881a6e740c9526ab7b7f5dab55c3
EmanuelCN <emanuelghub@gmail.com>
2023-06-10 21:12:21 +0300

    Revert "qcacmn: Update scheduler_msg callback type"
    This reverts commit b9c4cabdaaa3d74915f2e13e73fa52b7d77b6f17.

--- drivers/staging/qca-wifi-host-cmn/target_if/mlme/vdev_mgr/inc/target_if_vdev_mgr_rx_ops.h
+++ drivers/staging/qca-wifi-host-cmn/target_if/mlme/vdev_mgr/inc/target_if_vdev_mgr_rx_ops.h
@@ -150,10 +150,10 @@ QDF_STATUS target_if_vdev_mgr_wmi_event_unregister(
 
 /**
  * target_if_vdev_mgr_rsp_timer_cb() - function to handle vdev related timeouts
- * @arg: pointer to argument
+ * @vdev_rsp: pointer to vdev response timer
  *
  * Return: none
  */
-void target_if_vdev_mgr_rsp_timer_cb(void *arg);
+void target_if_vdev_mgr_rsp_timer_cb(struct vdev_response_timer *vdev_rsp);
 
 #endif /* __TARGET_IF_VDEV_MGR_RX_OPS_H__ */
