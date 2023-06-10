1c5ac63635f2faf9103afb97b25dcd052de3b0af
EmanuelCN <emanuelghub@gmail.com>
2023-06-10 20:02:04 +0300

    Revert "adreno_tz: Fix GPU target frequency calculation for high refresh rates"
    This reverts commit 1957592d4093d21b249c41a6303fb8d5f48da23a.

--- include/drm/drm_refresh_rate.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright (C) 2020 Sultan Alsawaf <sultan@kerneltoast.com>.
- */
-
-unsigned int dsi_panel_get_refresh_rate(void);
--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -55,8 +55,6 @@ static const struct of_device_id dsi_display_dt_match[] = {
 
 struct dsi_display *primary_display;
 
-static unsigned int cur_refresh_rate = 60;
-
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -7930,11 +7928,6 @@ int dsi_display_pre_commit(void *display,
 	return rc;
 }
 
-unsigned int dsi_panel_get_refresh_rate(void)
-{
-	return READ_ONCE(cur_refresh_rate);
-}
-
 int dsi_display_enable(struct dsi_display *display)
 {
 	int rc = 0;
@@ -8012,7 +8005,6 @@ int dsi_display_enable(struct dsi_display *display)
 	mutex_lock(&display->display_lock);
 
 	mode = display->panel->cur_mode;
-	WRITE_ONCE(cur_refresh_rate, mode->timing.refresh_rate);
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
 		rc = dsi_panel_switch(display->panel);
