baced3c6fd3c072f5d54185a134ef086eeaf7ed6
EmanuelCN <emanuelghub@gmail.com>
2023-06-10 21:12:20 +0300

    Revert "qcacld-3.0: Update to match scheduler_msg callback"
    This reverts commit ce34f3d9fdd689af49264a2db6a01bbf2e9239b4.

--- drivers/staging/qcacld-3.0/core/mac/src/sys/common/src/wlan_qct_sys.c
+++ drivers/staging/qcacld-3.0/core/mac/src/sys/common/src/wlan_qct_sys.c
@@ -50,26 +50,24 @@ QDF_STATUS sys_build_message_header(SYS_MSG_ID msg_id,
 
 /**
  * umac_stop_complete_cb() - a callback when system stop completes
- * @msg: pointer to actual message being handled
+ * @user_data: pointer to user provided data context
  *
  * this callback is used once system stop is completed.
  *
- * Return: QDF_STATUS
+ * Return: none
  */
 #ifdef QDF_ENABLE_TRACING
-static QDF_STATUS umac_stop_complete_cb(struct scheduler_msg *msg)
+static void umac_stop_complete_cb(void *user_data)
 {
-	qdf_event_t *stop_evt = msg->bodyptr;
+	qdf_event_t *stop_evt = (qdf_event_t *) user_data;
 	QDF_STATUS qdf_status = qdf_event_set(stop_evt);
 
 	QDF_ASSERT(QDF_IS_STATUS_SUCCESS(qdf_status));
-
-	return qdf_status;
 }
 #else
-static QDF_STATUS umac_stop_complete_cb(struct scheduler_msg *msg)
+static void umac_stop_complete_cb(void *user_data)
 {
-	return QDF_STATUS_SUCCESS;
+	return;
 }
 #endif
 
@@ -131,6 +129,7 @@ QDF_STATUS umac_stop(void)
 static QDF_STATUS sys_mc_process_msg(struct scheduler_msg *pMsg)
 {
 	QDF_STATUS qdf_status = QDF_STATUS_SUCCESS;
+	data_stall_detect_cb data_stall_detect_callback;
 	mac_handle_t mac_handle;
 
 	if (!pMsg) {
@@ -164,12 +163,14 @@ static QDF_STATUS sys_mc_process_msg(struct scheduler_msg *pMsg)
 			QDF_ASSERT(QDF_IS_STATUS_SUCCESS(qdf_status));
 			qdf_status = mac_stop(mac_handle);
 			QDF_ASSERT(QDF_IS_STATUS_SUCCESS(qdf_status));
-			qdf_status = pMsg->callback(pMsg);
+			((sys_rsp_cb) pMsg->callback)(pMsg->bodyptr);
+			qdf_status = QDF_STATUS_SUCCESS;
 			break;
-		case SYS_MSG_ID_DATA_STALL_MSG:
-			if (pMsg->callback)
-				qdf_status = pMsg->callback(pMsg);
 
+		case SYS_MSG_ID_DATA_STALL_MSG:
+			data_stall_detect_callback = pMsg->callback;
+			if (data_stall_detect_callback)
+				data_stall_detect_callback(pMsg->bodyptr);
 			qdf_mem_free(pMsg->bodyptr);
 			break;
 		default:
