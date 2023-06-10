6a7b74be6e412af1723b1597468d30a3610c923f
Divyanshu-Modi <divyan.m05@gmail.com>
2023-06-10 20:47:21 +0300

    msm-tsens: Silence ipc log spam after disabling ipc logging  Silences:  [    0.141269] tsens_tm_probe : unable to create IPC Logging 0 for tsens 0x00000000010ae000  [    0.141282] tsens_tm_probe : unable to create IPC Logging 1 for tsens 0x00000000010ae000  [    0.141294] tsens_tm_probe : unable to create IPC Logging 2 for tsens 0x00000000010ae000
    Change-Id: I7e64b3d94273248e686d6a45491aa838529b9b57

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -339,6 +339,7 @@ int tsens_tm_probe(struct platform_device *pdev)
 		return rc;
 	}
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_0",
 					&tmdev->phys_addr_tm);
 
@@ -367,6 +368,7 @@ int tsens_tm_probe(struct platform_device *pdev)
 					__func__, &tmdev->phys_addr_tm);
 
 	list_add_tail(&tmdev->list, &tsens_device_list);
+#endif
 	platform_set_drvdata(pdev, tmdev);
 
 	return rc;
