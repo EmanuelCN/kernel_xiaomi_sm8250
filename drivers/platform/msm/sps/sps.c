d5b077aa7a25892eb6bedf003214c71e9e60d462
Divyanshu-Modi <divyan.m05@gmail.com>
2023-06-10 20:37:56 +0300

    platform: msm: sps: silence ipc log spam when ipc logging is disabled  Silences:  [    0.383634] sps_register_bam_device : unable to create IPC Logging 0 for bam 0x000000000c184000  [    0.383636] sps_register_bam_device : unable to create IPC Logging 1 for bam 0x000000000c184000  [    0.383642] sps_register_bam_device : unable to create IPC Logging 2 for bam 0x000000000c184000  [    0.383647] sps_register_bam_device : unable to create IPC Logging 3 for bam 0x000000000c184000  [    0.383651] sps_register_bam_device : unable to create IPC Logging 4 for bam 0x000000000c184000  [    0.438542] sps_register_bam_device : unable to create IPC Logging 0 for bam 0x0000000001dc4000  [    0.438545] sps_register_bam_device : unable to create IPC Logging 1 for bam 0x0000000001dc4000  [    0.438551] sps_register_bam_device : unable to create IPC Logging 2 for bam 0x0000000001dc4000  [    0.438556] sps_register_bam_device : unable to create IPC Logging 3 for bam 0x0000000001dc4000  [    0.438561] sps_register_bam_device : unable to create IPC Logging 4 for bam 0x0000000001dc4000

--- drivers/platform/msm/sps/sps.c
+++ drivers/platform/msm/sps/sps.c
@@ -2098,7 +2098,9 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 {
 	struct sps_bam *bam = NULL;
 	void __iomem *virt_addr = NULL;
+#ifdef CONFIG_IPC_LOGGING
 	char bam_name[MAX_MSG_LEN];
+#endif
 	u32 manage;
 	int ok;
 	int result;
@@ -2192,6 +2194,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	if (virt_addr != NULL)
 		bam->props.virt_addr = virt_addr;
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_0",
 					&bam->props.phys_addr);
 	bam->ipc_log0 = ipc_log_context_create(SPS_IPC_LOGPAGES,
@@ -2231,6 +2234,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	if (!bam->ipc_log4)
 		SPS_ERR(sps, "%s : unable to create IPC Logging 4 for bam %pa",
 					__func__, &bam->props.phys_addr);
+#endif
 
 	if (bam_props->ipc_loglevel)
 		bam->ipc_loglevel = bam_props->ipc_loglevel;
