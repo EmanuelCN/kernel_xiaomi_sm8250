d85ffc58b1786bf2872dd599a03ff7820c6cdc71
Cyber Knight <cyberknight755@gmail.com>
2023-06-10 20:35:56 +0300

    net/cnss_prealloc: Guard debugfs bits with CONFIG_DEBUG_FS Silences:
    [    0.722847] wcnss_pre_alloc_init: Failed to create debugfs dir
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/net/wireless/cnss_utils/cnss_utils.c
+++ drivers/net/wireless/cnss_utils/cnss_utils.c
@@ -7,7 +7,9 @@
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/etherdevice.h>
+#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
+#endif
 #include <net/cnss_utils.h>
 
 #define CNSS_MAX_CH_NUM 157
@@ -422,7 +424,7 @@ static const struct file_operations cnss_utils_mac_fops = {
 	.owner		= THIS_MODULE,
 	.llseek		= seq_lseek,
 };
-
+#ifdef CONFIG_DEBUG_FS
 static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 {
 	int ret = 0;
@@ -441,7 +443,7 @@ static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 out:
 	return ret;
 }
-
+#endif
 static int __init cnss_utils_init(void)
 {
 	struct cnss_utils_priv *priv = NULL;
@@ -454,7 +456,9 @@ static int __init cnss_utils_init(void)
 
 	mutex_init(&priv->unsafe_channel_list_lock);
 	spin_lock_init(&priv->dfs_nol_info_lock);
+#ifdef CONFIG_DEBUG_FS
 	cnss_utils_debugfs_create(priv);
+#endif
 	cnss_utils_priv = priv;
 
 	return 0;
