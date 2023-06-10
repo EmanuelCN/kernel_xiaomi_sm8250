c4c394ab14a966420a6879c4bb8e276d9e25851b
EmanuelCN <emanuelghub@gmail.com>
2023-06-10 21:12:21 +0300

    Revert "qcacmn: Update scheduler_msg callback type"
    This reverts commit b9c4cabdaaa3d74915f2e13e73fa52b7d77b6f17.

--- drivers/staging/qca-wifi-host-cmn/scheduler/src/scheduler_api.c
+++ drivers/staging/qca-wifi-host-cmn/scheduler/src/scheduler_api.c
@@ -464,10 +464,7 @@ QDF_STATUS scheduler_timer_q_mq_handler(struct scheduler_msg *msg)
 	if (msg->reserved != SYS_MSG_COOKIE || msg->type != SYS_MSG_ID_MC_TIMER)
 		return sched_ctx->legacy_sys_handler(msg);
 
-	/* scheduler_msg_process_fn_t and qdf_mc_timer_callback_t have
-	 * different parameters and return type
-	 */
-	timer_callback = (qdf_mc_timer_callback_t)msg->callback;
+	timer_callback = msg->callback;
 	QDF_BUG(timer_callback);
 	if (!timer_callback)
 		return QDF_STATUS_E_FAILURE;
@@ -644,7 +641,7 @@ void scheduler_mc_timer_callback(qdf_mc_timer_t *timer)
 	/* serialize to scheduler controller thread */
 	msg.type = SYS_MSG_ID_MC_TIMER;
 	msg.reserved = SYS_MSG_COOKIE;
-	msg.callback = (scheduler_msg_process_fn_t)callback;
+	msg.callback = callback;
 	msg.bodyptr = user_data;
 	msg.bodyval = 0;
 
