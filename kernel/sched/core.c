7ad036f7861d2b732e5e98511e066a29900c3a35
Hao Jia <jiahao.os@bytedance.com>
2023-06-13 16:20:09 +0800

    sched/core: Fixed missing rq clock update before calling set_rq_offline()
    When using a cpufreq governor that uses
    cpufreq_add_update_util_hook(), it is possible to trigger a missing
    update_rq_clock() warning for the CPU hotplug path:
    
      rq_attach_root()
        set_rq_offline()
          rq_offline_rt()
    	__disable_runtime()
    	  sched_rt_rq_enqueue()
    	    enqueue_top_rt_rq()
    	      cpufreq_update_util()
    		data->func(data, rq_clock(rq), flags)
    
    Move update_rq_clock() from sched_cpu_deactivate() (one of it's
    callers) into set_rq_offline() such that it covers all
    set_rq_offline() usage.
    
    Additionally change rq_attach_root() to use rq_lock_irqsave() so that
    it will properly manage the runqueue clock flags.
    
    Suggested-by: Ben Segall <bsegall@google.com>
    Signed-off-by: Hao Jia <jiahao.os@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20230613082012.49615-2-jiahao.os@bytedance.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8007,6 +8007,7 @@ void set_rq_offline(struct rq *rq)
 	if (rq->online) {
 		const struct sched_class *class;
 
+		update_rq_clock(rq);
 		for_each_class(class) {
 			if (class->rq_offline)
 				class->rq_offline(rq);
@@ -8145,7 +8146,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 
 	rq_lock_irqsave(rq, &rf);
 	if (rq->rd) {
-		update_rq_clock(rq);
 		BUG_ON(!cpumask_test_cpu(cpu, rq->rd->span));
 		set_rq_offline(rq);
 	}
