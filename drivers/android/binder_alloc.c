2e29cdec052ba0468da7fd8e2e0fe7a31b1a8a4e
EmanuelCN <emanuelghub@gmail.com>
2023-06-20 17:28:14 +0000

    Revert "binder: Reserve caches for small, high-frequency memory allocations"
    This reverts commit 0911c269631f7ba2e33a6c7690c1fbfa332ef3c0.

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -45,22 +45,6 @@ module_param_named(debug_mask, binder_alloc_debug_mask,
 #define binder_alloc_debug(mask, x...) {}
 #endif
 
-static struct kmem_cache *binder_buffer_pool;
-
-int binder_buffer_pool_create(void)
-{
-	binder_buffer_pool = KMEM_CACHE(binder_buffer, SLAB_HWCACHE_ALIGN);
-	if (!binder_buffer_pool)
-		return -ENOMEM;
-
-	return 0;
-}
-
-void binder_buffer_pool_destroy(void)
-{
-	kmem_cache_destroy(binder_buffer_pool);
-}
-
 static struct binder_buffer *binder_buffer_next(struct binder_buffer *buffer)
 {
 	return list_entry(buffer->entry.next, struct binder_buffer, entry);
@@ -458,8 +442,7 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 		return ERR_PTR(ret);
 	if (buffer_size != size) {
 		struct binder_buffer *new_buffer;
-
-		new_buffer = kmem_cache_zalloc(binder_buffer_pool, GFP_KERNEL);
+		new_buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
 		if (!new_buffer) {
 			pr_err("%s: %d failed to alloc new buffer struct\n",
 			       __func__, alloc->pid);
@@ -587,7 +570,7 @@ static void binder_delete_free_buffer(struct binder_alloc *alloc,
 					 buffer_start_page(buffer) + PAGE_SIZE);
 	}
 	list_del(&buffer->entry);
-	kmem_cache_free(binder_buffer_pool, buffer);
+	kfree(buffer);
 }
 static void binder_free_buf_locked(struct binder_alloc *alloc,
 				   struct binder_buffer *buffer)
@@ -699,8 +682,7 @@ int binder_alloc_mmap_handler(struct binder_alloc *alloc,
 		failure_string = "alloc page array";
 		goto err_alloc_pages_failed;
 	}
-
-	buffer = kmem_cache_zalloc(binder_buffer_pool, GFP_KERNEL);
+	buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
 	if (!buffer) {
 		ret = -ENOMEM;
 		failure_string = "alloc buffer struct";
@@ -754,7 +736,7 @@ void binder_alloc_deferred_release(struct binder_alloc *alloc)
 		WARN_ON(!buffer->free);
 		list_del(&buffer->entry);
 		WARN_ON_ONCE(!list_empty(&alloc->buffers));
-		kmem_cache_free(binder_buffer_pool, buffer);
+		kfree(buffer);
 	}
 	page_count = 0;
 	if (alloc->pages) {
