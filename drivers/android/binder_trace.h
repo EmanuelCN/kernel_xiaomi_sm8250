db792696f7e56f4492e7b34d244610b2f37b4a76
EmanuelCN <emanuelghub@gmail.com>
2023-06-21 18:24:25 +0000

    drivers: android: binder: Properly guard trace events *Fixes compilation after "binder: Conditionally compile logging"

--- drivers/android/binder_trace.h
+++ drivers/android/binder_trace.h
@@ -371,6 +371,7 @@ DEFINE_EVENT(binder_lru_page_class, binder_unmap_kernel_start,
 DEFINE_EVENT(binder_lru_page_class, binder_unmap_kernel_end,
 	TP_PROTO(const struct binder_alloc *alloc, size_t page_index),
 	TP_ARGS(alloc, page_index));
+#ifdef CONFIG_ANDROID_BINDER_LOGS
 TRACE_EVENT(binder_command,
 	TP_PROTO(uint32_t cmd),
 	TP_ARGS(cmd),
@@ -401,6 +402,7 @@ TRACE_EVENT(binder_return,
 			  binder_return_strings[_IOC_NR(__entry->cmd)] :
 			  "unknown")
 );
+#endif
 #endif /* _BINDER_TRACE_H */
 #undef TRACE_INCLUDE_PATH
 #undef TRACE_INCLUDE_FILE
