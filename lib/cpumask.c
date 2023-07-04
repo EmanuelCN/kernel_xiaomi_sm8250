b272c3cf62c3d23bf808f46b2cb19defe371169f
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-03 17:22:38 -0700

    cpumask: Add optimized helpers when NR_CPUS fits in a long
    When NR_CPUS fits in a long, it's possible to use compiler built-ins to
    produce much faster code when operating on cpumasks compared to just using
    the generic bitops APIs.
    
    Therefore, add optimized helpers using compiler built-ins when NR_CPUS fits
    in a long. This also turns nr_cpu_ids into a compile-time constant for
    further optimization potential.
    
    Note that compared to the upstream cpumask rewrite with this feature, these
    optimized helpers perfectly preserve the semantics of the helpers they
    replace. And this change is much smaller than the upstream version.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- lib/cpumask.c
+++ lib/cpumask.c
@@ -6,6 +6,7 @@
 #include <linux/export.h>
 #include <linux/bootmem.h>
 
+#if NR_CPUS > BITS_PER_LONG
 /**
  * cpumask_next - get the next cpu in a cpumask
  * @n: the cpu prior to the place to search (ie. return will be > @n)
@@ -60,6 +61,7 @@ int cpumask_any_but(const struct cpumask *mask, unsigned int cpu)
 	return i;
 }
 EXPORT_SYMBOL(cpumask_any_but);
+#endif /* NR_CPUS > BITS_PER_LONG */
 
 /**
  * cpumask_next_wrap - helper to implement for_each_cpu_wrap
