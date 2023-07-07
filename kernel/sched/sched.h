9d1344fcf03ad64d9f282836b1734ab90c5e8bff
Tim C Chen <tim.c.chen@linux.intel.com>
2023-07-07 15:57:01 -0700

    sched/topology: Record number of cores in sched group
    When balancing sibling domains that have different number of cores,
    tasks in respective sibling domain should be proportional to the
    number of cores in each domain. In preparation of implementing such a
    policy, record the number of cores in a scheduling group.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/04641eeb0e95c21224352f5743ecb93dfac44654.1688770494.git.tim.c.chen@linux.intel.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1655,6 +1655,7 @@ struct sched_group {
 	atomic_t		ref;
 
 	unsigned int		group_weight;
+	unsigned int		cores;
 	struct sched_group_capacity *sgc;
 	int			asym_prefer_cpu;	/* CPU of highest priority in group */
 
