d7c56a94cdacf5a0b7fa4ce67425439bf383c864
EmanuelCN <emanuelghub@gmail.com>
2023-09-05 22:31:04 +0000

    kernel/scheed: Drop top-app boost
    not needed anymore

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -847,7 +847,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0, 0, 0 },
 		{ "foreground",	0, 1, 0, 0 },
 		{ "rt",		0, 0, 0, 0 },
-		{ "top-app",	10, 1, 0, 0 },
+		{ "top-app",	0, 1, 0, 0 },
 	};
 	int i;
 
