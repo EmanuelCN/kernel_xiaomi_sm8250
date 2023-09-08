64301769ee16c307925141352d2273cee7a0b18d
EmanuelCN <emanuelghub@gmail.com>
2023-09-08 21:32:51 +0300

    arm64: lib: Reset to akg

--- arch/x86/include/asm/linkage.h
+++ arch/x86/include/asm/linkage.h
@@ -13,13 +13,9 @@
 
 #ifdef __ASSEMBLY__
 
-/*
- * GLOBAL is DEPRECATED
- *
- * use SYM_DATA_START, SYM_FUNC_START, SYM_INNER_LABEL, SYM_CODE_START, or
- * similar
- */
-#define GLOBAL(name)	SYM_ENTRY(name, SYM_L_GLOBAL, SYM_A_NONE)
+#define GLOBAL(name)	\
+	.globl name;	\
+	name:
 
 #if defined(CONFIG_X86_64) || defined(CONFIG_X86_ALIGNMENT_16)
 #define __ALIGN		.p2align 4, 0x90
