2cbc56f72769baa748011c36e04c73bd2d48cdf9
Guenter Roeck <linux@roeck-us.net>
2023-09-15 08:22:37 -0700

    rtc: Add API function to return alarm time bound by hardware limit
    Add rtc_bound_alarmtime() to return the requested alarm timeout bound by
    the maxmum alarm timeout that is supported by a given RTC.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Change-Id: I33fe01f886651e87fd9afdd25a6a06a059829a3a
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230915152238.1144706-2-linux@roeck-us.net
    (cherry picked from commit a0fddaa0b5a587cc8d185f8802fe7e48493c43ed)
    (cherry picked from commit 5cf72f6543cb562d3b596a5e29a06c168db084a3)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/rtc.h
+++ include/linux/rtc.h
@@ -259,6 +259,23 @@ static inline bool rtc_tv_nsec_ok(s64 set_offset_nsec,
 #define rtc_register_device(device) \
 	__rtc_register_device(THIS_MODULE, device)
 
+/**
+ * rtc_bound_alarmtime() - Return alarm time bound by rtc limit
+ * @rtc: Pointer to rtc device structure
+ * @requested: Requested alarm timeout
+ *
+ * Return: Alarm timeout bound by maximum alarm time supported by rtc.
+ */
+static inline ktime_t rtc_bound_alarmtime(struct rtc_device *rtc,
+					  ktime_t requested)
+{
+	if (rtc->alarm_offset_max &&
+	    rtc->alarm_offset_max * MSEC_PER_SEC < ktime_to_ms(requested))
+		return ms_to_ktime(rtc->alarm_offset_max * MSEC_PER_SEC);
+
+	return requested;
+}
+
 #ifdef CONFIG_RTC_HCTOSYS_DEVICE
 extern int rtc_hctosys_ret;
 #else
