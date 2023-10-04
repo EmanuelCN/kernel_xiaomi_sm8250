1b48c1c1d88a76565d0d4c23d57994f35ad62d64
EmanuelCN <emanuelghub@gmail.com>
2023-10-04 11:24:57 +0300

    Revert "sched/walt: Improve the scheduler"
    This reverts commit 6e4616dfe8f00c705f3ba5fd69a69bb0c2cbab69.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2723,12 +2723,6 @@ enum sched_boost_policy {
 
 #ifdef CONFIG_SCHED_WALT
 
-#define WALT_MANY_WAKEUP_DEFAULT 1000
-static inline bool walt_want_remote_wakeup(void)
-{
-	return sysctl_sched_many_wakeup_threshold < WALT_MANY_WAKEUP_DEFAULT;
-}
-
 static inline int cluster_first_cpu(struct sched_cluster *cluster)
 {
 	return cpumask_first(&cluster->cpus);
@@ -3179,10 +3173,6 @@ static inline bool early_detection_notify(struct rq *rq, u64 wallclock)
 }
 
 static inline void note_task_waking(struct task_struct *p, u64 wallclock) { }
-static inline bool walt_want_remote_wakeup(void)
-{
-	return false;
-}
 #endif	/* CONFIG_SCHED_WALT */
 
 struct sched_avg_stats {
