bfa11dd17109511ebb8dd6bfd391df7165ac1c36
EmanuelCN <emanuelghub@gmail.com>
2023-10-09 23:14:22 +0300

    Revert "Revert "alarmtimer: Don't fail on wakeup""
    This reverts commit bf8f6f90e46b6966cb8c3df14ca43c4919155735.

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -286,10 +286,8 @@ static int alarmtimer_suspend(struct device *dev)
 	if (min == 0)
 		return 0;
 
-	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC) {
-		__pm_wakeup_event(ws, ktime_to_ms(min) + 10);
-		return -EBUSY;
-	}
+	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC)
+		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
 
 	trace_alarmtimer_suspend(expires, type);
 
@@ -302,7 +300,7 @@ static int alarmtimer_suspend(struct device *dev)
 	/* Set alarm, if in the past reject suspend briefly to handle */
 	ret = rtc_timer_start(rtc, &rtctimer, now, 0);
 	if (ret < 0)
-		__pm_wakeup_event(ws, MSEC_PER_SEC);
+ 		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
 	return ret;
 }
 
