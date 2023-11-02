21a14512fc97ce8971adf495ac20a9447cb0d167
bntxperses <dbikramyt@gmail.com>
2023-11-02 16:51:14 +0530

    Revert "tree: Apply KernelSU patches"
    This reverts commit 27f89601eab057083ff2c2951bd5c38f74937939.

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -378,13 +378,10 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
-	ksu_handle_input_handle_event(&type, &code, &value);
 
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 		add_input_randomness(type, code, value);
