bfbb37e91591e3422a02e0cee00676a0579557f4
EmanuelCN <emanuelghub@gmail.com>
2024-01-30 15:09:42 +0200

    treewide: Revert Samsung UFS Features

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -23,10 +23,6 @@
 #include <linux/nls.h>
 #include <linux/sched/signal.h>
 
-#if defined(CONFIG_UFSTW)
-#include <linux/ufstw.h>
-#endif
-
 #include "f2fs.h"
 #include "node.h"
 #include "segment.h"
@@ -267,10 +263,7 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 	};
 	unsigned int seq_id = 0;
 
-#if defined(CONFIG_UFSTW)
-	bool turbo_set = false;
-#endif
-        if (unlikely(f2fs_readonly(inode->i_sb)))
+	if (unlikely(f2fs_readonly(inode->i_sb)))
 		return 0;
 
 	trace_f2fs_sync_file_enter(inode);
@@ -344,10 +337,6 @@ go_write:
 		clear_inode_flag(inode, FI_UPDATE_WRITE);
 		goto out;
 	}
-#if defined(CONFIG_UFSTW)
-	bdev_set_turbo_write(sbi->sb->s_bdev);
-	turbo_set = true;
-#endif
 sync_nodes:
 	atomic_inc(&sbi->wb_sync_req[NODE]);
 	ret = f2fs_fsync_node_pages(sbi, inode, &wbc, atomic, &seq_id);
@@ -395,10 +384,6 @@ flush_out:
 	}
 	f2fs_update_time(sbi, REQ_TIME);
 out:
-#if defined(CONFIG_UFSTW)
-	if (turbo_set)
-		bdev_clear_turbo_write(sbi->sb->s_bdev);
-#endif
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
 	return ret;
 }
@@ -1030,12 +1015,7 @@ int f2fs_setattr(struct dentry *dentry, struct iattr *attr)
 			clear_inode_flag(inode, FI_ACL_MODE);
 		}
 	}
-#ifdef CONFIG_FS_HPB
-		if (__is_hpb_file(dentry->d_name.name, inode))
-			set_inode_flag(inode, FI_HPB_INODE);
-		else
-			clear_inode_flag(inode, FI_HPB_INODE);
-#endif
+
 	/* file size may changed here */
 	f2fs_mark_inode_dirty_sync(inode, true);
 
