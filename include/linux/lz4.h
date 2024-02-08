d5134d54c5c98673e17086e25c495551dfef0ec3
EmanuelCN <emanuelghub@gmail.com>
2024-02-08 20:49:08 +0200

    Revert "include/linux: lz4: Reduce LZ4 memory usage to 1KB"
    This reverts commit fd9b4c198f6f591fb8a24546ec9940228a47dac1.

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -55,8 +55,7 @@
  * Reduced memory usage can improve speed, due to cache effect
  * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
  */
-/* We only use LZ4 for zRAM, so the blocks are 4KB in size. 1KB is enough here */
-#define LZ4_MEMORY_USAGE 10
+#define LZ4_MEMORY_USAGE 14
 
 #define LZ4_MAX_INPUT_SIZE	0x7E000000 /* 2 113 929 216 bytes */
 #define LZ4_COMPRESSBOUND(isize)	(\
