dee61d82f790d3ec954289e716ed7f91444bc67e
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-14 23:31:56 +0200

    techpack: display: Eliminate unnecessary snprintf() usage from hot paths There's no reason to constantly use snprintf() to generate pretty debug strings from hot paths. We don't need them, so remove them.
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -84,7 +84,6 @@ uint32_t sde_sync_get_name_prefix(void *fence)
 struct sde_fence {
 	struct dma_fence base;
 	struct sde_fence_context *ctx;
-	char name[SDE_FENCE_NAME_SIZE];
 	struct list_head	fence_list;
 	int fd;
 };
@@ -109,9 +108,7 @@ static inline struct sde_fence *to_sde_fence(struct dma_fence *fence)
 
 static const char *sde_fence_get_driver_name(struct dma_fence *fence)
 {
-	struct sde_fence *f = to_sde_fence(fence);
-
-	return f->name;
+	return "sde_fence";
 }
 
 static const char *sde_fence_get_timeline_name(struct dma_fence *fence)
@@ -202,8 +199,6 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
-	snprintf(sde_fence->name, SDE_FENCE_NAME_SIZE, "sde_fence:%s:%u",
-						sde_fence->ctx->name, val);
 	dma_fence_init(&sde_fence->base, &sde_fence_ops, &ctx->lock,
 		ctx->context, val);
 	kref_get(&ctx->kref);
@@ -211,8 +206,8 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	/* create fd */
 	fd = get_unused_fd_start_flags(1, 0);
 	if (fd < 0) {
-		SDE_ERROR("failed to get_unused_fd_flags(), %s\n",
-							sde_fence->name);
+		SDE_ERROR("failed to get_unused_fd_flags(), sde_fence:%s:%u\n",
+			  sde_fence->ctx->name, val);
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
@@ -222,7 +217,8 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	if (sync_file == NULL) {
 		put_unused_fd(fd);
 		fd = -EINVAL;
-		SDE_ERROR("couldn't create fence, %s\n", sde_fence->name);
+		SDE_ERROR("couldn't create fence, sde_fence:%s:%u\n",
+			  sde_fence->ctx->name, val);
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
