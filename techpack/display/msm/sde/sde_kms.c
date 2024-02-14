71dc317a9cacccfedb96385c461b439dd8723dca
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-14 23:34:27 +0200

    qos: Replace cpumasks with atomic bitwise operations This is expensive and unneeded, especially since the qos functions are hot code paths. Mose of the cpumask functions use the bitmap API, which is also more expensive than just doing some simple operations on a word.
    Since we're operating with a CPU count that can fit within a word,
    replace the expensive cpumask operations with plain bitwise operations
    wherever possible to make the pm_qos framework more efficient.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>s

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -3276,12 +3276,13 @@ void sde_kms_update_pm_qos_irq_request(struct sde_kms *sde_kms,
 
 		req = &sde_kms->pm_qos_irq_req;
 		req->type = PM_QOS_REQ_AFFINE_CORES;
-		req->cpus_affine = sde_kms->irq_cpu_mask;
+		atomic_set(&req->cpus_affine,
+			   *cpumask_bits(&sde_kms->irq_cpu_mask));
 		cpu_irq_latency = sde_kms->catalog->perf.cpu_irq_latency;
 
 		if (pm_qos_request_active(req))
 			pm_qos_update_request(req, cpu_irq_latency);
-		else if (!cpumask_empty(&req->cpus_affine)) {
+		else if (atomic_read(&req->cpus_affine)) {
 			/** If request is not active yet and mask is not empty
 			 *  then it needs to be added initially
 			 */
