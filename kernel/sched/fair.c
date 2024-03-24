59d2674f0a2a2049d9d1c2b4a049946fe32a18d6
Qais Yousef <qyousef@layalina.io>
2024-03-24 00:45:52 +0000

    sched/fair: Don't double balance_interval for migrate_misfit
    It is not necessarily an indication of the system being busy and
    requires a backoff of the load balancer activities. But pushing it high
    could mean generally delaying other misfit activities or other type of
    imbalances.
    
    Also don't pollute nr_balance_failed because of misfit failures. The
    value is used for enabling cache hot migration and in migrate_util/load
    types. None of which should be impacted (skewed) by misfit failures.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20240324004552.999936-5-qyousef@layalina.io

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11639,8 +11639,12 @@ more_balance:
 		 * We do not want newidle balance, which can be very
 		 * frequent, pollute the failure counter causing
 		 * excessive cache_hot migrations and active balances.
+		 *
+		 * Similarly for migration_misfit which is not related to
+		 * load/util migration, don't pollute nr_balance_failed.
 		 */
-		if (idle != CPU_NEWLY_IDLE)
+		if (idle != CPU_NEWLY_IDLE &&
+		    env.migration_type != migrate_misfit)
 			sd->nr_balance_failed++;
 
 		if (need_active_balance(&env)) {
@@ -11729,8 +11733,13 @@ out_one_pinned:
 	 * repeatedly reach this code, which would lead to balance_interval
 	 * skyrocketting in a short amount of time. Skip the balance_interval
 	 * increase logic to avoid that.
+	 *
+	 * Similarly misfit migration which is not necessarily an indication of
+	 * the system being busy and requires lb to backoff to let it settle
+	 * down.
 	 */
-	if (env.idle == CPU_NEWLY_IDLE)
+	if (env.idle == CPU_NEWLY_IDLE ||
+	    env.migration_type == migrate_misfit)
 		goto out;
 
 	/* tune up the balancing interval */
