ea788d1bcd4f9cb13540432db70877b4e459b33a
Sultan Alsawaf <sultan@kerneltoast.com>
2024-04-04 19:32:47 +0000

    cpufreq: schedutil: Use the frequency below the target if they're close Schedutil targets a frequency tipping point of 80% to vote for a higher frequency when utilization crosses that threshold.
    Since the tipping point calculation is done without regard to the size of
    the gap between each frequency step, this often results in a large
    frequency jump when it isn't strictly necessary, which hurts energy
    efficiency.
    
    For example, if a CPU has 2000 MHz and 3000 MHz frequency steps, and
    schedutil targets a frequency of 2005 MHz, then the 3000 MHz frequency step
    will be used even though the target frequency of 2005 MHz is very close to
    2000 MHz. In this hypothetical scenario, using 2000 MHz would clearly
    satisfy the system's performance needs while consuming less energy than the
    3000 MHz step.
    
    To counter-balance the frequency tipping point, add a frequency tipping
    point in the opposite direction to prefer the frequency step below the
    calculated target frequency when the target frequency is less than 20%
    higher than that lower step. A threshold of 20% was empirically determined
    to provide significant energy savings without really impacting performance.
    
    This improves schedutil's energy efficiency on CPUs which have large gaps
    between their frequency steps, as is often the case on ARM.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -215,7 +215,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	struct cpufreq_policy *policy = sg_policy->policy;
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
-
+	unsigned int idx, l_freq, h_freq;
 	freq = map_util_freq(util, freq, max);
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
@@ -224,7 +224,21 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	sg_policy->need_freq_update = false;
 	sg_policy->prev_cached_raw_freq = sg_policy->cached_raw_freq;
 	sg_policy->cached_raw_freq = freq;
-	return cpufreq_driver_resolve_freq(policy, freq);
+	l_freq = cpufreq_driver_resolve_freq(policy, freq);
+	idx = cpufreq_frequency_table_target(policy, freq, CPUFREQ_RELATION_H);
+	h_freq = policy->freq_table[idx].frequency;
+	h_freq = clamp(h_freq, policy->min, policy->max);
+	if (l_freq <= h_freq || l_freq == policy->min)
+		return l_freq;
+
+	/*
+	 * Use the frequency step below if the calculated frequency is <20%
+	 * higher than it.
+	 */
+	if (mult_frac(100, freq - h_freq, l_freq - h_freq) < 20)
+		return h_freq;
+
+	return l_freq;
 }
 
 extern long
