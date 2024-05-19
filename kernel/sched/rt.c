bcf2fc6c60966d6a7a85a7b4b21a6fb739d238b3
EmanuelCN <emanuelghub@gmail.com>
2024-05-19 12:37:12 +0300

    sched/rt: Guard walt's rt_energy_aware_wake_cpu

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1790,6 +1790,7 @@ static struct task_struct *pick_highest_pushable_task(struct rq *rq, int cpu)
 
 static DEFINE_PER_CPU(cpumask_var_t, local_cpu_mask);
 
+#ifdef CONFIG_SCHED_WALT
 static int rt_energy_aware_wake_cpu(struct task_struct *task)
 {
 	struct sched_domain *sd;
@@ -1893,6 +1894,12 @@ unlock:
 	rcu_read_unlock();
 	return best_cpu;
 }
+#else
+static inline int rt_energy_aware_wake_cpu(struct task_struct *task)
+{
+	return -1;
+}
+#endif
 
 static int find_lowest_rq(struct task_struct *task)
 {
