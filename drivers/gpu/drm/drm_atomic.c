4bba7c2c56c8696cc86571e40273cd295762e109
EmanuelCN <emanuelghub@gmail.com>
2024-06-09 14:27:55 +0300

    defconfig: Disable cpu input boost

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,7 +34,6 @@
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/pm_qos.h>
 
 #include "drm_crtc_internal.h"
@@ -2615,7 +2614,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 
 	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 	}
 
