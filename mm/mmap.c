48edeafb42bad3e2de806c76d76749f05c3b8dae
darkhz <kmachanwenw@gmail.com>
2024-08-01 20:47:55 +0300

    mm: Do not reserve memory for users
    Waste of memory.

--- mm/mmap.c
+++ mm/mmap.c
@@ -3739,11 +3739,6 @@ void __init mmap_init(void)
  */
 static int init_user_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_user_reserve_kbytes = min(free_kbytes / 32, 1UL << 17);
 	return 0;
 }
 subsys_initcall(init_user_reserve);
--- mm/util.c
+++ mm/util.c
@@ -581,7 +581,7 @@ int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_GUESS;
 int sysctl_overcommit_ratio __read_mostly = 50;
 unsigned long sysctl_overcommit_kbytes __read_mostly;
 int sysctl_max_map_count __read_mostly = DEFAULT_MAX_MAP_COUNT;
-unsigned long sysctl_user_reserve_kbytes __read_mostly = 1UL << 17; /* 128MB */
+unsigned long sysctl_user_reserve_kbytes __read_mostly; /* 0MB */
 unsigned long sysctl_admin_reserve_kbytes __read_mostly; /* 0MB */
 
 int overcommit_ratio_handler(struct ctl_table *table, int write,
