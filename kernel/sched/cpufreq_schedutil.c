bedd9095f79c073b2110fe2c965148d18036e141
EmanuelCN <emanuelghub@gmail.com>
2024-08-06 20:24:40 +0300

    cpufreq: schedutil: Set rate-limits globally
    Since we are not gonna modify them per cluster anymore its redundant to keep them this way, plus its less expensive.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -933,23 +933,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
-
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 1000;
-	}
-
-	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 1000;
-	}
-
-        if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-                tunables->up_rate_limit_us = 500;
-                tunables->down_rate_limit_us = 1000;
-        }
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 1000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
