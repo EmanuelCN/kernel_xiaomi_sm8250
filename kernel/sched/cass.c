c3a6058d951953b106142ef40e9dcaf9225f6baf
Sultan Alsawaf <sultan@kerneltoast.com>
2024-09-24 18:14:46 -0700

    sched/cass: Avoid using the prime CPU on systems that have one
    On arm64 systems with a prime CPU, treating this CPU – which is the single
    fastest one in the system – the same as any other CPU is extremely bad for
    energy efficiency. This is because prime CPUs are designed to be very fast
    at the expense of energy efficiency, serving as the single-core performance
    workhorse of a system.
    
    Since CASS hasn't been giving special treatment to prime CPUs, CASS has
    been balancing relative load onto prime CPUs at great expense to energy.
    
    Thanks to the checks in place for CPU overload and task fit, it's easy to
    adjust CASS to avoid using prime CPUs without hurting single-core or
    multi-core performance.
    
    Place a check just below the task fit check for whether or not a candidate
    is a prime CPU. This way, when CPUs are overloaded or a task only fits on
    the prime CPU, CASS will still utilize the prime CPU without a loss of
    performance.
    
    This provides a double-digit percent improvement to energy efficiency
    across the board as measured on Tensor G3 (Pixel 8) and Tensor G4 (Pixel 9
    Pro).
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -77,6 +77,21 @@ void cass_cpu_util(struct cass_cpu_cand *c, int this_cpu, bool sync)
 	c->cap_no_therm = c->cap_orig - min(c->hard_util, c->cap_orig - 1);
 }
 
+/*
+ * Returns true if @c is a CPU with the maximum possible original capacity and
+ * there's only one such CPU in the system (i.e., if @c is the prime CPU).
+ */
+static __always_inline
+bool cass_prime_cpu(const struct cass_cpu_cand *c)
+{
+	/*
+	 * On arm64, the prime CPU is always the last CPU. If it doesn't have
+	 * the same original capacity as the prior CPU, then it is prime.
+	 */
+	return c->cpu == nr_cpu_ids - 1 &&
+	       arch_scale_cpu_capacity(nr_cpu_ids - 2) != SCHED_CAPACITY_SCALE;
+}
+
 /* Returns true if @a is a better CPU than @b */
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
@@ -100,6 +115,10 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	/* Prefer the CPU that fits the task */
 	if (cass_cmp(fits_capacity(p_util, a->cap_max),
 		     fits_capacity(p_util, b->cap_max)))
+		goto done;
+
+	/* Prefer the CPU that isn't the single fastest one in the system */
+	if (cass_cmp(cass_prime_cpu(b), cass_prime_cpu(a)))
 		goto done;
 
 	/* Prefer the CPU with lower relative utilization */
@@ -185,13 +204,16 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * sync wakes, treat the current CPU as idle if @current is the
 		 * only running task.
 		 */
+		curr->cpu = cpu;
 		if ((sync && cpu == this_cpu && rq->nr_running == 1) ||
 		    available_idle_cpu(cpu) || sched_idle_cpu(cpu)) {
 			/*
-			 * A non-idle candidate may be better when @p is uclamp
-			 * boosted. Otherwise, always prefer idle candidates.
+			 * A non-idle candidate may be better for energy
+			 * efficiency when @p is uclamp boosted, or when the
+			 * only idle candidate found so far is the prime CPU.
+			 * Otherwise, prefer idle candidates.
 			 */
-			if (!uc_min) {
+			if (!uc_min && !cass_prime_cpu(curr)) {
 				/* Discard any previous non-idle candidate */
 				if (!has_idle)
 					best = curr;
@@ -215,7 +237,6 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		}
 
 		/* Get this CPU's capacity and utilization */
-		curr->cpu = cpu;
 		cass_cpu_util(curr, this_cpu, sync);
 
 		/*
