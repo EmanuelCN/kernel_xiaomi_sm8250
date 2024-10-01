8370fd5da5f2528f212b766577612c2c59516bad
Lina Iyer <ilina@codeaurora.org>
2024-10-01 21:17:11 +0300

    BACKPORT: drivers: qcom: rpmh: optimize RPMh debug for perf images Let's us not worry about reporting RPM retries in perf builds. This should help speed up the boot process.
    Change-Id: Ia0d64e7da293bd8e77fd1484a714c8f21121e3fb
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -479,8 +479,10 @@ int rpmh_rsc_send_data(struct rsc_drv *drv, const struct tcs_request *msg)
 	do {
 		ret = tcs_write(drv, msg);
 		if (ret == -EBUSY) {
+#ifdef QCOM_RPMH_DEBUG
 			pr_info_ratelimited("DRV:%s TCS Busy, retrying RPMH message send: addr=%#x\n",
 					    drv->name, msg->cmds[0].addr);
+#endif /* QCOM_RPMH_DEBUG */
 			udelay(10);
 			count++;
 		}
