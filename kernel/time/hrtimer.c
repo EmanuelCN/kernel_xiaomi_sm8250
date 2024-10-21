e1656e2cabc20afae4cc8da9c2d3a853cf611364
EmanuelCN <emanuelghub@gmail.com>
2024-10-21 22:09:18 +0300

    Revert "timer: Do not require CPUSETS to be enabled for migration"
    This reverts commit ec86ffd80c8fbcc22adff9bdb7f4e1aae0cb73fb.

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1897,7 +1897,7 @@ int hrtimers_prepare_cpu(unsigned int cpu)
 	return 0;
 }
 
-#ifdef CONFIG_HOTPLUG_CPU
+#if defined(CONFIG_HOTPLUG_CPU) || defined(CONFIG_CPUSETS)
 static void migrate_hrtimer_list(struct hrtimer_clock_base *old_base,
 				 struct hrtimer_clock_base *new_base,
 				 bool remove_pinned)
@@ -1989,7 +1989,9 @@ static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 	local_irq_restore(flags);
 	local_bh_enable();
 }
+#endif /* CONFIG_HOTPLUG_CPU || CONFIG_CPUSETS */
 
+#ifdef CONFIG_HOTPLUG_CPU
 int hrtimers_dead_cpu(unsigned int scpu)
 {
 	BUG_ON(cpu_online(scpu));
@@ -1998,13 +2000,14 @@ int hrtimers_dead_cpu(unsigned int scpu)
 	__migrate_hrtimers(scpu, true);
 	return 0;
 }
+#endif /* CONFIG_HOTPLUG_CPU */
 
+#ifdef CONFIG_CPUSETS
 void hrtimer_quiesce_cpu(void *cpup)
 {
 	__migrate_hrtimers(*(int *)cpup, false);
 }
-
-#endif /* CONFIG_HOTPLUG_CPU */
+#endif /* CONFIG_CPUSETS */
 
 void __init hrtimers_init(void)
 {
