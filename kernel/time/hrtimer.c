853408149124b3a9cb72fe9b736326c386e807e8
EmanuelCN <emanuelghub@gmail.com>
2024-10-21 22:05:37 +0300

    Revert "hrtimer: fix issue during core isolation"
    This reverts commit 18d10b90d859cc75d96459cab8dde2739f1bdb29.

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1954,6 +1954,12 @@ static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 	unsigned long flags;
 	int i;
 
+	/*
+	 * this BH disable ensures that raise_softirq_irqoff() does
+	 * not wakeup ksoftirqd (and acquire the pi-lock) while
+	 * holding the cpu_base lock
+	 */
+	local_bh_disable();
 	local_irq_save(flags);
 	old_base = &per_cpu(hrtimer_bases, scpu);
 	new_base = this_cpu_ptr(&hrtimer_bases);
@@ -1981,6 +1987,7 @@ static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 	/* Check, if we got expired work to do */
 	__hrtimer_peek_ahead_timers();
 	local_irq_restore(flags);
+	local_bh_enable();
 }
 #endif /* CONFIG_HOTPLUG_CPU || CONFIG_CPUSETS */
 
@@ -1990,14 +1997,7 @@ int hrtimers_dead_cpu(unsigned int scpu)
 	BUG_ON(cpu_online(scpu));
 	tick_cancel_sched_timer(scpu);
 
-	/*
-	 * this BH disable ensures that raise_softirq_irqoff() does
-	 * not wakeup ksoftirqd (and acquire the pi-lock) while
-	 * holding the cpu_base lock
-	 */
-	local_bh_disable();
 	__migrate_hrtimers(scpu, true);
-	local_bh_enable();
 	return 0;
 }
 #endif /* CONFIG_HOTPLUG_CPU */
