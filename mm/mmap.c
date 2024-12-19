3c1c1d25799196f1ba109b6d44986d662228d3fa
EmanuelCN <emanuelghub@gmail.com>
2024-12-19 19:49:08 +0200

    Revert "mm: Speed up PID map reads for Android's use case"
    This reverts commit 042acf97351dcd64df6be2d70737e14a7bfff8f0.

--- mm/mmap.c
+++ mm/mmap.c
@@ -172,8 +172,6 @@ static struct vm_area_struct *remove_vma(struct vm_area_struct *vma)
 	struct vm_area_struct *next = vma->vm_next;
 
 	might_sleep();
-	if (vma == vma->vm_mm->stack_vma)
-		vma->vm_mm->stack_vma = NULL;
 	if (vma->vm_ops && vma->vm_ops->close)
 		vma->vm_ops->close(vma);
 	if (vma->vm_file)
@@ -916,8 +914,6 @@ again:
 		mm->map_count--;
                 mpol_put(vma_policy(next));
                 vm_area_free(next);
-		if (next == mm->stack_vma)
-			mm->stack_vma = NULL;
 		/*
 		 * In mprotect's case 6 (see comments on vma_merge),
 		 * we must remove another next too. It would clutter
