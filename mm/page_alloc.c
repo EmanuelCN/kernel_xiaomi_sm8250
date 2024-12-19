d605a9fc8e67246b06a32a688faf7cbd3fd25073
EmanuelCN <emanuelghub@gmail.com>
2024-12-19 18:58:39 +0200

    Revert "mm: Disable pcp lists checks on !DEBUG_VM"
    This reverts commit f1fb3ea25d7b01d111fc55a58430c4d7f70c8a17.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1981,11 +1981,11 @@ static bool check_new_pcp(struct page *page)
 	return check_new_page(page);
 }
 #else
-static inline bool check_pcp_refill(struct page *page)
+static bool check_pcp_refill(struct page *page)
 {
-	return false;
+	return check_new_page(page);
 }
-static inline bool check_new_pcp(struct page *page)
+static bool check_new_pcp(struct page *page)
 {
 	return false;
 }
