8d0a8aa4ca366d2d2900ad5f485d035b36cd34f8
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-01-27 02:56:20 +0600

    soc: qcom: rpmh-rsc: Make tcs_invalidate() lockless
    Caller of tcs_invalidate() which goes to rpmh_rsc_invalidate(),
    ensures that no RPMH actions are running. Therefore, no lock is
    needed for tcs_invalidate().
    
    This is also suggested by Xiaomi in redwood-s-oss.
    
    Extracted from: https://github.com/torvalds/linux/commit/881808d0bbf33
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -136,28 +136,24 @@ static struct tcs_group *get_tcs_of_type(struct rsc_drv *drv, int type)
 
 static int tcs_invalidate(struct rsc_drv *drv, int type)
 {
-	int m, ret = 0;
+	int m;
 	struct tcs_group *tcs;
 
 	tcs = get_tcs_of_type(drv, type);
 
-	rpmh_spin_lock(&drv->lock);
 	if (bitmap_empty(tcs->slots, MAX_TCS_SLOTS))
-		goto done;
+		return 0;
 
 	for (m = tcs->offset; m < tcs->offset + tcs->num_tcs; m++) {
-		if (!tcs_is_free(drv, m)) {
-			ret = -EAGAIN;
-			goto done;
-		}
+		if (!tcs_is_free(drv, m))
+			return -EAGAIN;
+
 		write_tcs_reg_sync(drv, RSC_DRV_CMD_ENABLE, m, 0);
 		write_tcs_reg_sync(drv, RSC_DRV_CMD_WAIT_FOR_CMPL, m, 0);
 	}
 	bitmap_zero(tcs->slots, MAX_TCS_SLOTS);
 
-done:
-	rpmh_spin_unlock(&drv->lock);
-	return ret;
+	return 0;
 }
 
 /**
