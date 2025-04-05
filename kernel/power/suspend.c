f056438c62856083bbfb3d5d1bc15e9dd7138927
EmanuelCN <emanuelghub@gmail.com>
2025-04-05 22:46:41 +0300

    Reapply "PM / suspend: Make cpuidle work in the "freeze" state"
    This reverts commit 8997588c17ce90e2b0981c21c260aef9a633e7eb.

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -16,6 +16,7 @@
 #include <linux/init.h>
 #include <linux/console.h>
 #include <linux/cpu.h>
+#include <linux/cpuidle.h>
 #include <linux/syscalls.h>
 #include <linux/gfp.h>
 #include <linux/io.h>
@@ -101,6 +102,7 @@ static void s2idle_enter(void)
 	raw_spin_unlock_irq(&s2idle_lock);
 
 	get_online_cpus();
+	cpuidle_resume();
 
 	/* Push all the CPUs into the idle loop. */
 	wake_up_all_idle_cpus();
@@ -108,6 +110,7 @@ static void s2idle_enter(void)
 	swait_event_exclusive(s2idle_wait_head,
 		    s2idle_state == S2IDLE_STATE_WAKE);
 
+	cpuidle_pause();
 	put_online_cpus();
 
 	raw_spin_lock_irq(&s2idle_lock);
