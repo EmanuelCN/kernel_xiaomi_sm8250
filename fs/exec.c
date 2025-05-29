f7f8eacf956d03a22647a079e6dbf7e995449b4c
EmanuelCN <emanuelghub@gmail.com>
2025-05-29 12:29:59 +0300

    kernel: Affine miui SystemUI & Home to hp cores
    Only way to fix hyperos2 animations from running in slow motion without WALT.

--- fs/exec.c
+++ fs/exec.c
@@ -114,6 +114,8 @@ void dead_special_task(void)
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define SYSTEMUI_BIN "/system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk"
+#define MIUIHOME_BIN "product/priv-app/MiuiHome/MiuiHome.apk"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1928,6 +1930,17 @@ static int __do_execve_file(int fd, struct filename *filename,
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_sig = current->signal;
+		else if (unlikely(!strncmp(filename->name,
+					   SYSTEMUI_BIN,
+					   strlen(SYSTEMUI_BIN)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   MIUIHOME_BIN,
+					   strlen(MIUIHOME_BIN)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		}
 	}
 
 	if (is_global_init(current->parent)) {
