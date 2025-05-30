fde00ea008707005b1cb1f4d4c07e3b32ae7d325
EmanuelCN <emanuelghub@gmail.com>
2025-05-31 01:14:48 +0300

    simple_lmk: Lower vmpressure trigger point
    Its enough to avoid top-app from getting killed or stutter in extreme memory pressure situations.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -456,7 +456,7 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 				    unsigned long pressure, void *data)
 {
-	if (pressure == 100) {
+	if (pressure == 98) {
 		atomic_set(&needs_reclaim, 1);
 		smp_mb__after_atomic();
 		if (waitqueue_active(&oom_waitq))
