7cfcab5f78e06809412b400495e7ee34f1bb6929
EmanuelCN <emanuelghub@gmail.com>
2025-05-31 12:54:22 +0300

    adreno_tz: Rework target frequency calculation for high refresh rate
    The old frequency calculation lead to overly aggresive frequency requests in some games.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -409,10 +409,13 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
-		if (refresh_rate > 60)
-			scm_data[2] = priv->bin.busy_time * refresh_rate / 60;
-		else
+		if (refresh_rate >= 120) {
+			scm_data[2] = priv->bin.busy_time + (priv->bin.busy_time >> 1);
+		} else if (refresh_rate == 90) {
+			scm_data[2] = priv->bin.busy_time + (priv->bin.busy_time >> 2);
+		} else {
 			scm_data[2] = priv->bin.busy_time;
+		}
 		scm_data[3] = context_count;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv);
