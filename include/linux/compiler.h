8529fa76f3fa6cfb3bf41e678d582b079548d7aa
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-08-13 19:32:08 +0300

    compiler.h: Add data_race() and its dependencies
    data_race() will be used by commit "sched/core: Fix use-after-free
    bug in dup_user_cpus_ptr()".
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -320,6 +320,25 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 #include <asm/barrier.h>
 #include <linux/kasan-checks.h>
 
+/**
+ * data_race - mark an expression as containing intentional data races
+ *
+ * This data_race() macro is useful for situations in which data races
+ * should be forgiven.  One example is diagnostic code that accesses
+ * shared variables but is not a part of the core synchronization design.
+ *
+ * This macro *does not* affect normal code generation, but is a hint
+ * to tooling that data races here are to be ignored.
+ */
+#define data_race(expr)							\
+({									\
+	({								\
+		__unqual_scalar_typeof(({ expr; })) __v = ({ expr; });	\
+		__v;							\
+	});								\
+})
+
+
 #define __READ_ONCE(x, check)						\
 ({									\
 	union { typeof(x) __val; char __c[1]; } __u;			\
