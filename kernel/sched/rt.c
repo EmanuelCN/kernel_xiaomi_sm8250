1b8a68d0b3d9b5a6bce81b37b3f45a207fa50fe9
EmanuelCN <emanuelghub@gmail.com>
2025-08-25 17:14:49 +0300

    kernel/sched: Move rt_rq_throttled() to sched header
    To make it visible for cass
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -570,11 +570,6 @@ static void sched_rt_rq_dequeue(struct rt_rq *rt_rq)
 		dequeue_rt_entity(rt_se, 0);
 }
 
-static inline int rt_rq_throttled(struct rt_rq *rt_rq)
-{
-	return rt_rq->rt_throttled && !rt_rq->rt_nr_boosted;
-}
-
 static int rt_se_boosted(struct sched_rt_entity *rt_se)
 {
 	struct rt_rq *rt_rq = group_rt_rq(rt_se);
@@ -649,11 +644,6 @@ static inline void sched_rt_rq_enqueue(struct rt_rq *rt_rq)
 static inline void sched_rt_rq_dequeue(struct rt_rq *rt_rq)
 {
 	dequeue_top_rt_rq(rt_rq, rt_rq->rt_nr_running);
-}
-
-static inline int rt_rq_throttled(struct rt_rq *rt_rq)
-{
-	return rt_rq->rt_throttled;
 }
 
 static inline const struct cpumask *sched_rt_period_mask(void)
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -768,7 +768,17 @@ struct rt_rq {
 	struct task_group	*tg;
 #endif
 };
-
+#ifdef CONFIG_RT_GROUP_SCHED
+static inline int rt_rq_throttled(struct rt_rq *rt_rq)
+{
+        return rt_rq->rt_throttled && !rt_rq->rt_nr_boosted;
+}
+#else
+static inline int rt_rq_throttled(struct rt_rq *rt_rq)
+{
+        return rt_rq->rt_throttled;
+}
+#endif
 static inline bool rt_rq_is_runnable(struct rt_rq *rt_rq)
 {
 	return rt_rq->rt_queued && rt_rq->rt_nr_running;
