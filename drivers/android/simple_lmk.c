dbbc5bf07775c3f5c6732e3e808a234e8e3be497
Kazuki H <kazukih0205@gmail.com>
2025-10-12 16:15:52 +0900

    simple_lmk: Thaw the entire victim process
    Similar to
    https://github.com/aosp-mirror/kernel_common/commit/226e30d5a053c690688cfc009909b61b6de9349a.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -280,13 +280,13 @@ static void scan_and_kill(void)
 			set_tsk_thread_flag(t, TIF_MEMDIE);
 		for_each_thread(vtsk, t)
 			set_task_rt_prio(t, 1);
+		/* Signals can't wake frozen tasks; only a thaw operation can */
+		for_each_thread(vtsk, t)
+			__thaw_task(t);
 		rcu_read_unlock();
 
 		/* Allow the victim to run on any CPU. This won't schedule. */
 		set_cpus_allowed_ptr(vtsk, cpu_all_mask);
-
-		/* Signals can't wake frozen tasks; only a thaw operation can */
-		__thaw_task(vtsk);
 
 		/* Store the number of anon pages to sort victims for reaping */
 		victim->size = get_mm_counter(mm, MM_ANONPAGES);
